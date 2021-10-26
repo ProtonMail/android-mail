85c33c7904601718d757d4e47f3212eb7e48b73b
Marino Meneghel <marino.meneghel@protonmail.com>
2021-10-26 15:21:31 +0200

    Init project following Android Studio defaults
    MAILAND-2458

---
+++ app/src/main/java/ch/protonmail/android/MainActivity.kt
@@ -0,0 +1,11 @@
+package ch.protonmail.android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
