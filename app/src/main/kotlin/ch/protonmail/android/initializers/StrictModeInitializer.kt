7be377fe1fda4bf0546422f6593f8ed02e67a05a
Marino Meneghel <marino.meneghel@protonmail.com>
2021-11-10 16:23:38 +0100

    Initialise StrictMode at app start
    See [Strict Mode](https://developer.android.com/reference/android/os/StrictMode)
    for additional details
    
    MAILANDR-14

---
+++ app/src/main/kotlin/ch/protonmail/android/initializers/StrictModeInitializer.kt
@@ -0,0 +1,33 @@
+package ch.protonmail.android.initializers
+
+import android.content.Context
+import android.os.StrictMode
+import androidx.startup.Initializer
+import ch.protonmail.android.BuildConfig
+
+class StrictModeInitializer : Initializer<Unit> {
+
+    override fun create(context: Context) {
+        if (BuildConfig.DEBUG) {
+            enableStrictMode()
+        }
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> {
+        return emptyList()
+    }
+
+    private fun enableStrictMode() {
+        val threadPolicyBuilder = StrictMode.ThreadPolicy.Builder()
+            .detectAll()
+            .penaltyFlashScreen()
+            .penaltyLog()
+        val vmPolicyBuilder = StrictMode.VmPolicy.Builder()
+            .detectAll()
+            .penaltyLog()
+
+        StrictMode.setThreadPolicy(threadPolicyBuilder.build())
+        StrictMode.setVmPolicy(vmPolicyBuilder.build())
+    }
+
+}
