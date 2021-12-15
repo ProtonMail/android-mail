cdfd6444019ebda4fe020fcfdd6a0024d2721cb3
Vincent Brison <vincent.brison@proton.ch>
2021-12-15 11:57:52 +0100

    Add ability switch from core binaries to git submodule using sys env

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,7 +2,13 @@ rootProject.name = "ProtonMail"
 
 // All Core modules are taken/overridden from the local Git submodule proton-libs.
 // Comment the includeBuild line below to switch to Core modules published artefacts.
-// includeBuild("proton-libs")
+val coreCommitSha: String? = System.getenv("CORE_COMMIT_SHA")
+if (coreCommitSha?.isNotBlank() == true) {
+    println("Use core libs as included build with commit ref \'$coreCommitSha\'")
+    includeBuild("proton-libs")
+} else {
+    println("Use core libs binaries from Maven")
+}
 
 include(":app")
 include(":mail-message:data")
