d3e44a13c2a12e8a5b8f698e669cf84be42bcbfe
Vincent Brison <vincent.brison@proton.ch>
2021-12-20 16:36:20 +0100

    Use gradle properties to control git submodule usage
    Env variable does not work very well with gradle daemon as it will not detect updates. To fix that, we use a more gradle agnostic way to control that using gradle project properties.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 rootProject.name = "ProtonMail"
 
 // Use core libs from maven artifacts by default
-val useCoreGitSubmodule: Boolean = System.getenv("USE_CORE_GIT_SUBMODULE").toBoolean()
-if (useCoreGitSubmodule) {
+val useCoreGitSubmoduleAsBoolean: Boolean = extensions.extraProperties.properties["useCoreGitSubmodule"].toString().toBoolean()
+if (useCoreGitSubmoduleAsBoolean) {
     println("Use core libs from git submodule \'./proton-libs\'")
     includeBuild("proton-libs")
 } else {
