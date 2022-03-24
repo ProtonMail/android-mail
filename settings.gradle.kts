3e55c7790270afc426b6991d1c1344f432042ea1
Mateusz Armatys <mateusz.armatys@proton.ch>
2022-03-24 17:27:01 +0100

    Core submodule: add fallback in case the checked out Core doesn't contain the `include-core-libs.gradle.kts` file.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,13 @@ rootProject.name = "ProtonMail"
 // - to enable/disable on CI: .gitlab-ci.yml > ORG_GRADLE_PROJECT_useCoreGitSubmodule
 val coreSubmoduleDir = rootDir.resolve("proton-libs")
 extra.set("coreSubmoduleDir", coreSubmoduleDir)
-apply(from = "${coreSubmoduleDir.path}/gradle/include-core-libs.gradle.kts")
+val includeCoreLibsHelper = File(coreSubmoduleDir, "gradle/include-core-libs.gradle.kts")
+if (includeCoreLibsHelper.exists()) {
+    apply(from = "${coreSubmoduleDir.path}/gradle/include-core-libs.gradle.kts")
+} else if (extensions.extraProperties["useCoreGitSubmodule"].toString().toBoolean()) {
+    includeBuild("proton-libs")
+    println("Core libs from git submodule `$coreSubmoduleDir`")
+}
 
 include(":app")
 include(":test-data")
