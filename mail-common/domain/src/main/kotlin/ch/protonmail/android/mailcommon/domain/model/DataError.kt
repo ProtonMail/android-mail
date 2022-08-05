7903a2504837f3b6eb7277758ed0191f6c2fd90f
Davide Farella <davide.farella@proton.ch>
2022-08-05 11:28:43 +0200

    Create a model to represent errors while fetching data
    MAILANDR-203

---
+++ mail-common/domain/src/main/kotlin/ch/protonmail/android/mailcommon/domain/model/DataError.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcommon.domain.model
+
+/**
+ * Errors related to Data
+ */
+sealed interface DataError {
+
+    /**
+     * Errors related to Local persistence
+     */
+    sealed interface Local : DataError {
+
+        /**
+         * There's no cached value in the local persistence
+         */
+        object NoCache : Local
+
+        /**
+         * A not handled error happened in the local persistence
+         */
+        data class Unknown(val message: String) : Local
+    }
+
+    /**
+     * Error fetching date from Remote source
+     */
+    sealed interface Remote : DataError {
+
+        /**
+         * The API returned a failure response
+         */
+        data class Http(val networkError: NetworkError) : Remote
+
+        /**
+         * The API returned a success, but proton code is not OK
+         */
+        data class Proton(val protonError: ProtonError) : Remote
+
+        /**
+         * A not handled error happened in the remote source
+         */
+        data class Unknown(val message: String) : Remote
+    }
+}
---
+++ mail-common/domain/src/main/kotlin/ch/protonmail/android/mailcommon/domain/model/NetworkError.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcommon.domain.model
+
+/**
+ * Errors related to Network
+ */
+sealed interface NetworkError {
+
+    /**
+     * Request is forbidden
+     * 403 error
+     */
+    object Forbidden : NetworkError
+
+    /**
+     * Network connectivity is not available
+     */
+    object NoNetwork : NetworkError
+
+    /**
+     * Requested url cannot be found.
+     * 404 error
+     */
+    object NotFound : NetworkError
+
+    /**
+     * Server has encountered an error.
+     * 500 error
+     */
+    object Internal : NetworkError
+
+    /**
+     * Request is not authorized
+     * 401 error
+     */
+    object Unauthorized : NetworkError
+
+    /**
+     * Requested host is not reachable
+     */
+    object Unreachable : NetworkError
+
+    data class Unknown(val httpCode: Int) : NetworkError
+}
