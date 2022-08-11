bb5195b9d4862096b22f8ef3ea4f510ec86cdbc2
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2022-08-11 17:31:48 +0200

    Add AlternativeRoutingSettingViewModel
    MAILANDR-94

---
+++ mail-settings/presentation/src/main/kotlin/ch/protonmail/android/mailsettings/presentation/settings/alternativerouting/AlternativeRoutingSettingViewModel.kt
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.alternativerouting
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ch.protonmail.android.mailcommon.presentation.Effect
+import ch.protonmail.android.mailsettings.domain.usecase.ObserveAlternativeRoutingSetting
+import ch.protonmail.android.mailsettings.domain.usecase.SaveAlternativeRoutingSetting
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+import me.proton.core.compose.viewmodel.stopTimeoutMillis
+import javax.inject.Inject
+
+@HiltViewModel
+class AlternativeRoutingSettingViewModel @Inject constructor(
+    observeAlternativeRoutingSetting: ObserveAlternativeRoutingSetting,
+    private val saveAlternativeRoutingSetting: SaveAlternativeRoutingSetting
+) : ViewModel() {
+
+    private val alternativeRoutingSettingErrorFlow: MutableStateFlow<Effect<Unit>> = MutableStateFlow(Effect.empty())
+
+    val state: Flow<AlternativeRoutingSettingState> = combine(
+        observeAlternativeRoutingSetting(),
+        alternativeRoutingSettingErrorFlow
+    ) { alternativeRoutingPreferenceEither, alternativeRoutingSettingErrorEffect ->
+        alternativeRoutingPreferenceEither.fold(
+            ifLeft = {
+                AlternativeRoutingSettingState.Data(
+                    isEnabled = null,
+                    alternativeRoutingSettingErrorEffect = Effect.of(Unit)
+                )
+            },
+            ifRight = { combinedContactsPreference ->
+                AlternativeRoutingSettingState.Data(
+                    isEnabled = combinedContactsPreference.isEnabled,
+                    alternativeRoutingSettingErrorEffect = alternativeRoutingSettingErrorEffect
+                )
+            }
+        )
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(stopTimeoutMillis),
+        initialValue = AlternativeRoutingSettingState.Loading
+    )
+
+    fun saveAlternativeRoutingPreference(alternativeRoutingPreference: Boolean) = viewModelScope.launch {
+        saveAlternativeRoutingSetting(alternativeRoutingPreference)
+            .tapLeft { alternativeRoutingSettingErrorFlow.emit(Effect.of(Unit)) }
+    }
+}
---
+++ mail-settings/presentation/src/test/kotlin/ch/protonmail/android/mailsettings/presentation/settings/alternativerouting/AlternativeRoutingSettingViewModelTest.kt
@@ -0,0 +1,158 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.alternativerouting
+
+import app.cash.turbine.test
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.PreferencesError
+import ch.protonmail.android.mailsettings.domain.model.AlternativeRoutingPreference
+import ch.protonmail.android.mailsettings.domain.usecase.ObserveAlternativeRoutingSetting
+import ch.protonmail.android.mailsettings.domain.usecase.SaveAlternativeRoutingSetting
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Test
+import kotlin.test.BeforeTest
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+class AlternativeRoutingSettingViewModelTest {
+
+    private val alternativeRoutingPreference = AlternativeRoutingPreference(isEnabled = true)
+
+    private val observeAlternativeRoutingSetting: ObserveAlternativeRoutingSetting = mockk {
+        every { this@mockk() } returns flowOf(alternativeRoutingPreference.right())
+    }
+    private val saveAlternativeRoutingSetting: SaveAlternativeRoutingSetting = mockk()
+
+    private val alternativeRoutingSettingViewModel by lazy {
+        AlternativeRoutingSettingViewModel(
+            observeAlternativeRoutingSetting,
+            saveAlternativeRoutingSetting
+        )
+    }
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @Test
+    fun `should return loading state before the repository emits any value`() = runTest {
+        // Given
+        every { observeAlternativeRoutingSetting() } returns flowOf()
+
+        // When
+        alternativeRoutingSettingViewModel.state.test {
+            // Then
+            val loadingState = awaitItem()
+            assertEquals(AlternativeRoutingSettingState.Loading, loadingState)
+        }
+    }
+
+    @Test
+    fun `state should contain correct data when repository emits the preference`() = runTest {
+        // When
+        alternativeRoutingSettingViewModel.state.test {
+            // Then
+            val dataState = assertIs<AlternativeRoutingSettingState.Data>(awaitItem())
+            assertTrue(dataState.isEnabled!!)
+            assertNull(dataState.alternativeRoutingSettingErrorEffect.consume())
+        }
+    }
+
+    @Test
+    fun `state should contain correct data when an error occurs while observing the preference`() = runTest {
+        // Given
+        coEvery {
+            observeAlternativeRoutingSetting()
+        } returns flowOf(PreferencesError.left())
+
+        // When
+        alternativeRoutingSettingViewModel.state.test {
+            // Then
+            val dataState = assertIs<AlternativeRoutingSettingState.Data>(awaitItem())
+            assertNull(dataState.isEnabled)
+            assertNotNull(dataState.alternativeRoutingSettingErrorEffect.consume())
+        }
+    }
+
+    @Test
+    fun `should call repository save method when saving alternative routing preference`() = runTest {
+        // Given
+        coEvery {
+            saveAlternativeRoutingSetting(alternativeRoutingPreference.isEnabled)
+        } returns Unit.right()
+
+        // When
+        alternativeRoutingSettingViewModel.saveAlternativeRoutingPreference(alternativeRoutingPreference.isEnabled)
+
+        // Then
+        coVerify { saveAlternativeRoutingSetting(alternativeRoutingPreference.isEnabled) }
+    }
+
+    @Test
+    fun `state should emit error data when an exception is thrown during saving alternative routing preference`() = runTest {
+        // Given
+        coEvery {
+            saveAlternativeRoutingSetting(alternativeRoutingPreference.isEnabled)
+        } returns PreferencesError.left()
+
+        // When
+        alternativeRoutingSettingViewModel.saveAlternativeRoutingPreference(alternativeRoutingPreference.isEnabled)
+
+        // Then
+        alternativeRoutingSettingViewModel.state.test {
+            val dataState = assertIs<AlternativeRoutingSettingState.Data>(awaitItem())
+            assertNotNull(dataState.alternativeRoutingSettingErrorEffect.consume())
+        }
+    }
+
+    @Test
+    fun `can emit multiple errors`() = runTest {
+        // Given
+        coEvery {
+            saveAlternativeRoutingSetting(alternativeRoutingPreference.isEnabled)
+        } returns PreferencesError.left()
+
+        // When
+        alternativeRoutingSettingViewModel.saveAlternativeRoutingPreference(alternativeRoutingPreference.isEnabled)
+
+        // Then
+        alternativeRoutingSettingViewModel.state.test {
+            val dataState = assertIs<AlternativeRoutingSettingState.Data>(awaitItem())
+            assertNotNull(dataState.alternativeRoutingSettingErrorEffect.consume())
+
+            alternativeRoutingSettingViewModel.saveAlternativeRoutingPreference(alternativeRoutingPreference.isEnabled)
+
+            val dataState2 = assertIs<AlternativeRoutingSettingState.Data>(awaitItem())
+            assertNotNull(dataState2.alternativeRoutingSettingErrorEffect.consume())
+        }
+    }
+}
