89944dc4a7b81c217ce0b7b397a0ca0784e9b0c8
Davide Farella <davide.farella@proton.ch>
2022-08-16 17:34:34 +0200

    Add ColorMapper.kt to map BE color strings to Compose colors
    MAILANDR-174

---
+++ mail-common/presentation/src/main/kotlin/ch/protonmail/android/mailcommon/presentation/mapper/ColorMapper.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcommon.presentation.mapper
+
+import androidx.compose.ui.graphics.Color
+import arrow.core.Validated
+import arrow.core.invalid
+import arrow.core.valid
+
+class ColorMapper {
+
+    fun toColor(string: String): Validated<String, Color> {
+        with(string.substringAfter("#")) {
+            val (a, r, g, b) = when (length) {
+                3 -> listOf(NoTransparency, substring(0, 1), substring(1, 2), substring(2, 3))
+                6 -> listOf(NoTransparency, substring(0, 2), substring(2, 4), substring(4, 6))
+                4 -> listOf(substring(0, 1), substring(1, 2), substring(2, 3), substring(3, 4))
+                8 -> listOf(substring(0, 2), substring(2, 4), substring(4, 6), substring(6, 8))
+                else -> return string.invalid()
+            }
+            return Color(
+                alpha = a.toColorInt(),
+                red = r.toColorInt(),
+                green = g.toColorInt(),
+                blue = b.toColorInt()
+            ).valid()
+        }
+    }
+
+    private fun String.toColorInt() = toInt(radix = 16)
+
+    companion object {
+
+        const val NoTransparency = "FF"
+    }
+}
---
+++ mail-common/presentation/src/test/kotlin/ch/protonmail/android/mailcommon/presentation/mapper/ColorMapperTest.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcommon.presentation.mapper
+
+import androidx.compose.ui.graphics.Color
+import arrow.core.Validated
+import arrow.core.invalid
+import arrow.core.valid
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import org.junit.runners.Parameterized.Parameters
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+@RunWith(Parameterized::class)
+internal class ColorMapperTest(
+    private val testName: String,
+    private val input: String,
+    private val expected: Validated<String, Color>
+) {
+
+    @Test
+    fun test() {
+        println("Running $testName, Input: $input, Expected: $expected")
+        assertEquals(expected, ColorMapper().toColor(input))
+    }
+
+    data class Params(
+        val testName: String,
+        val input: String,
+        val expected: Validated<String, Color>
+    )
+
+    companion object {
+
+        @JvmStatic
+        @Parameters(name = "{0}")
+        fun data() = listOf(
+
+            Params(
+                testName = "success from #aarrggbb string",
+                input = "#80000000",
+                expected = Color.Black.copy(alpha = 0.5f).valid()
+            ),
+
+            Params(
+                testName = "success from aarrggbb string",
+                input = "80000000",
+                expected = Color.Black.copy(alpha = 0.5f).valid()
+            ),
+
+            Params(
+                testName = "success from #rrggbb string",
+                input = "#000000",
+                expected = Color.Black.valid()
+            ),
+
+            Params(
+                testName = "success from rrggbb string",
+                input = "000000",
+                expected = Color.Black.valid()
+            ),
+
+            Params(
+                testName = "success from #rgb string",
+                input = "#000",
+                expected = Color.Black.valid()
+            ),
+
+            Params(
+                testName = "success from rgb string",
+                input = "000",
+                expected = Color.Black.valid()
+            ),
+
+            Params(
+                testName = "success for red color",
+                input = "ff0000",
+                expected = Color.Red.valid()
+            ),
+
+            Params(
+                testName = "success for green color",
+                input = "00ff00",
+                expected = Color.Green.valid()
+            ),
+
+            Params(
+                testName = "success for blue color",
+                input = "0000ff",
+                expected = Color.Blue.valid()
+            ),
+
+            Params(
+                testName = "from invalid string",
+                input = "invalid",
+                expected = "invalid".invalid()
+            )
+
+        ).map { arrayOf(it.testName, it.input, it.expected) }
+    }
+}
