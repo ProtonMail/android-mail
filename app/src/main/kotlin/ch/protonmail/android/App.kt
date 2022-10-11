a8a27202f51d31359837f356cc2c12bdef396d6c
Davide Farella <davide.farella@proton.ch>
2022-10-11 16:17:47 +0200

    Move initializers back into the Manifest (removed workaround)

--- app/src/main/kotlin/ch/protonmail/android/App.kt
+++ app/src/main/kotlin/ch/protonmail/android/App.kt
@@ -19,22 +19,7 @@
 package ch.protonmail.android
 
 import android.app.Application
-import androidx.startup.AppInitializer
-import ch.protonmail.android.initializer.AccountStateHandlerInitializer
-import ch.protonmail.android.initializer.SentryInitializer
-import ch.protonmail.android.initializer.strictmode.StrictModeInitializer
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class App : Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-
-        with(AppInitializer.getInstance(this)) {
-            initializeComponent(AccountStateHandlerInitializer::class.java)
-            initializeComponent(SentryInitializer::class.java)
-            initializeComponent(StrictModeInitializer::class.java)
-        }
-    }
-}
+class App : Application()
