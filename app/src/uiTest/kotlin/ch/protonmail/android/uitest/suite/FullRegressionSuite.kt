35798a9de90cdfa87c3cdbc88180a9cd45337db5
Marino Meneghel <marino.meneghel@protonmail.com>
2022-11-24 09:57:14 +0100

    Remove login and settings tests from smoke suite extended
    This suite runs on each merge to master. Given settings screen doesn't
    change very often, running such tests each time isn't worth the time it
    takes for them to execute.
    
    All of the existing tests will be run in nighly builds as part of
    FullRegressionSuite

--- app/src/uiTest/kotlin/ch/protonmail/android/uitest/suite/FullRegressionSuite.kt
+++ app/src/uiTest/kotlin/ch/protonmail/android/uitest/suite/FullRegressionSuite.kt
@@ -18,11 +18,42 @@
 
 package ch.protonmail.android.uitest.suite
 
+import ch.protonmail.android.uitest.e2e.login.LoginFlowTests
+import ch.protonmail.android.uitest.e2e.mailbox.MailboxFlowTest
+import ch.protonmail.android.uitest.e2e.settings.SettingsFlowTest
+import ch.protonmail.android.uitest.screen.common.BottomActionBarTest
+import ch.protonmail.android.uitest.screen.mailbox.MailboxItemLabelsTest
+import ch.protonmail.android.uitest.screen.mailbox.MailboxScreenTest
+import ch.protonmail.android.uitest.screen.mailbox.MailboxTopAppBarTest
+import ch.protonmail.android.uitest.screen.settings.account.AccountSettingsScreenTest
+import ch.protonmail.android.uitest.screen.settings.account.conversationmode.ConversationModeSettingScreenTest
+import ch.protonmail.android.uitest.screen.settings.appsettings.SettingsScreenTest
+import ch.protonmail.android.uitest.screen.settings.appsettings.alternativerouting.AlternativeRoutingSettingScreenTest
+import ch.protonmail.android.uitest.screen.settings.appsettings.combinedcontacts.CombinedContactsSettingScreenTest
+import ch.protonmail.android.uitest.screen.settings.appsettings.swipeactions.EditSwipeActionPreferenceScreenTest
+import ch.protonmail.android.uitest.screen.settings.appsettings.swipeactions.SwipeActionsPreferenceScreenTest
+import ch.protonmail.android.uitest.screen.settings.appsettings.theme.ThemeSettingScreenTest
+import ch.protonmail.android.uitest.screen.sidebar.SidebarScreenTest
 import org.junit.runner.RunWith
 import org.junit.runners.Suite
 
 @RunWith(Suite::class)
 @Suite.SuiteClasses(
-    SmokeSuiteExtended::class
+    AccountSettingsScreenTest::class,
+    AlternativeRoutingSettingScreenTest::class,
+    BottomActionBarTest::class,
+    CombinedContactsSettingScreenTest::class,
+    ConversationModeSettingScreenTest::class,
+    EditSwipeActionPreferenceScreenTest::class,
+    LoginFlowTests::class,
+    MailboxFlowTest::class,
+    MailboxItemLabelsTest::class,
+    MailboxScreenTest::class,
+    MailboxTopAppBarTest::class,
+    SettingsFlowTest::class,
+    SettingsScreenTest::class,
+    SidebarScreenTest::class,
+    SwipeActionsPreferenceScreenTest::class,
+    ThemeSettingScreenTest::class
 )
 class FullRegressionSuite
