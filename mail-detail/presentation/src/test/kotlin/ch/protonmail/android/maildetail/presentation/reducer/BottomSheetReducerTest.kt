3fb87206476e9b233b3ccfc4ed5b213be020534e
Johannes Prueller <johannes.prueller@proton.ch>
2022-12-05 15:53:08 +0100

    Fix test issues
    MAILANDR-260

--- mail-detail/presentation/src/test/kotlin/ch/protonmail/android/maildetail/presentation/reducer/BottomSheetReducerTest.kt
+++ mail-detail/presentation/src/test/kotlin/ch/protonmail/android/maildetail/presentation/reducer/BottomSheetReducerTest.kt
@@ -65,6 +65,11 @@ internal class BottomSheetReducerTest(
                 currentState = BottomSheetState.Data(destinations),
                 operation = BottomSheetEvent.Data(updatedDestinations),
                 expectedState = BottomSheetState.Data(updatedDestinations)
+            ),
+            TestInput(
+                currentState = BottomSheetState.Data(updatedDestinations),
+                operation = BottomSheetAction.BottomSheetDismissed,
+                expectedState = BottomSheetState.Data(destinations)
             )
         )
 
