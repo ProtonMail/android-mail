336995da35ffbccbdc82080a27033842f3468164
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2022-12-30 16:56:37 +0100

    Create MessageBodyDao.kt
    MAILANDR-303

---
+++ mail-message/data/src/main/kotlin/ch/protonmail/android/mailmessage/data/local/dao/MessageBodyDao.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailmessage.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import ch.protonmail.android.mailmessage.data.local.entity.MessageBodyEntity
+import ch.protonmail.android.mailmessage.data.local.relation.MessageWithBodyRelation
+import ch.protonmail.android.mailmessage.domain.entity.MessageId
+import kotlinx.coroutines.flow.Flow
+import me.proton.core.data.room.db.BaseDao
+import me.proton.core.domain.entity.UserId
+
+@Dao
+abstract class MessageBodyDao : BaseDao<MessageBodyEntity>() {
+
+    @Query(
+        """
+            SELECT * FROM MessageEntity
+            JOIN MessageBodyEntity
+            ON MessageBodyEntity.userId = MessageEntity.userId
+            AND MessageBodyEntity.messageId = MessageEntity.messageId
+            JOIN MessageLabelEntity
+            ON MessageLabelEntity.userId = MessageEntity.userId
+            AND MessageLabelEntity.messageId = MessageEntity.messageId
+            WHERE MessageEntity.userId = :userId
+            AND MessageEntity.messageId = :messageId
+        """
+    )
+    abstract fun observeMessageWithBodyRelation(
+        userId: UserId,
+        messageId: MessageId
+    ): Flow<MessageWithBodyRelation?>
+}
