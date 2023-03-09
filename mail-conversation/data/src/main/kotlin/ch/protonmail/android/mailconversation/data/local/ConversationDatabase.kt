4a5f7f8d202963ea603326b9ae75d314b404d4c5
Johannes Prueller <johannes.prueller@proton.ch>
2023-03-09 13:19:11 +0100

    Remove database migrations
    MAILANDR-199

--- mail-conversation/data/src/main/kotlin/ch/protonmail/android/mailconversation/data/local/ConversationDatabase.kt
+++ mail-conversation/data/src/main/kotlin/ch/protonmail/android/mailconversation/data/local/ConversationDatabase.kt
@@ -18,47 +18,13 @@
 
 package ch.protonmail.android.mailconversation.data.local
 
-import androidx.sqlite.db.SupportSQLiteDatabase
 import ch.protonmail.android.mailconversation.data.local.dao.ConversationDao
 import ch.protonmail.android.mailconversation.data.local.dao.ConversationLabelDao
-import ch.protonmail.android.mailmessage.data.local.entity.AttachmentCountEntity
 import ch.protonmail.android.mailpagination.data.local.PageIntervalDatabase
 import me.proton.core.data.room.db.Database
-import me.proton.core.data.room.db.extension.addTableColumn
-import me.proton.core.data.room.db.migration.DatabaseMigration
-import me.proton.core.util.kotlin.serialize
 
 @Suppress("MaxLineLength")
 interface ConversationDatabase : Database, PageIntervalDatabase {
     fun conversationDao(): ConversationDao
     fun conversationLabelDao(): ConversationLabelDao
-
-    companion object {
-        val MIGRATION_0 = object : DatabaseMigration {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                // Added ConversationEntity.
-                database.execSQL("CREATE TABLE IF NOT EXISTS `ConversationEntity` (`userId` TEXT NOT NULL, `conversationId` TEXT NOT NULL, `order` INTEGER NOT NULL, `subject` TEXT NOT NULL, `senders` TEXT NOT NULL, `recipients` TEXT NOT NULL, `expirationTime` INTEGER NOT NULL, `numMessages` INTEGER NOT NULL, `numUnread` INTEGER NOT NULL, `numAttachments` INTEGER NOT NULL, PRIMARY KEY(`userId`, `conversationId`), FOREIGN KEY(`userId`) REFERENCES `UserEntity`(`userId`) ON UPDATE NO ACTION ON DELETE CASCADE )")
-                database.execSQL("CREATE INDEX IF NOT EXISTS `index_ConversationEntity_userId` ON `ConversationEntity` (`userId`)")
-                database.execSQL("CREATE INDEX IF NOT EXISTS `index_ConversationEntity_conversationId` ON `ConversationEntity` (`conversationId`)")
-
-                // Added ConversationLabelEntity.
-                database.execSQL("CREATE TABLE IF NOT EXISTS `ConversationLabelEntity` (`userId` TEXT NOT NULL, `conversationId` TEXT NOT NULL, `labelId` TEXT NOT NULL, `contextTime` INTEGER NOT NULL, `contextSize` INTEGER NOT NULL, `contextNumMessages` INTEGER NOT NULL, `contextNumUnread` INTEGER NOT NULL, `contextNumAttachments` INTEGER NOT NULL, PRIMARY KEY(`userId`, `conversationId`, `labelId`), FOREIGN KEY(`userId`) REFERENCES `UserEntity`(`userId`) ON UPDATE NO ACTION ON DELETE CASCADE , FOREIGN KEY(`userId`, `conversationId`) REFERENCES `ConversationEntity`(`userId`, `conversationId`) ON UPDATE NO ACTION ON DELETE CASCADE )")
-                database.execSQL("CREATE INDEX IF NOT EXISTS `index_ConversationLabelEntity_userId` ON `ConversationLabelEntity` (`userId`)")
-                database.execSQL("CREATE INDEX IF NOT EXISTS `index_ConversationLabelEntity_labelId` ON `ConversationLabelEntity` (`labelId`)")
-                database.execSQL("CREATE INDEX IF NOT EXISTS `index_ConversationLabelEntity_conversationId` ON `ConversationLabelEntity` (`conversationId`)")
-                database.execSQL("CREATE INDEX IF NOT EXISTS `index_ConversationLabelEntity_userId_conversationId` ON `ConversationLabelEntity` (`userId`, `conversationId`)")
-            }
-        }
-
-        val MIGRATION_1 = object : DatabaseMigration {
-            override fun migrate(database: SupportSQLiteDatabase) {
-                database.addTableColumn(
-                    table = "ConversationEntity",
-                    column = "attachmentCount",
-                    type = "TEXT NOT NULL",
-                    defaultValue = AttachmentCountEntity(0).serialize()
-                )
-            }
-        }
-    }
 }
--- mail-pagination/data/src/main/kotlin/ch/protonmail/android/mailpagination/data/local/PageIntervalDatabase.kt
+++ mail-pagination/data/src/main/kotlin/ch/protonmail/android/mailpagination/data/local/PageIntervalDatabase.kt
@@ -18,81 +18,11 @@
 
 package ch.protonmail.android.mailpagination.data.local
 
-import androidx.sqlite.db.SupportSQLiteDatabase
 import ch.protonmail.android.mailpagination.data.local.dao.PageIntervalDao
 import me.proton.core.data.room.db.Database
-import me.proton.core.data.room.db.migration.DatabaseMigration
 
 interface PageIntervalDatabase : Database {
 
     fun pageIntervalDao(): PageIntervalDao
 
-    companion object {
-
-        val MIGRATION_0 = object : DatabaseMigration {
-            @Suppress("LongMethod")
-            override fun migrate(database: SupportSQLiteDatabase) {
-                // Added PageIntervalEntity.
-                database.execSQL(
-                    """CREATE TABLE IF NOT EXISTS `PageIntervalEntity` (
-                            `userId` TEXT NOT NULL, 
-                            `type` TEXT NOT NULL, 
-                            `orderBy` TEXT NOT NULL, 
-                            `labelId` TEXT NOT NULL, 
-                            `keyword` TEXT NOT NULL, 
-                            `read` TEXT NOT NULL, 
-                            `minValue` INTEGER NOT NULL,
-                            `maxValue` INTEGER NOT NULL,
-                            `minOrder` INTEGER NOT NULL, 
-                            `maxOrder` INTEGER NOT NULL,
-                            `minId` TEXT,
-                            `maxId` TEXT, 
-                            PRIMARY KEY(
-                                `userId`,
-                                `type`,
-                                `orderBy`,
-                                `labelId`,
-                                `keyword`,
-                                `read`,
-                                `minValue`,
-                                `maxValue`
-                            ),
-                            FOREIGN KEY(`userId`) REFERENCES `UserEntity`(`userId`) 
-                                ON UPDATE NO ACTION ON DELETE CASCADE
-                        )
-                    """.trimIndent()
-                )
-                database.execSQL(
-                    """CREATE INDEX IF NOT EXISTS `index_PageIntervalEntity_userId`
-                            ON `PageIntervalEntity` (`userId`)
-                    """.trimIndent()
-                )
-                database.execSQL(
-                    """CREATE INDEX IF NOT EXISTS `index_PageIntervalEntity_type` 
-                        ON `PageIntervalEntity` (`type`)
-                    """.trimIndent()
-                )
-                database.execSQL(
-                    """CREATE INDEX IF NOT EXISTS `index_PageIntervalEntity_minValue` 
-                        ON `PageIntervalEntity` (`minValue`)
-                    """.trimIndent()
-                )
-                database.execSQL(
-                    """CREATE INDEX IF NOT EXISTS `index_PageIntervalEntity_maxValue` 
-                        ON `PageIntervalEntity` (`maxValue`)
-                    """.trimIndent()
-                )
-                database.execSQL(
-                    """CREATE INDEX IF NOT EXISTS `index_PageIntervalEntity_minOrder` 
-                        ON `PageIntervalEntity` (`minOrder`)
-                    """.trimIndent()
-                )
-                database.execSQL(
-                    """CREATE INDEX IF NOT EXISTS `index_PageIntervalEntity_maxOrder` 
-                        ON `PageIntervalEntity` (`maxOrder`)
-                    """.trimIndent()
-                )
-            }
-        }
-    }
 }
