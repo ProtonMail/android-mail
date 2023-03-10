b4076a141667a73ef7e81552de3f092e90f2dae6
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2023-03-10 18:40:03 +0100

    Use the MIME type from the decrypted message body data instead of the MIME type from the message
    The MIME type for PGP/MIME messages will be 'multipart/mixed' before decrypting it.
    After we decrypt the body, we can see the MIME type of the decrypted data,
    which we need in order to properly display the body.
    
    MAILANDR-435

--- mail-detail/domain/src/main/kotlin/ch/protonmail/android/maildetail/domain/usecase/GetDecryptedMessageBody.kt
+++ mail-detail/domain/src/main/kotlin/ch/protonmail/android/maildetail/domain/usecase/GetDecryptedMessageBody.kt
@@ -75,7 +75,7 @@ class GetDecryptedMessageBody @Inject constructor(
     private fun KeyHolderContext.decryptMessageBody(messageBody: MessageBody): DecryptedMessageBody {
         return if (messageBody.mimeType == MimeType.MultipartMixed) {
             decryptMimeMessage(messageBody.body).run {
-                DecryptedMessageBody(body.content, messageBody.mimeType)
+                DecryptedMessageBody(body.content, MimeType.from(body.mimeType))
             }
         } else {
             decryptText(messageBody.body).run {
