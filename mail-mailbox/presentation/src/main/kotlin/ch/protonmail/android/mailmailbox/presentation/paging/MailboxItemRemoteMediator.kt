3be045215983ace1224074055288fb860c842c57
Johannes Prueller <johannes.prueller@proton.ch>
2023-04-11 17:42:46 +0200

    Add remote mediator and tests
    MAILANDR-410

---
+++ mail-mailbox/presentation/src/main/kotlin/ch/protonmail/android/mailmailbox/presentation/paging/MailboxItemRemoteMediator.kt
@@ -0,0 +1,126 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailmailbox.presentation.paging
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import ch.protonmail.android.mailconversation.domain.repository.ConversationRepository
+import ch.protonmail.android.mailmailbox.domain.model.MailboxItem
+import ch.protonmail.android.mailmailbox.domain.model.MailboxItemType
+import ch.protonmail.android.mailmailbox.domain.model.MailboxPageKey
+import ch.protonmail.android.mailmessage.domain.repository.MessageRepository
+import ch.protonmail.android.mailpagination.domain.AdjacentPageKeys
+import ch.protonmail.android.mailpagination.domain.GetAdjacentPageKeys
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedFactory
+import dagger.assisted.AssistedInject
+import me.proton.core.domain.entity.UserId
+import timber.log.Timber
+
+@AssistedFactory
+interface MailboxItemRemoteMediatorFactory {
+
+    fun create(
+        mailboxPageKey: MailboxPageKey,
+        type: MailboxItemType
+    ): MailboxItemRemoteMediator
+}
+
+@OptIn(ExperimentalPagingApi::class)
+class MailboxItemRemoteMediator @AssistedInject constructor(
+    private val messageRepository: MessageRepository,
+    private val conversationRepository: ConversationRepository,
+    private val getAdjacentPageKeys: GetAdjacentPageKeys,
+    @Assisted private val mailboxPageKey: MailboxPageKey,
+    @Assisted private val type: MailboxItemType
+) : RemoteMediator<MailboxPageKey, MailboxItem>() {
+
+    override suspend fun initialize() = InitializeAction.LAUNCH_INITIAL_REFRESH
+
+    override suspend fun load(loadType: LoadType, state: PagingState<MailboxPageKey, MailboxItem>): MediatorResult {
+        val userId = mailboxPageKey.userIds.first()
+
+        if (loadType == LoadType.REFRESH) {
+            Timber.d("Paging: fetchItems -> markAsStale")
+            when (type) {
+                MailboxItemType.Message -> messageRepository.markAsStale(userId, mailboxPageKey.pageKey.filter.labelId)
+                MailboxItemType.Conversation -> conversationRepository.markAsStale(
+                    userId, mailboxPageKey.pageKey.filter.labelId
+                )
+            }
+        }
+
+        Timber.d("Paging: fetchItems: $loadType")
+        val prevKey = state.pages.firstOrNull { it.data.isNotEmpty() }?.prevKey?.pageKey
+        val nextKey = state.pages.lastOrNull { it.data.isNotEmpty() }?.nextKey?.pageKey
+        Timber.d("Paging: fetchItems: prevKey: $prevKey")
+        Timber.d("Paging: fetchItems: nextKey: $nextKey")
+
+        val pageKey = when (loadType) {
+            LoadType.REFRESH -> mailboxPageKey.pageKey.copy(size = state.config.initialLoadSize)
+            LoadType.PREPEND -> prevKey ?: getAdjacentPageKeys(state).prev
+            LoadType.APPEND -> nextKey ?: getAdjacentPageKeys(state).next
+        }.let { mailboxPageKey.copy(pageKey = it) }
+
+        Timber.d("Paging: fetchItems -> ${pageKey.pageKey}")
+
+        return when (type) {
+            MailboxItemType.Message -> fetchMessages(userId, pageKey)
+            MailboxItemType.Conversation -> fetchConversations(userId, pageKey)
+        }
+    }
+
+    private fun getAdjacentPageKeys(state: PagingState<MailboxPageKey, MailboxItem>): AdjacentPageKeys {
+        val items = state.pages.flatMap { it.data }
+        return getAdjacentPageKeys(items, mailboxPageKey.pageKey, state.config.pageSize)
+    }
+
+    private suspend fun fetchMessages(userId: UserId, pageKey: MailboxPageKey): MediatorResult {
+        return messageRepository.fetchMessages(
+            userId = userId,
+            pageKey = pageKey.pageKey,
+        ).fold(
+            ifRight = {
+                Timber.d("Paging: endOfPaginationReached: ${it.size}")
+                MediatorResult.Success(endOfPaginationReached = it.isEmpty())
+            },
+            ifLeft = {
+                Timber.d("Paging: fetchItems: Mediator failed to load messages: $it")
+                MediatorResult.Error(Exception(it.toString()))
+            }
+        )
+    }
+
+    private suspend fun fetchConversations(userId: UserId, pageKey: MailboxPageKey): MediatorResult {
+        return conversationRepository.fetchConversations(
+            userId = userId,
+            pageKey = pageKey.pageKey,
+        ).fold(
+            ifRight = {
+                MediatorResult.Success(endOfPaginationReached = it.isEmpty())
+            },
+            ifLeft = {
+                Timber.d("Paging: Mediator failed to load conversations: $it")
+                MediatorResult.Error(Exception(it.toString()))
+            }
+        )
+    }
+}
---
+++ mail-mailbox/presentation/src/test/kotlin/ch/protonmail/android/mailmailbox/presentation/paging/MailboxItemRemoteMediatorTest.kt
@@ -0,0 +1,306 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailmailbox.presentation.paging
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingConfig
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcommon.domain.model.NetworkError
+import ch.protonmail.android.mailcommon.domain.sample.UserIdSample
+import ch.protonmail.android.mailconversation.domain.repository.ConversationRepository
+import ch.protonmail.android.mailmailbox.domain.model.MailboxItem
+import ch.protonmail.android.mailmailbox.domain.model.MailboxItemType
+import ch.protonmail.android.mailmailbox.domain.model.MailboxPageKey
+import ch.protonmail.android.mailmessage.domain.repository.MessageRepository
+import ch.protonmail.android.mailpagination.domain.AdjacentPageKeys
+import ch.protonmail.android.mailpagination.domain.GetAdjacentPageKeys
+import ch.protonmail.android.mailpagination.domain.model.OrderDirection
+import ch.protonmail.android.mailpagination.domain.model.PageKey
+import ch.protonmail.android.testdata.conversation.ConversationWithContextTestData
+import ch.protonmail.android.testdata.mailbox.MailboxTestData
+import ch.protonmail.android.testdata.message.MessageTestData
+import io.mockk.Called
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertIs
+
+@OptIn(ExperimentalPagingApi::class)
+class MailboxItemRemoteMediatorTest {
+
+    private val userId = UserIdSample.Primary
+    private val mailboxPageKey = MailboxPageKey(
+        userIds = listOf(userId),
+        pageKey = PageKey()
+    )
+    private var mailboxItemType = MailboxItemType.Message
+
+    private val messageRepository = mockk<MessageRepository>(relaxUnitFun = true)
+    private val conversationRepository = mockk<ConversationRepository>(relaxUnitFun = true)
+    private val getAdjacentPageKeys = mockk<GetAdjacentPageKeys>()
+
+    private val mailboxItemRemoteMediator by lazy {
+        MailboxItemRemoteMediator(
+            messageRepository = messageRepository,
+            conversationRepository = conversationRepository,
+            getAdjacentPageKeys = getAdjacentPageKeys,
+            mailboxPageKey = mailboxPageKey,
+            type = mailboxItemType
+        )
+    }
+
+    @Test
+    fun `returns success when mediator is called with refresh for message, then data is marked as stale`() =
+        runTest {
+            // Given
+            coEvery {
+                messageRepository.fetchMessages(userId, mailboxPageKey.pageKey)
+            } returns listOf(MessageTestData.message).right()
+
+            // When
+            val result = mailboxItemRemoteMediator.load(
+                loadType = LoadType.REFRESH,
+                state = buildPagingState()
+            )
+
+            // Then
+            coVerify { messageRepository.markAsStale(userId, mailboxPageKey.pageKey.filter.labelId) }
+            coVerify(exactly = 0) { conversationRepository.markAsStale(any(), any()) }
+            assertIs<RemoteMediator.MediatorResult.Success>(result)
+            assertFalse(result.endOfPaginationReached)
+        }
+
+    @Test
+    fun `returns success when mediator is called with refresh for conversation, then data is marked as stale`() =
+        runTest {
+            // Given
+            mailboxItemType = MailboxItemType.Conversation
+            coEvery {
+                conversationRepository.fetchConversations(userId, mailboxPageKey.pageKey)
+            } returns listOf(ConversationWithContextTestData.conversation1).right()
+
+            // When
+            val result = mailboxItemRemoteMediator.load(
+                loadType = LoadType.REFRESH,
+                state = buildPagingState()
+            )
+
+            // Then
+            coVerify { conversationRepository.markAsStale(userId, mailboxPageKey.pageKey.filter.labelId) }
+            coVerify(exactly = 0) { messageRepository.markAsStale(any(), any()) }
+            assertIs<RemoteMediator.MediatorResult.Success>(result)
+            assertFalse(result.endOfPaginationReached)
+        }
+
+    @Test
+    fun `given the mediator is called with append, then data is not marked as stale`() =
+        runTest {
+            // Given
+            val pageKey = mailboxPageKey.pageKey.copy(
+                orderDirection = OrderDirection.Ascending
+            )
+            val pages = emptyList<PagingSource.LoadResult.Page<MailboxPageKey, MailboxItem>>()
+            val items = pages.flatMap { it.data }
+            val pageSize = PageKey.defaultPageSize
+            coEvery { getAdjacentPageKeys(items, mailboxPageKey.pageKey, pageSize) } returns AdjacentPageKeys(
+                prev = PageKey(),
+                current = mailboxPageKey.pageKey,
+                next = pageKey
+            )
+            coEvery { messageRepository.fetchMessages(userId, pageKey) } returns listOf(MessageTestData.message).right()
+
+            // When
+            mailboxItemRemoteMediator.load(
+                loadType = LoadType.APPEND,
+                state = buildPagingState(pages, pageSize)
+            )
+
+            // Then
+            coVerify(exactly = 0) { messageRepository.markAsStale(any(), any()) }
+            coVerify { messageRepository.fetchMessages(userId, pageKey) }
+        }
+
+    @Test
+    fun `returns error when mediator is called with append and message api call fails`() =
+        runTest {
+            // Given
+            val expectedRemoteError = DataError.Remote.Http(NetworkError.Unreachable)
+            val pageKey = mailboxPageKey.pageKey.copy(
+                orderDirection = OrderDirection.Ascending
+            )
+            val pages = emptyList<PagingSource.LoadResult.Page<MailboxPageKey, MailboxItem>>()
+            val items = pages.flatMap { it.data }
+            val pageSize = PageKey.defaultPageSize
+            coEvery { getAdjacentPageKeys(items, mailboxPageKey.pageKey, pageSize) } returns AdjacentPageKeys(
+                prev = PageKey(),
+                current = mailboxPageKey.pageKey,
+                next = pageKey
+            )
+            coEvery {
+                messageRepository.fetchMessages(userId, pageKey)
+            } returns expectedRemoteError.left()
+
+            // When
+            val result = mailboxItemRemoteMediator.load(
+                loadType = LoadType.APPEND,
+                state = buildPagingState(pages, pageSize)
+            )
+
+            // Then
+            coVerify(exactly = 0) { messageRepository.markAsStale(any(), any()) }
+            coVerify { messageRepository.fetchMessages(userId, pageKey) }
+            assertIs<RemoteMediator.MediatorResult.Error>(result)
+            assertEquals(expectedRemoteError.toString(), result.throwable.message)
+        }
+
+    @Test
+    fun `returns error when mediator is called with append and conversation api call fails`() =
+        runTest {
+            // Given
+            val expectedRemoteError = DataError.Remote.Http(NetworkError.Unreachable)
+            val pageKey = mailboxPageKey.pageKey.copy(
+                orderDirection = OrderDirection.Ascending
+            )
+            val pages = emptyList<PagingSource.LoadResult.Page<MailboxPageKey, MailboxItem>>()
+            val items = pages.flatMap { it.data }
+            val pageSize = PageKey.defaultPageSize
+            coEvery { getAdjacentPageKeys(items, mailboxPageKey.pageKey, pageSize) } returns AdjacentPageKeys(
+                prev = PageKey(),
+                current = mailboxPageKey.pageKey,
+                next = pageKey
+            )
+            mailboxItemType = MailboxItemType.Conversation
+            coEvery {
+                conversationRepository.fetchConversations(userId, pageKey)
+            } returns expectedRemoteError.left()
+
+            // When
+            val result = mailboxItemRemoteMediator.load(
+                loadType = LoadType.APPEND,
+                state = buildPagingState(pages, pageSize)
+            )
+
+            // Then
+            coVerify(exactly = 0) { conversationRepository.markAsStale(any(), any()) }
+            coVerify { conversationRepository.fetchConversations(userId, pageKey) }
+            assertIs<RemoteMediator.MediatorResult.Error>(result)
+            assertEquals(expectedRemoteError.toString(), result.throwable.message)
+        }
+
+    @Test
+    fun `given the remote mediator is called with append and and state contains next key, then this key is used`() =
+        runTest {
+            // Given
+            val nextKey = mailboxPageKey.pageKey.copy(
+                orderDirection = OrderDirection.Ascending
+            )
+            val pages = listOf(
+                PagingSource.LoadResult.Page(
+                    data = listOf(
+                        MailboxTestData.readMailboxItem,
+                        MailboxTestData.unreadMailboxItem
+                    ),
+                    prevKey = null,
+                    nextKey = MailboxPageKey(
+                        pageKey = nextKey,
+                        userIds = listOf()
+                    )
+                )
+            )
+            val pageSize = PageKey.defaultPageSize
+            mailboxItemType = MailboxItemType.Conversation
+            coEvery {
+                conversationRepository.fetchConversations(userId, nextKey)
+            } returns listOf(ConversationWithContextTestData.conversation1).right()
+
+            // When
+            val result = mailboxItemRemoteMediator.load(
+                loadType = LoadType.APPEND,
+                state = buildPagingState(pages, pageSize)
+            )
+
+            // Then
+            coVerify { conversationRepository.fetchConversations(userId, nextKey) }
+            coVerify { getAdjacentPageKeys wasNot Called }
+            assertIs<RemoteMediator.MediatorResult.Success>(result)
+        }
+
+    @Test
+    fun `given the remote mediator is called with prepend and and state contains prev key, then this key is used`() =
+        runTest {
+            // Given
+            val prevKey = mailboxPageKey.pageKey.copy(
+                orderDirection = OrderDirection.Ascending
+            )
+            val pages = listOf(
+                PagingSource.LoadResult.Page(
+                    data = listOf(
+                        MailboxTestData.readMailboxItem,
+                        MailboxTestData.unreadMailboxItem
+                    ),
+                    prevKey = MailboxPageKey(
+                        pageKey = prevKey,
+                        userIds = listOf()
+                    ),
+                    nextKey = null
+                )
+            )
+            val pageSize = PageKey.defaultPageSize
+            mailboxItemType = MailboxItemType.Conversation
+            coEvery {
+                conversationRepository.fetchConversations(userId, prevKey)
+            } returns listOf(ConversationWithContextTestData.conversation1).right()
+
+            // When
+            val result = mailboxItemRemoteMediator.load(
+                loadType = LoadType.PREPEND,
+                state = buildPagingState(pages, pageSize)
+            )
+
+            // Then
+            coVerify { conversationRepository.fetchConversations(userId, prevKey) }
+            coVerify { getAdjacentPageKeys wasNot Called }
+            assertIs<RemoteMediator.MediatorResult.Success>(result)
+        }
+
+    private fun buildPagingState(
+        pages: List<PagingSource.LoadResult.Page<MailboxPageKey, MailboxItem>> = emptyList(),
+        pageSize: Int = PageKey.defaultPageSize
+    ) = PagingState(
+        pages = pages,
+        config = PagingConfig(
+            pageSize = pageSize,
+            enablePlaceholders = false,
+            initialLoadSize = pageSize,
+            prefetchDistance = 1
+        ),
+        anchorPosition = 0,
+        leadingPlaceholderCount = 0
+    )
+}
