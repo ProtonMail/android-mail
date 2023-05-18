90620397e9dcb4ba67f943e21a6571e5eb753e4f
Niccolò Forlini <niccolo.forlini@proton.ch>
2023-05-18 09:57:23 +0200

    Add UI test 183527
    Regression suite for MailboxSwitchTests.
    
    MAILANDR-567

---
+++ app/src/uiTest/kotlin/ch/protonmail/android/uitest/e2e/mailbox/MailboxSwitchTests.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.uitest.e2e.mailbox
+
+import ch.protonmail.android.di.ServerProofModule
+import ch.protonmail.android.networkmocks.mockwebserver.requests.respondWith
+import ch.protonmail.android.networkmocks.mockwebserver.requests.serveOnce
+import ch.protonmail.android.networkmocks.mockwebserver.requests.withStatusCode
+import ch.protonmail.android.test.annotations.suite.RegressionTest
+import ch.protonmail.android.uitest.MockedNetworkTest
+import ch.protonmail.android.uitest.helpers.core.TestId
+import ch.protonmail.android.uitest.helpers.core.navigation.Destination
+import ch.protonmail.android.uitest.helpers.core.navigation.navigator
+import ch.protonmail.android.uitest.helpers.login.LoginStrategy
+import ch.protonmail.android.uitest.helpers.network.mockNetworkDispatcher
+import ch.protonmail.android.uitest.models.avatar.AvatarInitial
+import ch.protonmail.android.uitest.models.mailbox.MailboxListItemEntry
+import ch.protonmail.android.uitest.robot.mailbox.inbox.InboxRobot
+import ch.protonmail.android.uitest.robot.menu.MenuRobot
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import io.mockk.mockk
+import me.proton.core.auth.domain.usecase.ValidateServerProof
+import org.junit.Test
+
+@RegressionTest
+@HiltAndroidTest
+@UninstallModules(ServerProofModule::class)
+internal class MailboxSwitchTests : MockedNetworkTest(loginStrategy = LoginStrategy.LoggedOut) {
+
+    private val inboxRobot = InboxRobot(composeTestRule)
+    private val menuRobot = MenuRobot(composeTestRule)
+
+    @JvmField
+    @BindValue
+    val serverProofValidation: ValidateServerProof = mockk(relaxUnitFun = true)
+
+    private val expectedFirstSentItem = MailboxListItemEntry(
+        index = 0,
+        avatarInitial = AvatarInitial.WithText("M"),
+        participants = "Mobile Apps UI Testing 2",
+        subject = "Test message TOP",
+        date = "Apr 3, 2023"
+    )
+
+    @Test
+    @TestId("183527")
+    fun checkMailboxSwitchConversationToMessageMode() {
+        mockWebServer.dispatcher = mockNetworkDispatcher(useDefaultMailSettings = false) {
+            addMockRequests(
+                "/mail/v4/settings"
+                    respondWith "/mail/v4/settings/mail-v4-settings_183527.json"
+                    withStatusCode 200,
+                "/mail/v4/conversations?Page=0&PageSize=75&Limit=75&LabelID=0&Sort=Time&Desc=1"
+                    respondWith "/mail/v4/conversations/conversations_183527.json"
+                    withStatusCode 200 serveOnce true,
+                "/mail/v4/messages?Page=0&PageSize=75&Limit=75&LabelID=7&Sort=Time&Desc=1"
+                    respondWith "/mail/v4/messages/messages_183527.json"
+                    withStatusCode 200 serveOnce true
+            )
+        }
+
+        navigator {
+            navigateTo(Destination.Inbox)
+        }
+
+        inboxRobot.scrollToBottom()
+
+        menuRobot
+            .swipeOpenSidebarMenu()
+            .openSent()
+            .verify { listItemsAreShown(expectedFirstSentItem) }
+    }
+}
