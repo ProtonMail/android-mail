6bf25af16f522c0c2cd34dd908b8cd7634fb416b
Niccolò Forlini <niccolo.forlini@proton.ch>
2023-06-19 15:50:09 +0200

    Implement PTR tests for Mailbox
    From 188892 to 188897 and 188899 to 188904
    
    MAILANDR-591

---
+++ app/src/uiTest/kotlin/ch/protonmail/android/uitest/e2e/mailbox/errors/ConversationModeMailboxPullToRefreshErrorTests.kt
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.uitest.e2e.mailbox.errors
+
+import ch.protonmail.android.di.ServerProofModule
+import ch.protonmail.android.networkmocks.mockwebserver.requests.ignoreQueryParams
+import ch.protonmail.android.networkmocks.mockwebserver.requests.respondWith
+import ch.protonmail.android.networkmocks.mockwebserver.requests.serveOnce
+import ch.protonmail.android.networkmocks.mockwebserver.requests.withNetworkDelay
+import ch.protonmail.android.networkmocks.mockwebserver.requests.withStatusCode
+import ch.protonmail.android.test.annotations.suite.RegressionTest
+import ch.protonmail.android.test.annotations.suite.SmokeTest
+import ch.protonmail.android.uitest.MockedNetworkTest
+import ch.protonmail.android.uitest.helpers.core.TestId
+import ch.protonmail.android.uitest.helpers.core.navigation.Destination
+import ch.protonmail.android.uitest.helpers.core.navigation.navigator
+import ch.protonmail.android.uitest.helpers.login.LoginStrategy
+import ch.protonmail.android.uitest.helpers.network.mockNetworkDispatcher
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import io.mockk.mockk
+import me.proton.core.auth.domain.usecase.ValidateServerProof
+import org.junit.Test
+
+@RegressionTest
+@HiltAndroidTest
+@UninstallModules(ServerProofModule::class)
+internal class ConversationModeMailboxPullToRefreshErrorTests :
+    MockedNetworkTest(loginStrategy = LoginStrategy.LoggedOut), MailboxPullToRefreshErrorTests {
+
+    @JvmField
+    @BindValue
+    val serverProofValidation: ValidateServerProof = mockk(relaxUnitFun = true)
+
+    @Test
+    @TestId("188892")
+    fun checkConversationsLoadingErrorToError() {
+        mockWebServer.dispatcher = mockNetworkDispatcher(useDefaultMailSettings = false) {
+            addMockRequests(
+                "/mail/v4/settings"
+                    respondWith "/mail/v4/settings/mail-v4-settings_placeholder_conversation.json"
+                    withStatusCode 200,
+                "/mail/v4/conversations"
+                    respondWith "/global/errors/error_mock.json"
+                    withStatusCode 503 ignoreQueryParams true serveOnce true,
+                "/mail/v4/conversations"
+                    respondWith "/global/errors/error_mock.json"
+                    withStatusCode 503 ignoreQueryParams true withNetworkDelay 2000 serveOnce true
+            )
+        }
+
+        navigator {
+            navigateTo(Destination.Inbox)
+        }
+
+        verifyErrorToError()
+    }
+
+    @Test
+    @TestId("188893")
+    fun checkConversationsLoadingContentToError() {
+        mockWebServer.dispatcher = mockNetworkDispatcher(useDefaultMailSettings = false) {
+            addMockRequests(
+                "/mail/v4/settings"
+                    respondWith "/mail/v4/settings/mail-v4-settings_placeholder_conversation.json"
+                    withStatusCode 200,
+                "/mail/v4/conversations"
+                    respondWith "/mail/v4/conversations/conversations_base_placeholder.json"
+                    withStatusCode 200 ignoreQueryParams true serveOnce true,
+                "/mail/v4/conversations"
+                    respondWith "/global/errors/error_mock.json"
+                    withStatusCode 503 ignoreQueryParams true withNetworkDelay 2000 serveOnce true
+            )
+        }
+
+        navigator {
+            navigateTo(Destination.Inbox)
+        }
+
+        verifyContentToError()
+    }
+
+    @Test
+    @SmokeTest
+    @TestId("188894")
+    fun checkConversationsLoadingErrorToContent() {
+        mockWebServer.dispatcher = mockNetworkDispatcher(useDefaultMailSettings = false) {
+            addMockRequests(
+                "/mail/v4/settings"
+                    respondWith "/mail/v4/settings/mail-v4-settings_placeholder_conversation.json"
+                    withStatusCode 200,
+                "/mail/v4/conversations"
+                    respondWith "/global/errors/error_mock.json"
+                    withStatusCode 503 ignoreQueryParams true serveOnce true,
+                "/mail/v4/conversations"
+                    respondWith "/mail/v4/conversations/conversations_base_placeholder.json"
+                    withStatusCode 200 ignoreQueryParams true withNetworkDelay 2000 serveOnce true
+            )
+        }
+
+        navigator {
+            navigateTo(Destination.Inbox)
+        }
+
+        verifyErrorToContent()
+    }
+
+    @Test
+    @TestId("188895")
+    fun checkConversationsLoadingEmptyToError() {
+        mockWebServer.dispatcher = mockNetworkDispatcher(useDefaultMailSettings = false) {
+            addMockRequests(
+                "/mail/v4/settings"
+                    respondWith "/mail/v4/settings/mail-v4-settings_placeholder_conversation.json"
+                    withStatusCode 200,
+                "/mail/v4/conversations"
+                    respondWith "/mail/v4/conversations/conversations_empty.json"
+                    withStatusCode 200 ignoreQueryParams true serveOnce true,
+                "/mail/v4/conversations"
+                    respondWith "/global/errors/error_mock.json"
+                    withStatusCode 503 ignoreQueryParams true withNetworkDelay 2000 serveOnce true
+            )
+        }
+
+        navigator {
+            navigateTo(Destination.Inbox)
+        }
+
+        verifyEmptyToError()
+    }
+
+    @Test
+    @TestId("188896")
+    fun checkConversationsLoadingErrorToEmpty() {
+        mockWebServer.dispatcher = mockNetworkDispatcher(useDefaultMailSettings = false) {
+            addMockRequests(
+                "/mail/v4/settings"
+                    respondWith "/mail/v4/settings/mail-v4-settings_placeholder_conversation.json"
+                    withStatusCode 200,
+                "/mail/v4/conversations"
+                    respondWith "/global/errors/error_mock.json"
+                    withStatusCode 503 ignoreQueryParams true serveOnce true,
+                "/mail/v4/conversations"
+                    respondWith "/mail/v4/conversations/conversations_empty.json"
+                    withStatusCode 200 ignoreQueryParams true withNetworkDelay 2000 serveOnce true
+            )
+        }
+
+        navigator {
+            navigateTo(Destination.Inbox)
+        }
+
+        verifyErrorToEmpty()
+    }
+
+    @Test
+    @SmokeTest
+    @TestId("188897")
+    fun checkConversationsLoadingEmptyToContent() {
+        mockWebServer.dispatcher = mockNetworkDispatcher(useDefaultMailSettings = false) {
+            addMockRequests(
+                "/mail/v4/settings"
+                    respondWith "/mail/v4/settings/mail-v4-settings_placeholder_conversation.json"
+                    withStatusCode 200,
+                "/mail/v4/conversations"
+                    respondWith "/mail/v4/conversations/conversations_empty.json"
+                    withStatusCode 200 ignoreQueryParams true serveOnce true,
+                "/mail/v4/conversations"
+                    respondWith "/mail/v4/conversations/conversations_base_placeholder.json"
+                    withStatusCode 200 ignoreQueryParams true withNetworkDelay 2000 serveOnce true
+            )
+        }
+
+        navigator {
+            navigateTo(Destination.Inbox)
+        }
+
+        verifyEmptyToContent()
+    }
+}
---
+++ app/src/uiTest/kotlin/ch/protonmail/android/uitest/e2e/mailbox/errors/MailboxPullToRefreshErrorTests.kt
@@ -0,0 +1,144 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.uitest.e2e.mailbox.errors
+
+import ch.protonmail.android.uitest.models.avatar.AvatarInitial
+import ch.protonmail.android.uitest.models.mailbox.MailboxListItemEntry
+import ch.protonmail.android.uitest.models.snackbar.SnackbarTextEntry
+import ch.protonmail.android.uitest.robot.common.section.snackbarSection
+import ch.protonmail.android.uitest.robot.common.section.verify
+import ch.protonmail.android.uitest.robot.mailbox.mailboxRobot
+import ch.protonmail.android.uitest.robot.mailbox.section.emptyListSection
+import ch.protonmail.android.uitest.robot.mailbox.section.fullScreenErrorSection
+import ch.protonmail.android.uitest.robot.mailbox.section.listSection
+import ch.protonmail.android.uitest.robot.mailbox.section.verify
+
+internal interface MailboxPullToRefreshErrorTests {
+
+    private val baseItem: MailboxListItemEntry
+        get() = MailboxListItemEntry(
+            index = 0,
+            avatarInitial = AvatarInitial.WithText("M"),
+            participants = "mobileappsuitesting2",
+            subject = "Test message",
+            date = "Mar 6, 2023"
+        )
+
+    fun verifyContentToEmpty() {
+        mailboxRobot {
+            listSection {
+                verify { listItemsAreShown(baseItem) }
+
+                pullDownToRefresh()
+            }
+
+            emptyListSection {
+                verify { isShown() }
+            }
+        }
+    }
+
+    fun verifyEmptyToContent() {
+        mailboxRobot {
+            emptyListSection {
+                verify { isShown() }
+
+                pullDownToRefresh()
+            }
+
+            listSection {
+                verify { listItemsAreShown(baseItem) }
+            }
+        }
+    }
+
+    fun verifyErrorToEmpty() {
+        mailboxRobot {
+            fullScreenErrorSection {
+                verify { isShown() }
+
+                pullDownToRefresh()
+            }
+
+            emptyListSection {
+                verify { isShown() }
+            }
+        }
+    }
+
+    fun verifyEmptyToError() {
+        mailboxRobot {
+            emptyListSection {
+                verify { isShown() }
+
+                pullDownToRefresh()
+            }
+
+            fullScreenErrorSection {
+                verify { isShown() }
+            }
+        }
+    }
+
+    fun verifyErrorToContent() {
+        mailboxRobot {
+            fullScreenErrorSection {
+                verify { isShown() }
+
+                pullDownToRefresh()
+            }
+
+            listSection {
+                verify { listItemsAreShown(baseItem) }
+            }
+        }
+    }
+
+    fun verifyContentToError() {
+        mailboxRobot {
+            listSection {
+                verify { listItemsAreShown(baseItem) }
+
+                pullDownToRefresh()
+            }
+
+            snackbarSection {
+                verify { hasMessage(SnackbarTextEntry.FailedToLoadNewItems) }
+            }
+
+            listSection {
+                verify { listItemsAreShown(baseItem) }
+            }
+        }
+    }
+
+    fun verifyErrorToError() {
+        mailboxRobot {
+            fullScreenErrorSection {
+                verify { isShown() }
+            }
+
+            emptyListSection { pullDownToRefresh() }
+
+            fullScreenErrorSection {
+                verify { isShown() }
+            }
+        }
+    }
+}
