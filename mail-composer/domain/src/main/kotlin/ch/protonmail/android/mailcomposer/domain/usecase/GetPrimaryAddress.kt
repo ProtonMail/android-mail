31cb8657e34ce6a07249af64c4eb18fdee1ab3a7
Marino Meneghel <marino.meneghel@protonmail.com>
2023-06-23 16:03:11 +0200

    Create use case to get primary user's address
    MAILANDR-487

---
+++ mail-composer/domain/src/main/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/GetPrimaryAddress.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import kotlinx.coroutines.flow.first
+import me.proton.core.domain.entity.UserId
+import me.proton.core.user.domain.entity.UserAddress
+import javax.inject.Inject
+
+class GetPrimaryAddress @Inject constructor(
+    private val observeUserAddresses: ObserveUserAddresses
+) {
+
+    suspend operator fun invoke(userId: UserId): Either<DataError, UserAddress> {
+        val addresses = observeUserAddresses.invoke(userId).first()
+        if (addresses.isEmpty()) {
+            return DataError.Local.NoDataCached.left()
+        }
+
+        val primary = addresses.minBy { it.order }
+        return primary.right()
+    }
+
+}
---
+++ mail-composer/domain/src/test/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/GetPrimaryAddressTest.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcommon.domain.sample.UserAddressSample
+import ch.protonmail.android.testdata.user.UserIdTestData
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import me.proton.core.user.domain.entity.UserAddress
+import org.junit.Test
+import kotlin.test.assertEquals
+
+internal class GetPrimaryAddressTest {
+
+    private val userId = UserIdTestData.userId
+
+    private val observeUserAddresses = mockk<ObserveUserAddresses>()
+
+    private val getPrimaryAddress = GetPrimaryAddress(observeUserAddresses)
+
+    @Test
+    fun `returns primary user address when list contains more than one`() = runTest {
+        // Given
+        val addresses = listOf(UserAddressSample.secondaryAddress, UserAddressSample.primaryAddress)
+        every { observeUserAddresses.invoke(userId) } returns flowOf(addresses)
+
+        // When
+        val actual = getPrimaryAddress(userId)
+
+        // Then
+        assertEquals(UserAddressSample.primaryAddress.right(), actual)
+    }
+
+    @Test
+    fun `returns no cached data error when list is empty`() = runTest {
+        // Given
+        val addresses = emptyList<UserAddress>()
+        every { observeUserAddresses.invoke(userId) } returns flowOf(addresses)
+
+        // When
+        val actual = getPrimaryAddress(userId)
+
+        // Then
+        assertEquals(DataError.Local.NoDataCached.left(), actual)
+    }
+
+}
