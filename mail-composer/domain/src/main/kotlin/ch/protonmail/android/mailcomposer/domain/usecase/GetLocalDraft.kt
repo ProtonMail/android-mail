75c6ffe3af9fe05135a7f466adb5a2acef629286
Marino Meneghel <marino.meneghel@protonmail.com>
2023-07-06 14:33:00 +0200

    Extract GetLocalDraft use case
    To allow reusing the common behaviors from the saveDraftWith- use cases.
    
    MAILANDR-489

---
+++ mail-composer/domain/src/main/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/GetLocalDraft.kt
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.Either
+import arrow.core.continuations.either
+import ch.protonmail.android.mailcomposer.domain.model.SenderEmail
+import ch.protonmail.android.mailmessage.domain.entity.MessageId
+import ch.protonmail.android.mailmessage.domain.entity.MessageWithBody
+import ch.protonmail.android.mailmessage.domain.repository.MessageRepository
+import me.proton.core.domain.entity.UserId
+import javax.inject.Inject
+
+class GetLocalDraft @Inject constructor(
+    private val createEmptyDraft: CreateEmptyDraft,
+    private val messageRepository: MessageRepository,
+    private val resolveUserAddress: ResolveUserAddress
+) {
+
+    suspend operator fun invoke(
+        userId: UserId,
+        messageId: MessageId,
+        senderEmail: SenderEmail
+    ): Either<Error, MessageWithBody> = either {
+        val senderAddress = resolveUserAddress(userId, senderEmail)
+            .mapLeft { Error.ResolveUserAddressError }
+            .bind()
+
+        return@either messageRepository.getLocalMessageWithBody(userId, messageId)
+            ?: createEmptyDraft(messageId, userId, senderAddress)
+    }
+
+    sealed interface Error {
+        object ResolveUserAddressError : Error
+    }
+}
---
+++ mail-composer/presentation/src/test/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/GetLocalDraftTest.kt
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.sample.UserAddressSample
+import ch.protonmail.android.mailcommon.domain.sample.UserIdSample
+import ch.protonmail.android.mailcomposer.domain.model.SenderEmail
+import ch.protonmail.android.mailmessage.domain.entity.MessageId
+import ch.protonmail.android.mailmessage.domain.entity.MessageWithBody
+import ch.protonmail.android.mailmessage.domain.repository.MessageRepository
+import ch.protonmail.android.mailmessage.domain.sample.MessageIdSample
+import ch.protonmail.android.mailmessage.domain.sample.MessageWithBodySample
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import me.proton.core.domain.entity.UserId
+import me.proton.core.user.domain.entity.UserAddress
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class GetLocalDraftTest {
+
+    private val createEmptyDraftMock = mockk<CreateEmptyDraft>()
+    private val messageRepositoryMock = mockk<MessageRepository>()
+    private val resolveUserAddressMock = mockk<ResolveUserAddress>()
+
+    private val getLocalDraft = GetLocalDraft(
+        createEmptyDraftMock,
+        messageRepositoryMock,
+        resolveUserAddressMock
+    )
+
+    @Test
+    fun `returns error when sender email fails being resolved`() = runTest {
+        // Given
+        val userId = UserIdSample.Primary
+        val senderEmail = SenderEmail("unresolvable@sender.email")
+        val draftMessageId = MessageIdSample.build()
+        expectResolveUserAddressFailure(userId, senderEmail)
+
+        // When
+        val actualEither = getLocalDraft(userId, draftMessageId, senderEmail)
+
+        // Then
+        assertEquals(GetLocalDraft.Error.ResolveUserAddressError.left(), actualEither)
+    }
+
+    @Test
+    fun `returns existing draft when it exists locally`() = runTest {
+        // Given
+        val userId = UserIdSample.Primary
+        val userAddress = UserAddressSample.PrimaryAddress
+        val senderEmail = SenderEmail(userAddress.email)
+        val draftMessageId = MessageIdSample.build()
+        val expectedExistingDraft = expectedExistingDraft(userId, draftMessageId) { MessageWithBodySample.EmptyDraft }
+        expectedResolvedUserAddress(userId, senderEmail) { userAddress }
+
+        // When
+        val actualEither = getLocalDraft(userId, draftMessageId, senderEmail)
+
+        // Then
+        assertEquals(expectedExistingDraft.right(), actualEither)
+    }
+
+    @Test
+    fun `create and returns new empty draft when it does not exist locally`() = runTest {
+        // Given
+        val userId = UserIdSample.Primary
+        val userAddress = UserAddressSample.PrimaryAddress
+        val senderEmail = SenderEmail(userAddress.email)
+        val draftMessageId = MessageIdSample.build()
+        val expectedNewDraft = expectedNewDraft(userId, draftMessageId, userAddress) {
+            MessageWithBodySample.EmptyDraft
+        }
+        expectedResolvedUserAddress(userId, senderEmail) { userAddress }
+
+        // When
+        val actualEither = getLocalDraft(userId, draftMessageId, senderEmail)
+
+        // Then
+        assertEquals(expectedNewDraft.right(), actualEither)
+    }
+
+    private fun expectedNewDraft(
+        userId: UserId,
+        messageId: MessageId,
+        senderAddress: UserAddress,
+        existingDraft: () -> MessageWithBody
+    ): MessageWithBody = existingDraft().also {
+        coEvery { messageRepositoryMock.getLocalMessageWithBody(userId, messageId) } returns null
+        every { createEmptyDraftMock(messageId, userId, senderAddress) } returns it
+    }
+
+    private fun expectedExistingDraft(
+        userId: UserId,
+        messageId: MessageId,
+        existingDraft: () -> MessageWithBody
+    ): MessageWithBody = existingDraft().also {
+        coEvery { messageRepositoryMock.getLocalMessageWithBody(userId, messageId) } returns it
+    }
+
+    private fun expectedResolvedUserAddress(
+        userId: UserId,
+        email: SenderEmail,
+        address: () -> UserAddress
+    ) = address().also { coEvery { resolveUserAddressMock(userId, email) } returns it.right() }
+
+    private fun expectResolveUserAddressFailure(userId: UserId, email: SenderEmail) {
+        coEvery { resolveUserAddressMock(userId, email) } returns ResolveUserAddress.Error.UserAddressNotFound.left()
+    }
+}
