abe90a8d7490c5945430edec5dfba1ecda7f68b0
Marino Meneghel <marino.meneghel@protonmail.com>
2023-07-06 16:04:07 +0200

    Create use case to save draft with subject
    MAILANDR-489

---
+++ mail-composer/domain/src/main/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/StoreDraftWithSubject.kt
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.Either
+import arrow.core.continuations.either
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcomposer.domain.model.SenderEmail
+import ch.protonmail.android.mailcomposer.domain.model.Subject
+import ch.protonmail.android.mailmessage.domain.entity.MessageId
+import me.proton.core.domain.entity.UserId
+import javax.inject.Inject
+
+class StoreDraftWithSubject @Inject constructor(
+    private val getLocalDraft: GetLocalDraft,
+    private val saveDraft: SaveDraft
+) {
+    suspend operator fun invoke(
+        userId: UserId,
+        messageId: MessageId,
+        senderEmail: SenderEmail,
+        subject: Subject
+    ): Either<Error, Unit> = either {
+        val draftWithBody = getLocalDraft(userId, messageId, senderEmail)
+            .mapLeft { Error.DraftReadError }
+            .bind()
+
+        val updatedDraft = draftWithBody.copy(
+            message = draftWithBody.message.copy(subject = subject.value)
+        )
+        saveDraft(updatedDraft, userId)
+            .mapFalse { Error.DraftSaveError }
+            .bind()
+    }
+
+    private fun Boolean.mapFalse(block: () -> Error): Either<Error, Unit> = if (this) Unit.right() else block().left()
+
+    sealed interface Error {
+        object DraftSaveError : Error
+        object DraftReadError : Error
+    }
+}
---
+++ mail-composer/domain/src/test/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/StoreDraftWithSubjectTest.kt
@@ -0,0 +1,135 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.sample.UserAddressSample
+import ch.protonmail.android.mailcommon.domain.sample.UserIdSample
+import ch.protonmail.android.mailcomposer.domain.model.SenderEmail
+import ch.protonmail.android.mailcomposer.domain.model.Subject
+import ch.protonmail.android.mailmessage.domain.entity.MessageId
+import ch.protonmail.android.mailmessage.domain.entity.MessageWithBody
+import ch.protonmail.android.mailmessage.domain.sample.MessageIdSample
+import ch.protonmail.android.mailmessage.domain.sample.MessageWithBodySample
+import io.mockk.Called
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import me.proton.core.domain.entity.UserId
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class StoreDraftWithSubjectTest {
+
+    private val saveDraftMock = mockk<SaveDraft>()
+    private val getLocalDraftMock = mockk<GetLocalDraft>()
+
+    private val storeDraftWithSubject = StoreDraftWithSubject(
+        getLocalDraftMock,
+        saveDraftMock
+    )
+
+    @Test
+    fun `save draft with subject`() = runTest {
+        // Given
+        val userId = UserIdSample.Primary
+        val draftMessageId = MessageIdSample.build()
+        val senderEmail = SenderEmail(UserAddressSample.PrimaryAddress.email)
+        val subject = Subject("Subject of this email")
+        val draftWithBody = expectedGetLocalDraft(userId, draftMessageId, senderEmail) {
+            MessageWithBodySample.EmptyDraft
+        }
+        val expectedSavedDraft = draftWithBody.copy(message = draftWithBody.message.copy(subject = subject.value))
+        givenSaveDraftSucceeds(expectedSavedDraft, userId)
+
+        // When
+        val actualEither = storeDraftWithSubject(userId, draftMessageId, senderEmail, subject)
+
+        // Then
+        coVerify { saveDraftMock(expectedSavedDraft, userId) }
+        assertEquals(Unit.right(), actualEither)
+    }
+
+    @Test
+    fun `returns error when get local draft fails`() = runTest {
+        // Given
+        val userId = UserIdSample.Primary
+        val draftMessageId = MessageIdSample.build()
+        val senderEmail = SenderEmail(UserAddressSample.PrimaryAddress.email)
+        val subject = Subject("Subject of this email")
+        expectedGetLocalDraftFails(userId, draftMessageId, senderEmail) {
+            GetLocalDraft.Error.ResolveUserAddressError
+        }
+
+        // When
+        val actualEither = storeDraftWithSubject(userId, draftMessageId, senderEmail, subject)
+
+        // Then
+        coVerify { saveDraftMock wasNot Called }
+        assertEquals(StoreDraftWithSubject.Error.DraftReadError.left(), actualEither)
+    }
+
+    @Test
+    fun `returns error when save draft fails`() = runTest {
+        // Given
+        val userId = UserIdSample.Primary
+        val draftMessageId = MessageIdSample.build()
+        val senderEmail = SenderEmail(UserAddressSample.PrimaryAddress.email)
+        val subject = Subject("Subject of this email")
+        val draftWithBody = expectedGetLocalDraft(userId, draftMessageId, senderEmail) {
+            MessageWithBodySample.EmptyDraft
+        }
+        val expectedSavedDraft = draftWithBody.copy(message = draftWithBody.message.copy(subject = subject.value))
+        givenSaveDraftFails(expectedSavedDraft, userId)
+
+        // When
+        val actualEither = storeDraftWithSubject(userId, draftMessageId, senderEmail, subject)
+
+        // Then
+        assertEquals(StoreDraftWithSubject.Error.DraftSaveError.left(), actualEither)
+    }
+
+    private fun expectedGetLocalDraft(
+        userId: UserId,
+        messageId: MessageId,
+        senderEmail: SenderEmail,
+        localDraft: () -> MessageWithBody
+    ): MessageWithBody = localDraft().also {
+        coEvery { getLocalDraftMock.invoke(userId, messageId, senderEmail) } returns it.right()
+    }
+
+    private fun expectedGetLocalDraftFails(
+        userId: UserId,
+        messageId: MessageId,
+        senderEmail: SenderEmail,
+        error: () -> GetLocalDraft.Error
+    ): GetLocalDraft.Error = error().also {
+        coEvery { getLocalDraftMock.invoke(userId, messageId, senderEmail) } returns it.left()
+    }
+
+    private fun givenSaveDraftSucceeds(messageWithBody: MessageWithBody, userId: UserId) {
+        coEvery { saveDraftMock(messageWithBody, userId) } returns true
+    }
+
+    private fun givenSaveDraftFails(messageWithBody: MessageWithBody, userId: UserId) {
+        coEvery { saveDraftMock(messageWithBody, userId) } returns false
+    }
+}
