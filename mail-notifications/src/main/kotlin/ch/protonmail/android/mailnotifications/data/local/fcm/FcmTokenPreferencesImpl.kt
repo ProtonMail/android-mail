006f72fe2db7b5872420e34ad197bd6f210cd22e
Alejandro Mezcua <alejandro.mezcua@proton.ch>
2023-07-21 14:10:27 +0200

    Create API client for device registration
    When we get a firebase token, we need to send it to the API to associate it with the user ids of the accounts logged in in the device.
    
    Here we add the API definition, the worker that calls the API and the DataStore to store the token for later use.
    
    MAILANDR-751

---
+++ mail-notifications/src/main/kotlin/ch/protonmail/android/mailnotifications/data/local/fcm/FcmTokenPreferencesImpl.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailnotifications.data.local.fcm
+
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import ch.protonmail.android.mailnotifications.domain.FcmTokenPreferences
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+internal class FcmTokenPreferencesImpl @Inject constructor(
+    @ApplicationContext private val context: Context
+) : FcmTokenPreferences {
+
+    private val Context.fcmTokenStore: DataStore<Preferences> by preferencesDataStore(name = STORE_NAME)
+
+    override suspend fun storeToken(token: String) {
+        context.fcmTokenStore.edit { preferences -> preferences[stringPreferencesKey(FCM_TOKEN_KEY)] = token }
+    }
+
+    override suspend fun getToken(): String =
+        context.fcmTokenStore.data.map { preferences -> preferences[stringPreferencesKey(FCM_TOKEN_KEY)] ?: "" }.first()
+
+    private companion object {
+
+        const val STORE_NAME = "fcmTokenStore"
+        const val FCM_TOKEN_KEY = "fcmTokenKey"
+    }
+}
