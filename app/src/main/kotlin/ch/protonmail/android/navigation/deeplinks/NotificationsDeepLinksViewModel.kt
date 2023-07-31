894e11691f0932fc950af9963b6bb8b50f3e1251
Alejandro Mezcua <alejandro.mezcua@proton.ch>
2023-07-31 19:31:10 +0200

    Updated the view model to check for the active user and switch it if the user target of the notification is a different one.

--- app/src/main/kotlin/ch/protonmail/android/navigation/deeplinks/NotificationsDeepLinksViewModel.kt
+++ app/src/main/kotlin/ch/protonmail/android/navigation/deeplinks/NotificationsDeepLinksViewModel.kt
@@ -21,17 +21,25 @@ package ch.protonmail.android.navigation.deeplinks
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import ch.protonmail.android.mailcommon.domain.model.ConversationId
+import ch.protonmail.android.mailcommon.domain.model.DataError
 import ch.protonmail.android.mailconversation.domain.repository.ConversationRepository
+import ch.protonmail.android.mailmessage.domain.entity.Message
 import ch.protonmail.android.mailmessage.domain.entity.MessageId
 import ch.protonmail.android.mailmessage.domain.repository.MessageRepository
 import ch.protonmail.android.mailnotifications.domain.NotificationsDeepLinkHelper
+import ch.protonmail.android.navigation.deeplinks.NotificationsDeepLinksViewModel.State.NavigateToInbox
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
+import me.proton.core.account.domain.entity.isReady
+import me.proton.core.accountmanager.domain.AccountManager
 import me.proton.core.domain.entity.UserId
 import me.proton.core.mailsettings.domain.entity.ViewMode
 import me.proton.core.mailsettings.domain.repository.MailSettingsRepository
@@ -39,10 +47,12 @@ import me.proton.core.network.domain.NetworkManager
 import me.proton.core.network.domain.NetworkStatus
 import timber.log.Timber
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
 @HiltViewModel
 class NotificationsDeepLinksViewModel @Inject constructor(
     private val networkManager: NetworkManager,
+    private val accountManager: AccountManager,
     private val messageRepository: MessageRepository,
     private val conversationRepository: ConversationRepository,
     private val mailSettingsRepository: MailSettingsRepository,
@@ -54,64 +64,162 @@ class NotificationsDeepLinksViewModel @Inject constructor(
 
     private var navigateToMessageJob: Job? = null
 
+    // accountManager.setAsPrimary(userId)
     fun navigateToMessage(
         notificationId: Int,
         messageId: String,
         userId: String
     ) {
         if (isOffline()) {
-            _state.value = State.NavigateToInbox
-            notificationsDeepLinkHelper.cancelNotification(notificationId)
+            navigateToInbox(notificationId, userId)
         } else {
-            Timber.d("navigateToMessage: $messageId, $userId")
-            navigateToMessageJob?.cancel()
-            navigateToMessageJob = viewModelScope.launch {
-                val conversationModeEnabled = mailSettingsRepository.getMailSettings(UserId(userId))
-                    .viewMode
-                    ?.value == ViewMode.ConversationGrouping.value
-                messageRepository.observeCachedMessage(UserId(userId), MessageId(messageId))
-                    .distinctUntilChanged()
-                    .collectLatest { messageResult ->
-                        notificationsDeepLinkHelper.cancelNotification(notificationId)
-                        messageResult
-                            .onLeft {
-                                _state.value = State.NavigateToInbox
-                            }
-                            .onRight { message ->
-                                if (conversationModeEnabled) {
-                                    conversationRepository.observeConversation(
-                                        UserId(userId),
-                                        message.conversationId,
-                                        true
-                                    ).collectLatest { conversationResult ->
-                                        conversationResult
-                                            .onLeft { Timber.d("Conversation not found: $it") }
-                                            .onRight { conversation ->
-                                                _state.value =
-                                                    State.NavigateToConversation(conversation.conversationId, false)
-                                            }
-                                    }
-                                } else {
-                                    _state.value = State.NavigateToMessageDetails(message.messageId, false)
-                                }
-                                navigateToMessageJob?.cancel()
-                            }
-                    }
+            navigateToMessageOrConversation(notificationId, messageId, UserId(userId))
+        }
+    }
+
+    fun navigateToInbox(notificationId: Int, userId: String) {
+        notificationsDeepLinkHelper.cancelNotification(notificationId)
+        viewModelScope.launch {
+            val activeUserId = accountManager.getPrimaryUserId().firstOrNull()
+            if (activeUserId != null && activeUserId.id != userId) {
+                switchUserAndNavigateToInbox(userId)
+            } else {
+                _state.value = NavigateToInbox.ActiveUser
             }
         }
     }
 
-    fun navigateToInbox(notificationId: Int) {
-        notificationsDeepLinkHelper.cancelNotification(notificationId)
-        _state.value = State.NavigateToInbox
+    private suspend fun switchUserAndNavigateToInbox(userId: String) {
+        val switchAccountResult = switchActiveUserIfRequiredTo(userId)
+        _state.value = when (switchAccountResult) {
+            AccountSwitchResult.AccountSwitchError -> NavigateToInbox.ActiveUser
+            is AccountSwitchResult.AccountSwitched -> NavigateToInbox.ActiveUserSwitched(switchAccountResult.newEmail)
+            AccountSwitchResult.NotRequired -> NavigateToInbox.ActiveUser
+        }
+    }
+
+    private fun navigateToMessageOrConversation(
+        notificationId: Int,
+        messageId: String,
+        userId: UserId
+    ) {
+        Timber.d("navigateToMessage: $messageId, $userId")
+        navigateToMessageJob?.cancel()
+        navigateToMessageJob = viewModelScope.launch {
+            when (val switchAccountResult = switchActiveUserIfRequiredTo(userId.id)) {
+                AccountSwitchResult.AccountSwitchError -> navigateToInbox(notificationId, userId.id)
+                is AccountSwitchResult.AccountSwitched -> navigateToMessageOrConversation(
+                    this.coroutineContext,
+                    notificationId,
+                    messageId,
+                    switchAccountResult.newUserId,
+                    switchAccountResult.newEmail
+                )
+
+                AccountSwitchResult.NotRequired -> navigateToMessageOrConversation(
+                    this.coroutineContext,
+                    notificationId,
+                    messageId,
+                    userId
+                )
+            }
+        }
+    }
+
+    private suspend fun navigateToMessageOrConversation(
+        coroutineContext: CoroutineContext,
+        notificationId: Int,
+        messageId: String,
+        userId: UserId,
+        switchedAccountEmail: String? = null
+    ) {
+        messageRepository.observeCachedMessage(userId, MessageId(messageId))
+            .distinctUntilChanged()
+            .collectLatest { messageResult ->
+                notificationsDeepLinkHelper.cancelNotification(notificationId)
+                messageResult
+                    .onLeft {
+                        if (it != DataError.Local.NoDataCached) navigateToInbox(notificationId, userId.id)
+                    }
+                    .onRight { message ->
+                        if (isConversationModeEnabled(userId)) {
+                            navigateToConversation(message, userId, notificationId, switchedAccountEmail)
+                        } else {
+                            _state.value = State.NavigateToMessageDetails(message.messageId, switchedAccountEmail)
+                        }
+                        coroutineContext.cancel()
+                    }
+            }
+    }
+
+    private suspend fun switchActiveUserIfRequiredTo(userId: String): AccountSwitchResult {
+        return if (accountManager.getPrimaryUserId().firstOrNull()?.id == userId) {
+            AccountSwitchResult.NotRequired
+        } else {
+            val targetAccount = accountManager.getAccount(UserId(userId))
+                .filter { account -> account?.isReady() == true }
+                .firstOrNull()
+            if (targetAccount != null) {
+                accountManager.setAsPrimary(UserId(userId))
+                AccountSwitchResult.AccountSwitched(targetAccount.userId, targetAccount.email ?: "")
+            } else {
+                AccountSwitchResult.AccountSwitchError
+            }
+        }
+    }
+
+    private suspend fun isConversationModeEnabled(userId: UserId): Boolean =
+        mailSettingsRepository.getMailSettings(userId)
+            .viewMode
+            ?.value == ViewMode.ConversationGrouping.value
+
+    private suspend fun navigateToConversation(
+        message: Message,
+        userId: UserId,
+        notificationId: Int,
+        switchedAccountEmail: String?
+    ) {
+        conversationRepository.observeConversation(
+            userId,
+            message.conversationId,
+            true
+        ).collectLatest { conversationResult ->
+            conversationResult
+                .onLeft {
+                    Timber.d("Conversation not found: $it")
+                    if (it != DataError.Local.NoDataCached) navigateToInbox(notificationId, userId.id)
+                }
+                .onRight { conversation ->
+                    _state.value =
+                        State.NavigateToConversation(conversation.conversationId, switchedAccountEmail)
+                }
+        }
     }
 
     private fun isOffline() = networkManager.networkStatus == NetworkStatus.Disconnected
 
     sealed interface State {
         object None : State
-        object NavigateToInbox : State
-        data class NavigateToMessageDetails(val messageId: MessageId, val userSwitched: Boolean) : State
-        data class NavigateToConversation(val conversationId: ConversationId, val userSwitched: Boolean) : State
+        sealed interface NavigateToInbox : State {
+            object ActiveUser : NavigateToInbox
+            data class ActiveUserSwitched(val email: String) : NavigateToInbox
+        }
+
+        data class NavigateToMessageDetails(
+            val messageId: MessageId,
+            val userSwitchedEmail: String? = null
+        ) : State
+
+        data class NavigateToConversation(
+            val conversationId: ConversationId,
+            val userSwitchedEmail: String? = null
+        ) : State
+    }
+
+    private sealed interface AccountSwitchResult {
+        object NotRequired : AccountSwitchResult
+        data class AccountSwitched(val newUserId: UserId, val newEmail: String) : AccountSwitchResult
+
+        object AccountSwitchError : AccountSwitchResult
     }
 }
--- app/src/test/kotlin/ch/protonmail/android/navigation/deeplinks/NotificationsDeepLinksViewModelTest.kt
+++ app/src/test/kotlin/ch/protonmail/android/navigation/deeplinks/NotificationsDeepLinksViewModelTest.kt
@@ -19,10 +19,12 @@
 package ch.protonmail.android.navigation.deeplinks
 
 import java.util.UUID
+import androidx.lifecycle.viewmodel.compose.viewModel
 import app.cash.turbine.test
 import arrow.core.left
 import arrow.core.right
 import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcommon.domain.sample.AccountSample
 import ch.protonmail.android.mailconversation.domain.repository.ConversationRepository
 import ch.protonmail.android.mailconversation.domain.sample.ConversationSample
 import ch.protonmail.android.mailmessage.domain.entity.MessageId
@@ -33,6 +35,7 @@ import ch.protonmail.android.navigation.deeplinks.NotificationsDeepLinksViewMode
 import ch.protonmail.android.navigation.deeplinks.NotificationsDeepLinksViewModel.State.NavigateToInbox
 import ch.protonmail.android.navigation.deeplinks.NotificationsDeepLinksViewModel.State.NavigateToMessageDetails
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import io.mockk.verify
 import kotlinx.coroutines.Dispatchers
@@ -40,6 +43,7 @@ import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
+import me.proton.core.accountmanager.domain.AccountManager
 import me.proton.core.domain.entity.UserId
 import me.proton.core.domain.type.IntEnum
 import me.proton.core.mailsettings.domain.entity.MailSettings
@@ -57,6 +61,7 @@ class NotificationsDeepLinksViewModelTest {
     private val networkManager: NetworkManager = mockk {
         coEvery { networkStatus } returns NetworkStatus.Unmetered
     }
+    private val accountManager: AccountManager = mockk(relaxed = true)
     private val messageRepository: MessageRepository = mockk()
     private val conversationRepository: ConversationRepository = mockk()
     private val mailSettings: MailSettings = mockk()
@@ -74,14 +79,15 @@ class NotificationsDeepLinksViewModelTest {
     fun `Should emit navigate to inbox and cancel the group notification`() = runTest {
         // Given
         val viewModel = buildViewModel()
+        val userId = UUID.randomUUID().toString()
 
         // When
         val notificationId = Random.nextInt()
-        viewModel.navigateToInbox(notificationId)
+        viewModel.navigateToInbox(notificationId, userId)
 
         // Then
         viewModel.state.test {
-            assertEquals(NavigateToInbox, awaitItem())
+            assertEquals(NavigateToInbox.ActiveUser, awaitItem())
             verify { notificationsDeepLinkHelper.cancelNotification(notificationId) }
         }
     }
@@ -92,6 +98,7 @@ class NotificationsDeepLinksViewModelTest {
         val notificationId = Random.nextInt()
         val messageId = UUID.randomUUID().toString()
         val userId = UUID.randomUUID().toString()
+        coEvery { accountManager.getPrimaryUserId() } returns flowOf(UserId(userId))
         coEvery { mailSettings.viewMode } returns IntEnum(ViewMode.ConversationGrouping.value, null)
         coEvery { messageRepository.observeCachedMessage(UserId(userId), MessageId(messageId)) } returns flowOf(
             AlphaAppQAReport.right()
@@ -113,10 +120,7 @@ class NotificationsDeepLinksViewModelTest {
         // Then
         viewModel.state.test {
             assertEquals(
-                NavigateToConversation(
-                    AlphaAppQAReport.conversationId,
-                    false
-                ),
+                NavigateToConversation(AlphaAppQAReport.conversationId),
                 awaitItem()
             )
             verify { notificationsDeepLinkHelper.cancelNotification(notificationId) }
@@ -129,6 +133,7 @@ class NotificationsDeepLinksViewModelTest {
         val notificationId = Random.nextInt()
         val messageId = UUID.randomUUID().toString()
         val userId = UUID.randomUUID().toString()
+        coEvery { accountManager.getPrimaryUserId() } returns flowOf(UserId(userId))
         coEvery { mailSettings.viewMode } returns IntEnum(ViewMode.NoConversationGrouping.value, null)
         coEvery { messageRepository.observeCachedMessage(UserId(userId), MessageId(messageId)) } returns flowOf(
             AlphaAppQAReport.right()
@@ -141,10 +146,7 @@ class NotificationsDeepLinksViewModelTest {
         // Then
         viewModel.state.test {
             assertEquals(
-                NavigateToMessageDetails(
-                    AlphaAppQAReport.messageId,
-                    false
-                ),
+                NavigateToMessageDetails(AlphaAppQAReport.messageId),
                 awaitItem()
             )
             verify { notificationsDeepLinkHelper.cancelNotification(notificationId) }
@@ -165,7 +167,7 @@ class NotificationsDeepLinksViewModelTest {
 
         // Then
         viewModel.state.test {
-            assertEquals(NavigateToInbox, awaitItem())
+            assertEquals(NavigateToInbox.ActiveUser, awaitItem())
             verify { notificationsDeepLinkHelper.cancelNotification(notificationId) }
         }
     }
@@ -187,13 +189,104 @@ class NotificationsDeepLinksViewModelTest {
 
         // Then
         viewModel.state.test {
-            assertEquals(NavigateToInbox, awaitItem())
+            assertEquals(NavigateToInbox.ActiveUser, awaitItem())
             verify { notificationsDeepLinkHelper.cancelNotification(notificationId) }
+        }
+    }
+
+    @Test
+    fun `Should navigate to inbox if conversation mode is enabled but the conversation can not be read`() = runTest {
+        // Given
+        val notificationId = Random.nextInt()
+        val messageId = UUID.randomUUID().toString()
+        val userId = UUID.randomUUID().toString()
+        coEvery { mailSettings.viewMode } returns IntEnum(ViewMode.ConversationGrouping.value, null)
+        coEvery { messageRepository.observeCachedMessage(UserId(userId), MessageId(messageId)) } returns flowOf(
+            AlphaAppQAReport.right()
+        )
+        coEvery {
+            conversationRepository.observeConversation(
+                UserId(userId),
+                AlphaAppQAReport.conversationId,
+                true
+            )
+        } returns flowOf(DataError.Local.Unknown.left())
+        val viewModel = buildViewModel()
+
+        // When
+        viewModel.navigateToMessage(notificationId, messageId, userId)
+
+        // Then
+        viewModel.state.test {
+            assertEquals(NavigateToInbox.ActiveUser, awaitItem())
+            verify { notificationsDeepLinkHelper.cancelNotification(notificationId) }
+        }
+    }
+
+    @Test
+    fun `Should switch account and emit switched for inbox notification to an active non primary account`() = runTest {
+        // Given
+        val notificationId = Random.nextInt()
+        val notificationUserId = UUID.randomUUID().toString()
+        val activeUserId = UUID.randomUUID().toString()
+        val viewModel = buildViewModel()
+        coEvery { accountManager.getPrimaryUserId() } returns flowOf(UserId(activeUserId))
+        coEvery { accountManager.getAccount(UserId(notificationUserId)) } returns flowOf(AccountSample.Primary)
+
+        // When
+        viewModel.navigateToInbox(notificationId, notificationUserId)
+
+        // Then
+        viewModel.state.test {
+            assertEquals(NavigateToInbox.ActiveUserSwitched(AccountSample.Primary.email!!), awaitItem())
+            verify { notificationsDeepLinkHelper.cancelNotification(notificationId) }
+            coVerify { accountManager.setAsPrimary(UserId(notificationUserId)) }
+        }
+    }
+
+    @Test
+    fun `Should switch account and emit switched for message notification to active non primary account`() = runTest {
+        // Given
+        val notificationId = Random.nextInt()
+        val notificationTargetAccount = AccountSample.Primary
+        val activeUserId = UUID.randomUUID().toString()
+        val messageId = UUID.randomUUID().toString()
+        val viewModel = buildViewModel()
+        coEvery { accountManager.getPrimaryUserId() } returns flowOf(UserId(activeUserId))
+        coEvery {
+            accountManager.getAccount(notificationTargetAccount.userId)
+        } returns flowOf(notificationTargetAccount)
+        coEvery { mailSettings.viewMode } returns IntEnum(ViewMode.ConversationGrouping.value, null)
+        coEvery {
+            messageRepository.observeCachedMessage(notificationTargetAccount.userId, any())
+        } returns flowOf(AlphaAppQAReport.right())
+        coEvery {
+            conversationRepository.observeConversation(
+                notificationTargetAccount.userId,
+                AlphaAppQAReport.conversationId,
+                true
+            )
+        } returns flowOf(
+            ConversationSample.AlphaAppFeedback.right()
+        )
+
+        // When
+        viewModel.navigateToMessage(notificationId, messageId, notificationTargetAccount.userId.id)
+
+        // Then
+        viewModel.state.test {
+            assertEquals(
+                NavigateToConversation(AlphaAppQAReport.conversationId, AccountSample.Primary.email),
+                awaitItem()
+            )
+            verify { notificationsDeepLinkHelper.cancelNotification(notificationId) }
+            coVerify { accountManager.setAsPrimary(notificationTargetAccount.userId) }
         }
     }
 
     private fun buildViewModel() = NotificationsDeepLinksViewModel(
         networkManager = networkManager,
+        accountManager = accountManager,
         messageRepository = messageRepository,
         conversationRepository = conversationRepository,
         mailSettingsRepository = mailSettingsRepository,
