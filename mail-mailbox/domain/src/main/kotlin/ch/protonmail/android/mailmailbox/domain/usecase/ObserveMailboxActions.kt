368f762ac7aa5e97652fdf250daa1732644c2f02
Johannes Prueller <johannes.prueller@proton.ch>
2023-08-28 15:18:58 +0200

    Add use case to observe mailbox bottombar action items
    MAILANDR-671

---
+++ mail-mailbox/domain/src/main/kotlin/ch/protonmail/android/mailmailbox/domain/usecase/ObserveMailboxActions.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailmailbox.domain.usecase
+
+import arrow.core.Either
+import arrow.core.continuations.either
+import ch.protonmail.android.mailcommon.domain.model.Action
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.maillabel.domain.model.MailLabel
+import ch.protonmail.android.maillabel.domain.model.SystemLabelId
+import ch.protonmail.android.mailmailbox.domain.model.MailboxBottomBarDefaults
+import javax.inject.Inject
+
+class ObserveMailboxActions @Inject constructor() {
+
+    suspend operator fun invoke(currentMailLabel: MailLabel): Either<DataError, List<Action>> {
+        return either {
+            val actions = MailboxBottomBarDefaults.actions.toMutableList()
+
+            if (currentMailLabel.isTrashOrSpam()) {
+                actions[actions.indexOf(Action.Trash)] = Action.Delete
+            }
+            actions
+        }
+    }
+
+    private fun MailLabel.isTrashOrSpam() =
+        id.labelId == SystemLabelId.Trash.labelId || id.labelId == SystemLabelId.Spam.labelId
+}
---
+++ mail-mailbox/domain/src/test/kotlin/ch/protonmail/android/mailmailbox/domain/usecase/ObserveMailboxActionsTest.kt
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailmailbox.domain.usecase
+
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.Action
+import ch.protonmail.android.maillabel.domain.model.MailLabel
+import ch.protonmail.android.maillabel.domain.model.MailLabelId
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class ObserveMailboxActionsTest {
+
+    private val observeMailboxActions = ObserveMailboxActions()
+
+    @Test
+    fun `returns default actions for non-trash or non-spam labels`() = runTest {
+        // Given
+        val currentMailLabel = MailLabel.System(MailLabelId.System.Inbox)
+        val expected = listOf(Action.MarkUnread, Action.Trash, Action.Move, Action.Label, Action.More)
+
+        // When
+        val actions = observeMailboxActions(currentMailLabel)
+
+        // Then
+        assertEquals(expected.right(), actions)
+    }
+
+    @Test
+    fun `returns delete actions for trash label`() = runTest {
+        // Given
+        val currentMailLabel = MailLabel.System(MailLabelId.System.Trash)
+        val expected = listOf(Action.MarkUnread, Action.Delete, Action.Move, Action.Label, Action.More)
+
+        // When
+        val actions = observeMailboxActions(currentMailLabel)
+
+        // Then
+        assertEquals(expected.right(), actions)
+    }
+
+    @Test
+    fun `returns delete actions for spam label`() = runTest {
+        // Given
+        val currentMailLabel = MailLabel.System(MailLabelId.System.Spam)
+        val expected = listOf(Action.MarkUnread, Action.Delete, Action.Move, Action.Label, Action.More)
+
+        // When
+        val actions = observeMailboxActions(currentMailLabel)
+
+        // Then
+        assertEquals(expected.right(), actions)
+    }
+}
