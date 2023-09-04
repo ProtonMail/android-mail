772daf9d5e76fff30640a957d2fb6b5e0982c8b0
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2023-09-04 17:17:15 +0200

    Add UriHelper and ExternalFileStorage classes to manage reading files from external storage
    MAILANDR-707

---
+++ mail-common/data/src/main/kotlin/ch/protonmail/android/mailcommon/data/file/UriHelper.kt
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcommon.data.file
+
+import android.content.Context
+import android.net.Uri
+import android.provider.OpenableColumns
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.withContext
+import me.proton.core.util.kotlin.DispatcherProvider
+import javax.inject.Inject
+
+class UriHelper @Inject constructor(
+    private val dispatcherProvider: DispatcherProvider,
+    private val contentResolverHelper: ContentResolverHelper
+) {
+
+    suspend fun readFromUri(uri: Uri): ByteArray? = withContext(dispatcherProvider.Io) {
+        runCatching {
+            contentResolverHelper.openInputStream(uri)
+                ?.bufferedReader()
+                ?.use { it.readText() }
+                ?.toByteArray()
+        }.getOrNull()
+    }
+
+    suspend fun getFileInformationFromUri(uri: Uri): FileInformation? {
+        val name = getFileNameFromUri(uri)
+        val size = getFileSizeFromUri(uri)
+        val mimeType = getFileMimeTypeFromUri(uri)
+
+        if (name.isNullOrEmpty() || size == null || mimeType.isNullOrEmpty()) return null
+
+        return FileInformation(name, size, mimeType)
+    }
+
+    private suspend fun getFileNameFromUri(uri: Uri) = withContext(dispatcherProvider.Io) {
+        contentResolverHelper.query(uri)
+            ?.use { cursor ->
+                val nameIndex = cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME)
+                cursor.moveToFirst()
+                cursor.getString(nameIndex)
+            }
+    }
+
+    private suspend fun getFileSizeFromUri(uri: Uri) = withContext(dispatcherProvider.Io) {
+        contentResolverHelper.query(uri)
+            ?.use { cursor ->
+                val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
+                cursor.moveToFirst()
+                cursor.getLong(sizeIndex)
+            }
+    }
+
+    private suspend fun getFileMimeTypeFromUri(uri: Uri) = withContext(dispatcherProvider.Io) {
+        contentResolverHelper.getType(uri)
+    }
+}
+
+class ContentResolverHelper @Inject constructor(
+    @ApplicationContext private val applicationContext: Context
+) {
+
+    fun openInputStream(uri: Uri) = applicationContext.contentResolver.openInputStream(uri)
+
+    fun getType(uri: Uri) = applicationContext.contentResolver.getType(uri)
+
+    fun query(uri: Uri) = applicationContext.contentResolver.query(uri, null, null, null, null)
+}
+
+data class FileInformation(
+    val name: String,
+    val size: Long,
+    val mimeType: String
+)
---
+++ mail-common/data/src/test/kotlin/ch/protonmail/android/mailcommon/data/file/UriHelperTest.kt
@@ -0,0 +1,107 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcommon.data.file
+
+import java.io.ByteArrayInputStream
+import java.io.FileNotFoundException
+import android.database.Cursor
+import android.net.Uri
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import me.proton.core.test.kotlin.TestDispatcherProvider
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNull
+
+class UriHelperTest {
+
+    private val uri = mockk<Uri>()
+    private val cursor = mockk<Cursor>(relaxUnitFun = true) {
+        every { getColumnIndex(any()) } returns 0
+        every { moveToFirst() } returns true
+        every { getString(any()) } returns TestData.FileName
+        every { getLong(any()) } returns TestData.FileSize
+    }
+    private val contentResolverHelper = mockk<ContentResolverHelper> {
+        coEvery { openInputStream(uri) } returns ByteArrayInputStream(TestData.FileContent)
+        coEvery { getType(uri) } returns TestData.FileMimeType
+        coEvery { query(uri) } returns cursor
+    }
+
+    private val testDispatcherProvider = TestDispatcherProvider()
+    private val uriHelper = UriHelper(testDispatcherProvider, contentResolverHelper)
+
+    @Test
+    fun `should read file content from uri`() = runTest(testDispatcherProvider.Main) {
+        // Given
+        val expected = TestData.FileContent
+
+        // When
+        val actual = uriHelper.readFromUri(uri)
+
+        // Then
+        assertEquals(expected.decodeToString(), actual?.decodeToString())
+    }
+
+    @Test
+    fun `should return null if reading file content from uri fails`() = runTest(testDispatcherProvider.Main) {
+        // Given
+        coEvery { contentResolverHelper.openInputStream(uri) } throws FileNotFoundException()
+
+        // When
+        val actual = uriHelper.readFromUri(uri)
+
+        // Then
+        assertNull(actual)
+    }
+
+    @Test
+    fun `should get file information from uri`() = runTest(testDispatcherProvider.Main) {
+        // Given
+        val expected = FileInformation(TestData.FileName, TestData.FileSize, TestData.FileMimeType)
+
+        // When
+        val actual = uriHelper.getFileInformationFromUri(uri)
+
+        // Then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should return null if one of the file information is null`() = runTest(testDispatcherProvider.Main) {
+        // Given
+        every { cursor.getString(any()) } returns null
+
+        // When
+        val actual = uriHelper.getFileInformationFromUri(uri)
+
+        // Then
+        assertNull(actual)
+    }
+
+    private object TestData {
+
+        val FileContent = "I am a file content".toByteArray()
+        const val FileName = "image.jpg"
+        const val FileSize = 123L
+        const val FileMimeType = "image/jpeg"
+    }
+}
