a64ddd8a1aee28a51ca93b4094cae8ab47b644cb
Johannes Prueller <johannes.prueller@proton.ch>
2023-10-05 18:06:29 +0200

    Add use case for storing parent attachment states
    MAILANDR-1018

---
+++ mail-composer/domain/src/test/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/StoreParentAttachmentStatesTest.kt
@@ -0,0 +1,72 @@
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcommon.domain.sample.UserIdSample
+import ch.protonmail.android.mailcomposer.domain.model.AttachmentSyncState
+import ch.protonmail.android.mailcomposer.domain.repository.AttachmentStateRepository
+import ch.protonmail.android.mailmessage.domain.model.AttachmentId
+import ch.protonmail.android.mailmessage.domain.sample.MessageIdSample
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class StoreParentAttachmentStatesTest {
+
+    private val userId = UserIdSample.Primary
+    private val messageId = MessageIdSample.Invoice
+    private val attachmentIds = listOf(AttachmentId("1"), AttachmentId("2"))
+    private val attachmentStateRepository = mockk<AttachmentStateRepository>()
+
+    private val storeParentAttachmentStates = StoreParentAttachmentStates(attachmentStateRepository)
+
+    @Test
+    fun `when store parent attachments is called then the attachment state repository is called`() = runTest {
+        // Given
+        expectCreateOrUpdateStatesSucceeds()
+
+        // When
+        val actual = storeParentAttachmentStates(userId, messageId, attachmentIds, AttachmentSyncState.Parent)
+
+        // Then
+        assertEquals(Unit.right(), actual)
+    }
+
+    @Test
+    fun `when store parent attachment fails then data error is returned`() = runTest {
+        // Given
+        expectCreateOrUpdateStatesFails()
+
+        // When
+        val actual = storeParentAttachmentStates(userId, messageId, attachmentIds, AttachmentSyncState.Parent)
+
+        // Then
+        assertEquals(DataError.Local.Unknown.left(), actual)
+    }
+
+    private fun expectCreateOrUpdateStatesSucceeds() {
+        coEvery {
+            attachmentStateRepository.createOrUpdateLocalStates(
+                userId,
+                messageId,
+                attachmentIds,
+                AttachmentSyncState.Parent
+            )
+        } returns Unit.right()
+    }
+
+    private fun expectCreateOrUpdateStatesFails() {
+        coEvery {
+            attachmentStateRepository.createOrUpdateLocalStates(
+                userId,
+                messageId,
+                attachmentIds,
+                AttachmentSyncState.Parent
+            )
+        } returns DataError.Local.Unknown.left()
+    }
+
+}
