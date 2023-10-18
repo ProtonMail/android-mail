5155e289183ffb40110e2c5e532a66c02801443c
Johannes Prueller <johannes.prueller@proton.ch>
2023-10-18 17:05:14 +0200

    Add use case to mark messages as read as bulk
    MAILANDR-910

---
+++ mail-mailbox/domain/src/test/kotlin/ch/protonmail/android/mailmailbox/domain/usecase/MarkMessagesAsReadTest.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailmailbox.domain.usecase
+
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcommon.domain.sample.UserIdSample
+import ch.protonmail.android.mailmessage.domain.repository.MessageRepository
+import ch.protonmail.android.mailmessage.domain.sample.MessageIdSample
+import ch.protonmail.android.mailmessage.domain.sample.MessageSample
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MarkMessagesAsReadTest {
+
+    private val userId = UserIdSample.Primary
+    private val messageIds = listOf(MessageIdSample.Invoice, MessageIdSample.PlainTextMessage)
+
+    private val messageRepository = mockk<MessageRepository>()
+
+    private val markRead = MarkMessagesAsRead(messageRepository)
+
+    @Test
+    fun `when repository fails then error is returned`() = runTest {
+        // given
+        val error = DataError.Local.NoDataCached.left()
+        coEvery { messageRepository.markRead(userId, messageIds) } returns error
+
+        // when
+        val result = markRead(userId, messageIds)
+
+        // then
+        assertEquals(error, result)
+    }
+
+    @Test
+    fun `when repository succeed then list of messages is returned`() = runTest {
+        // given
+        val messages = listOf(MessageSample.Invoice, MessageSample.HtmlInvoice).right()
+        coEvery { messageRepository.markRead(userId, messageIds) } returns messages
+
+        // when
+        val result = markRead(userId, messageIds)
+
+        // then
+        assertEquals(messages, result)
+    }
+}
