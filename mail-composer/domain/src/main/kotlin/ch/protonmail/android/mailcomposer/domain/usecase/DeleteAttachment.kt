40c323264c3162289641017abd3e9bb43e091cb2
Johannes Prueller <johannes.prueller@proton.ch>
2023-10-18 16:13:26 +0200

    Fix deprecated import
    NOJIRA

--- mail-composer/domain/src/main/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/DeleteAttachment.kt
+++ mail-composer/domain/src/main/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/DeleteAttachment.kt
@@ -19,7 +19,7 @@
 package ch.protonmail.android.mailcomposer.domain.usecase
 
 import arrow.core.Either
-import arrow.core.continuations.either
+import arrow.core.raise.either
 import ch.protonmail.android.mailcommon.domain.model.DataError
 import ch.protonmail.android.mailcomposer.domain.model.SenderEmail
 import ch.protonmail.android.mailcomposer.domain.repository.AttachmentRepository
