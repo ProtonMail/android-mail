a09c4f6a96c8c42910a9ae06b4c89454d9549530
Niccolò Forlini <niccolo.forlini@proton.ch>
2023-10-31 12:02:31 +0100

    Add Privacy Settings Reducer + ViewModel
    MAILANDR-1057

---
+++ mail-settings/presentation/src/main/kotlin/ch/protonmail/android/mailsettings/presentation/settings/privacy/PrivacySettingsViewModel.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.privacy
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ch.protonmail.android.mailcommon.domain.usecase.ObservePrimaryUserId
+import ch.protonmail.android.mailsettings.domain.model.PrivacySettings
+import ch.protonmail.android.mailsettings.domain.usecase.ObserveMailSettings
+import ch.protonmail.android.mailsettings.presentation.settings.privacy.reducer.PrivacySettingsReducer
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.update
+import me.proton.core.domain.type.IntEnum
+import me.proton.core.mailsettings.domain.entity.MailSettings
+import me.proton.core.mailsettings.domain.entity.ShowImage
+import javax.inject.Inject
+
+@HiltViewModel
+class PrivacySettingsViewModel @Inject constructor(
+    observePrimaryUserId: ObservePrimaryUserId,
+    private val observeMailSettings: ObserveMailSettings,
+    private val privacySettingsReducer: PrivacySettingsReducer
+) : ViewModel() {
+
+    private val mutableState = MutableStateFlow<PrivacySettingsState>(PrivacySettingsState.Loading)
+    val state = mutableState.asStateFlow()
+
+    init {
+        observePrimaryUserId().mapLatest { userId ->
+            userId ?: return@mapLatest emitNewStateFrom(PrivacySettingsEvent.Error.LoadingError)
+
+            val settings = observeMailSettings(userId).firstOrNull()
+                ?: return@mapLatest emitNewStateFrom(PrivacySettingsEvent.Error.LoadingError)
+
+            emitNewStateFrom(PrivacySettingsEvent.Data.ContentLoaded(settings.toPrivacySettings()))
+        }.launchIn(viewModelScope)
+    }
+
+    private fun emitNewStateFrom(event: PrivacySettingsEvent) = mutableState.update {
+        privacySettingsReducer.newStateFrom(it, event)
+    }
+}
+
+private fun MailSettings.toPrivacySettings(): PrivacySettings {
+    return PrivacySettings(
+        autoShowRemoteContent = showImages.isAutoShowRemoteContentEnabled,
+        autoShowEmbeddedImages = showImages.isAutoShowEmbeddedImages,
+        preventTakingScreenshots = false, // to be extracted (MAILANDR-1059)
+        requestLinkConfirmation = confirmLink ?: false
+    )
+}
+
+private val IntEnum<ShowImage>?.isAutoShowRemoteContentEnabled: Boolean
+    get() = this?.enum == ShowImage.Remote || this?.enum == ShowImage.Both
+
+private val IntEnum<ShowImage>?.isAutoShowEmbeddedImages: Boolean
+    get() = this?.enum == ShowImage.Embedded || this?.enum == ShowImage.Both
---
+++ mail-settings/presentation/src/main/kotlin/ch/protonmail/android/mailsettings/presentation/settings/privacy/reducer/PrivacySettingsReducer.kt
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.privacy.reducer
+
+import ch.protonmail.android.mailcommon.presentation.Effect
+import ch.protonmail.android.mailsettings.presentation.settings.privacy.PrivacySettingsEvent
+import ch.protonmail.android.mailsettings.presentation.settings.privacy.PrivacySettingsState
+import javax.inject.Inject
+
+class PrivacySettingsReducer @Inject constructor() {
+
+    fun newStateFrom(currentState: PrivacySettingsState, event: PrivacySettingsEvent) =
+        currentState.toNewStateFromEvent(event)
+
+    private fun PrivacySettingsState.toNewStateFromEvent(event: PrivacySettingsEvent): PrivacySettingsState {
+
+        return when (this) {
+            is PrivacySettingsState.WithData -> when (event) {
+                is PrivacySettingsEvent.Data.ContentLoaded -> this
+                is PrivacySettingsEvent.Data.AutoLoadRemoteContentChanged ->
+                    copy(settings = settings.copy(autoShowRemoteContent = event.newValue))
+
+                is PrivacySettingsEvent.Data.AutoShowEmbeddedImagesChanged ->
+                    copy(settings = settings.copy(autoShowEmbeddedImages = event.newValue))
+
+                is PrivacySettingsEvent.Data.PreventScreenshotsChanged ->
+                    copy(settings = settings.copy(preventTakingScreenshots = event.newValue))
+
+                is PrivacySettingsEvent.Data.RequestLinkConfirmationChanged ->
+                    copy(settings = settings.copy(requestLinkConfirmation = event.newValue))
+
+                is PrivacySettingsEvent.Error ->
+                    copy(updateSettingsError = Effect.of(Unit))
+            }
+
+            is PrivacySettingsState.Loading -> when (event) {
+                is PrivacySettingsEvent.Data.ContentLoaded -> PrivacySettingsState.WithData(
+                    settings = event.settings,
+                    updateSettingsError = Effect.empty()
+                )
+
+                is PrivacySettingsEvent.Error.LoadingError -> PrivacySettingsState.LoadingError
+                else -> this
+            }
+
+            else -> this
+        }
+    }
+}
