3004150869c5bd9ac1eab12dedeaf4e865f13660
Niccolò Forlini <niccolo.forlini@proton.ch>
2023-10-31 12:11:56 +0100

    Add the remaining unit tests for the Privacy Settings screen
    ViewModel + Reducer unit tests.
    
    MAILANDR-1057

---
+++ mail-settings/presentation/src/test/kotlin/ch/protonmail/android/mailsettings/presentation/settings/privacy/PrivacySettingsReducerTest.kt
@@ -0,0 +1,118 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.privacy
+
+import ch.protonmail.android.mailcommon.presentation.Effect
+import ch.protonmail.android.mailsettings.domain.model.PrivacySettings
+import ch.protonmail.android.mailsettings.presentation.settings.privacy.reducer.PrivacySettingsReducer
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import kotlin.test.assertEquals
+
+@RunWith(Parameterized::class)
+internal class PrivacySettingsReducerTest(
+    private val testName: String,
+    private val testInput: TestInput
+) {
+
+    private val privacySettingsReducer = PrivacySettingsReducer()
+
+    @Test
+    fun `should produce the expected new state`() = with(testInput) {
+        val actualState = privacySettingsReducer.newStateFrom(currentState, event)
+        assertEquals(expectedState, actualState, testName)
+    }
+
+    companion object {
+
+        private val baseSettings = PrivacySettings(
+            autoShowRemoteContent = false,
+            autoShowEmbeddedImages = false,
+            preventTakingScreenshots = false,
+            requestLinkConfirmation = false
+        )
+
+        private val transitionFromLoadingState = listOf(
+            TestInput(
+                currentState = PrivacySettingsState.Loading,
+                event = PrivacySettingsEvent.Data.ContentLoaded(baseSettings),
+                expectedState = PrivacySettingsState.WithData(
+                    settings = baseSettings,
+                    updateSettingsError = Effect.empty()
+                )
+            ),
+            TestInput(
+                currentState = PrivacySettingsState.Loading,
+                event = PrivacySettingsEvent.Error.LoadingError,
+                expectedState = PrivacySettingsState.LoadingError
+            )
+        )
+
+        private val transitionsFromLoadedState = listOf(
+            TestInput(
+                currentState = PrivacySettingsState.WithData(baseSettings, updateSettingsError = Effect.empty()),
+                event = PrivacySettingsEvent.Error.UpdateError,
+                expectedState = PrivacySettingsState.WithData(baseSettings, updateSettingsError = Effect.of(Unit))
+            ),
+            TestInput(
+                currentState = PrivacySettingsState.WithData(baseSettings, updateSettingsError = Effect.empty()),
+                event = PrivacySettingsEvent.Data.AutoLoadRemoteContentChanged(newValue = true),
+                expectedState = PrivacySettingsState.WithData(
+                    baseSettings.copy(autoShowRemoteContent = true),
+                    updateSettingsError = Effect.empty()
+                )
+            ),
+            TestInput(
+                currentState = PrivacySettingsState.WithData(baseSettings, updateSettingsError = Effect.empty()),
+                event = PrivacySettingsEvent.Data.AutoShowEmbeddedImagesChanged(newValue = true),
+                expectedState = PrivacySettingsState.WithData(
+                    baseSettings.copy(autoShowEmbeddedImages = true),
+                    updateSettingsError = Effect.empty()
+                )
+            ),
+            TestInput(
+                currentState = PrivacySettingsState.WithData(baseSettings, updateSettingsError = Effect.empty()),
+                event = PrivacySettingsEvent.Data.RequestLinkConfirmationChanged(newValue = true),
+                expectedState = PrivacySettingsState.WithData(
+                    baseSettings.copy(requestLinkConfirmation = true),
+                    updateSettingsError = Effect.empty()
+                )
+            )
+        )
+
+        @JvmStatic
+        @Parameterized.Parameters(name = "{0}")
+        fun data(): Collection<Array<Any>> = (transitionFromLoadingState + transitionsFromLoadedState)
+            .map { testInput ->
+                val testName = """
+                    Current state: ${testInput.currentState}
+                    Event: ${testInput.event}
+                    Next state: ${testInput.expectedState}        
+                """.trimIndent()
+                arrayOf(testName, testInput)
+            }
+    }
+
+    data class TestInput(
+        val currentState: PrivacySettingsState,
+        val event: PrivacySettingsEvent,
+        val expectedState: PrivacySettingsState
+    )
+}
---
+++ mail-settings/presentation/src/test/kotlin/ch/protonmail/android/mailsettings/presentation/settings/privacy/PrivacySettingsViewModelTest.kt
@@ -0,0 +1,328 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.privacy
+
+import app.cash.turbine.test
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcommon.domain.usecase.ObservePrimaryUserId
+import ch.protonmail.android.mailcommon.presentation.Effect
+import ch.protonmail.android.mailsettings.domain.model.PrivacySettings
+import ch.protonmail.android.mailsettings.domain.usecase.ObserveMailSettings
+import ch.protonmail.android.mailsettings.domain.usecase.privacy.UpdateAutoShowEmbeddedImagesSetting
+import ch.protonmail.android.mailsettings.domain.usecase.privacy.UpdateLinkConfirmationSetting
+import ch.protonmail.android.mailsettings.domain.usecase.privacy.UpdateShowRemoteContentSetting
+import ch.protonmail.android.mailsettings.presentation.settings.privacy.reducer.PrivacySettingsReducer
+import ch.protonmail.android.testdata.mailsettings.MailSettingsTestData
+import ch.protonmail.android.testdata.user.UserIdTestData
+import io.mockk.called
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.confirmVerified
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.spyk
+import io.mockk.unmockkAll
+import io.mockk.verify
+import io.mockk.verifySequence
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import me.proton.core.domain.entity.UserId
+import me.proton.core.domain.type.IntEnum
+import me.proton.core.mailsettings.domain.entity.ShowImage
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+internal class PrivacySettingsViewModelTest {
+
+    private val observePrimaryUserId = mockk<ObservePrimaryUserId>()
+    private val observeMailSettings = mockk<ObserveMailSettings>()
+    private val updateShowRemoteContentSettings = mockk<UpdateShowRemoteContentSetting>()
+    private val updateAutoShowEmbeddedImagesSetting = mockk<UpdateAutoShowEmbeddedImagesSetting>()
+    private val updateLinkConfirmationSetting = mockk<UpdateLinkConfirmationSetting>()
+    private val privacySettingsReducer = spyk<PrivacySettingsReducer>()
+    private val viewModel: PrivacySettingsViewModel by lazy {
+        PrivacySettingsViewModel(
+            observePrimaryUserId,
+            observeMailSettings,
+            updateShowRemoteContentSettings,
+            updateAutoShowEmbeddedImagesSetting,
+            updateLinkConfirmationSetting,
+            privacySettingsReducer
+        )
+    }
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @After
+    fun teardown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `should return loading state when first launched`() = runTest {
+        // Given
+        every { observePrimaryUserId.invoke() } returns flowOf()
+
+        // Then
+        viewModel.state.test {
+            val loadingState = awaitItem()
+            assertEquals(PrivacySettingsState.Loading, loadingState)
+        }
+    }
+
+    @Test
+    fun `should return an error when no primary user id is found`() = runTest {
+        // Given
+        every { observePrimaryUserId.invoke() } returns flowOf(null)
+
+        // Then
+        viewModel.state.test {
+            val errorState = awaitItem()
+            assertEquals(PrivacySettingsState.LoadingError, errorState)
+        }
+        verify(exactly = 1) {
+            privacySettingsReducer.newStateFrom(
+                PrivacySettingsState.Loading,
+                PrivacySettingsEvent.Error.LoadingError
+            )
+        }
+        confirmVerified(privacySettingsReducer)
+    }
+
+    @Test
+    fun `should return an error when settings cannot be fetched`() = runTest {
+        // Given
+        val userId = UserId("123")
+        every { observePrimaryUserId.invoke() } returns flowOf(userId)
+        every { observeMailSettings.invoke(userId) } returns flowOf(null)
+
+        // Then
+        viewModel.state.test {
+            val errorState = awaitItem()
+            assertEquals(PrivacySettingsState.LoadingError, errorState)
+        }
+        verify(exactly = 1) {
+            privacySettingsReducer.newStateFrom(
+                PrivacySettingsState.Loading,
+                PrivacySettingsEvent.Error.LoadingError
+            )
+        }
+        confirmVerified(privacySettingsReducer)
+    }
+
+    @Test
+    fun `should load content data when userId and settings are fetched correctly`() = runTest {
+        // Given
+        expectValidSettingsLoaded()
+
+        val expectedState = PrivacySettingsState.WithData(
+            basePrivacySettings,
+            Effect.empty()
+        )
+
+        // Then
+        viewModel.state.test {
+            assertEquals(expectedState, awaitItem())
+        }
+        verify(exactly = 1) {
+            privacySettingsReducer.newStateFrom(
+                PrivacySettingsState.Loading,
+                PrivacySettingsEvent.Data.ContentLoaded(basePrivacySettings)
+            )
+        }
+        confirmVerified(privacySettingsReducer)
+    }
+
+    @Test
+    fun `should return an update error when updating remote content setting fails`() = runTest {
+        // Given
+        expectValidSettingsLoaded()
+        coEvery { updateShowRemoteContentSettings(any()) } returns DataError.Local.NoDataCached.left()
+
+        // When + Then
+        verifyUpdateError { viewModel.onAutoShowRemoteContentToggled(true) }
+    }
+
+    @Test
+    fun `should return an update error when updating embedded image setting fails`() = runTest {
+        // Given
+        expectValidSettingsLoaded()
+        coEvery { updateAutoShowEmbeddedImagesSetting(any()) } returns DataError.Local.NoDataCached.left()
+
+        // When + Then
+        verifyUpdateError { viewModel.onAutoShowEmbeddedImagesToggled(true) }
+    }
+
+    @Test
+    fun `should return an update error when updating link confirmation setting fails`() = runTest {
+        // Given
+        expectValidSettingsLoaded()
+        coEvery { updateLinkConfirmationSetting(any()) } returns DataError.Local.NoDataCached.left()
+
+        // When + Then
+        verifyUpdateError { viewModel.onConfirmLinkToggled(true) }
+    }
+
+    @Test
+    fun `should call the correct use case and update state when remote content setting update is done`() = runTest {
+        // Given
+        expectValidSettingsLoaded()
+        val expectedFinalState = PrivacySettingsState.WithData(
+            basePrivacySettings.copy(autoShowRemoteContent = false),
+            Effect.empty()
+        )
+        coEvery { updateShowRemoteContentSettings(any()) } returns Unit.right()
+
+        // When
+        viewModel.state.test {
+            skipItems(1)
+
+            // When
+            viewModel.onAutoShowRemoteContentToggled(false)
+
+            // Then
+            assertEquals(expectedFinalState, awaitItem())
+        }
+
+        // Then
+        coVerify(exactly = 1) {
+            updateShowRemoteContentSettings(false)
+        }
+        confirmVerified(updateShowRemoteContentSettings)
+        verify {
+            updateAutoShowEmbeddedImagesSetting wasNot called
+            updateLinkConfirmationSetting wasNot called
+        }
+    }
+
+    @Test
+    fun `should call the correct use case and update state when embedded images setting update is done`() = runTest {
+        // Given
+        expectValidSettingsLoaded()
+        val expectedFinalState = PrivacySettingsState.WithData(
+            basePrivacySettings.copy(autoShowEmbeddedImages = false),
+            Effect.empty()
+        )
+        coEvery { updateAutoShowEmbeddedImagesSetting(any()) } returns Unit.right()
+
+        // When
+        viewModel.state.test {
+            skipItems(1)
+
+            // When
+            viewModel.onAutoShowEmbeddedImagesToggled(false)
+
+            // Then
+            assertEquals(expectedFinalState, awaitItem())
+        }
+
+        // Then
+        coVerify(exactly = 1) {
+            updateAutoShowEmbeddedImagesSetting(false)
+        }
+        confirmVerified(updateAutoShowEmbeddedImagesSetting)
+        verify {
+            updateShowRemoteContentSettings wasNot called
+            updateLinkConfirmationSetting wasNot called
+        }
+    }
+
+    @Test
+    fun `should call the correct use case and update state when link confirmation setting update is done`() = runTest {
+        // Given
+        expectValidSettingsLoaded()
+        val expectedFinalState = PrivacySettingsState.WithData(
+            basePrivacySettings.copy(requestLinkConfirmation = false),
+            Effect.empty()
+        )
+        coEvery { updateLinkConfirmationSetting(any()) } returns Unit.right()
+
+        // When
+        viewModel.state.test {
+            skipItems(1)
+
+            // When
+            viewModel.onConfirmLinkToggled(false)
+
+            // Then
+            assertEquals(expectedFinalState, awaitItem())
+        }
+
+        // Then
+        coVerify(exactly = 1) {
+            updateLinkConfirmationSetting(false)
+        }
+        confirmVerified(updateAutoShowEmbeddedImagesSetting)
+        verify {
+            updateShowRemoteContentSettings wasNot called
+            updateAutoShowEmbeddedImagesSetting wasNot called
+        }
+    }
+
+    private suspend fun verifyUpdateError(action: PrivacySettingsViewModel.() -> Unit) {
+        val expectedFinalState = PrivacySettingsState.WithData(basePrivacySettings, Effect.of(Unit))
+        viewModel.state.test {
+            skipItems(1) // Skip loading state
+            action(viewModel)
+            assertEquals(expectedFinalState, awaitItem())
+        }
+        // Then
+        verifySequence {
+            privacySettingsReducer.newStateFrom(
+                PrivacySettingsState.Loading,
+                PrivacySettingsEvent.Data.ContentLoaded(basePrivacySettings)
+            )
+            privacySettingsReducer.newStateFrom(
+                PrivacySettingsState.WithData(basePrivacySettings, Effect.empty()),
+                PrivacySettingsEvent.Error.UpdateError
+            )
+        }
+        confirmVerified(privacySettingsReducer)
+    }
+
+    private fun expectValidSettingsLoaded() {
+        every { observePrimaryUserId.invoke() } returns flowOf(userId)
+        every { observeMailSettings.invoke(userId) } returns flowOf(mailSettings)
+    }
+
+    private companion object {
+
+        val userId = UserIdTestData.userId
+        val mailSettings = MailSettingsTestData.buildMailSettings(
+            showImages = IntEnum(3, ShowImage.Both),
+            confirmLink = true
+        )
+        val basePrivacySettings = PrivacySettings(
+            autoShowRemoteContent = true,
+            autoShowEmbeddedImages = true,
+            preventTakingScreenshots = false,
+            requestLinkConfirmation = true
+        )
+    }
+}
