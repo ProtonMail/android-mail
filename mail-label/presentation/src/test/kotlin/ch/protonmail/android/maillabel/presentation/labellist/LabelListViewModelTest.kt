d129e53ab3ea7de97910cd3eebdc1cfc86c10445
Benjamin GARCIA <benjamin.garcia@proton.ch>
2023-11-08 10:27:41 +0100

    Create LabelListViewModelTest

---
+++ mail-label/presentation/src/test/kotlin/ch/protonmail/android/maillabel/presentation/labellist/LabelListViewModelTest.kt
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.maillabel.presentation.labellist
+
+import android.util.Log
+import app.cash.turbine.test
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.usecase.ObservePrimaryUserId
+import ch.protonmail.android.maillabel.domain.usecase.ObserveLabels
+import ch.protonmail.android.testdata.label.LabelTestData
+import ch.protonmail.android.testdata.user.UserIdTestData.userId
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkStatic
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import me.proton.core.label.domain.entity.Label
+import org.junit.Test
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.assertEquals
+
+class LabelListViewModelTest {
+
+    private val observePrimaryUserId = mockk<ObservePrimaryUserId> {
+        every { this@mockk.invoke() } returns flowOf(userId)
+    }
+
+    private val observeLabels = mockk<ObserveLabels> {
+        every { this@mockk.invoke(userId) } returns flowOf(
+            listOf(
+                LabelTestData.buildLabel(id = "LabelID")
+            ).right()
+        )
+    }
+
+    private val labelListViewModel by lazy {
+        LabelListViewModel(
+            observeLabels,
+            observePrimaryUserId
+        )
+    }
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        mockkStatic(Log::class)
+        every { Log.isLoggable(any(), any()) } returns false
+    }
+
+    @AfterTest
+    fun tearDown() {
+        Dispatchers.resetMain()
+        unmockkStatic(Log::class)
+    }
+
+    @Test
+    fun `emits initial empty state when initialized`() = runTest {
+        // Given
+        coEvery { observePrimaryUserId() } returns flowOf(userId)
+        coEvery { observeLabels(userId = any()) } returns flowOf(emptyList<Label>().right())
+
+        // When
+        labelListViewModel.state.test {
+            // Then
+            val actual = awaitItem()
+            val expected = LabelListState.EmptyLabelList
+
+            assertEquals(expected, actual)
+        }
+    }
+
+    @Test
+    fun `emits initial data state when initialized`() = runTest {
+        // When
+        labelListViewModel.state.test {
+            // Then
+            val actual = awaitItem()
+            val expected = LabelListState.Data(
+                listOf(LabelTestData.buildLabel(id = "LabelID"))
+            )
+
+            assertEquals(expected, actual)
+        }
+    }
+
+}
