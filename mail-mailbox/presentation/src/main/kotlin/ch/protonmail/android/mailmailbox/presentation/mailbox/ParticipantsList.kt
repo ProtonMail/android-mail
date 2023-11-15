ba80756e9458a858fd46026953adb56a88447bed
Serdar Ozturk <info@fogosoft.co.uk>
2023-11-15 01:15:58 +0000

    Cache repeated minWidth calculations
    (cherry picked from commit 8de1df103616ae3a48cb4bee04789ecfdc2ced70)

--- mail-mailbox/presentation/src/main/kotlin/ch/protonmail/android/mailmailbox/presentation/mailbox/ParticipantsList.kt
+++ mail-mailbox/presentation/src/main/kotlin/ch/protonmail/android/mailmailbox/presentation/mailbox/ParticipantsList.kt
@@ -54,30 +54,43 @@ fun ParticipantsList(
     SubcomposeLayout(
         modifier = modifier
     ) { constraints ->
-        val participantWithBadgeAndSeparatorMinWidth = measureMinParticipantRowWidth(
-            id = participantWithBadgeAndSeparatorId,
-            constraints = constraints,
-            participant = ParticipantUiModel(minLengthParticipantName, shouldShowOfficialBadge = true),
-            displayData = ParticipantRowDisplayData(shouldShowSeparator = true, fontWeight, fontColor)
-        )
-        val participantWithBadgeMinWidth = measureMinParticipantRowWidth(
-            id = participantWithBadgeId,
-            constraints = constraints,
-            participant = ParticipantUiModel(minLengthParticipantName, shouldShowOfficialBadge = true),
-            displayData = ParticipantRowDisplayData(shouldShowSeparator = false, fontWeight, fontColor)
-        )
-        val participantWithSeparatorMinWidth = measureMinParticipantRowWidth(
-            id = participantWithSeparatorId,
-            constraints = constraints,
-            participant = ParticipantUiModel(minLengthParticipantName, shouldShowOfficialBadge = false),
-            displayData = ParticipantRowDisplayData(shouldShowSeparator = true, fontWeight, fontColor)
-        )
-        val participantNameMinWidth = measureMinParticipantRowWidth(
-            id = participantNameId,
-            constraints = constraints,
-            participant = ParticipantUiModel(minLengthParticipantName, shouldShowOfficialBadge = false),
-            displayData = ParticipantRowDisplayData(shouldShowSeparator = false, fontWeight, fontColor)
-        )
+
+        val participantWithBadgeAndSeparatorMinWidth =
+            MeasurementCache.getMinWidthWithBadgeAndSeparator(fontWeight).takeIf { it > 0 }
+                ?: measureMinParticipantRowWidth(
+                    id = participantWithBadgeAndSeparatorId,
+                    constraints = constraints,
+                    participant = ParticipantUiModel(minLengthParticipantName, shouldShowOfficialBadge = true),
+                    displayData = ParticipantRowDisplayData(shouldShowSeparator = true, fontWeight, fontColor),
+                    cacheSetter = MeasurementCache::setMinWidthWithBadgeAndSeparator
+                )
+
+        val participantWithBadgeMinWidth = MeasurementCache.getMinWidthWithBadge(fontWeight).takeIf { it > 0 }
+            ?: measureMinParticipantRowWidth(
+                id = participantWithBadgeId,
+                constraints = constraints,
+                participant = ParticipantUiModel(minLengthParticipantName, shouldShowOfficialBadge = true),
+                displayData = ParticipantRowDisplayData(shouldShowSeparator = false, fontWeight, fontColor),
+                cacheSetter = MeasurementCache::setMinWidthWithBadge
+            )
+
+        val participantWithSeparatorMinWidth = MeasurementCache.getMinWidthWithSeparator(fontWeight).takeIf { it > 0 }
+            ?: measureMinParticipantRowWidth(
+                id = participantWithSeparatorId,
+                constraints = constraints,
+                participant = ParticipantUiModel(minLengthParticipantName, shouldShowOfficialBadge = false),
+                displayData = ParticipantRowDisplayData(shouldShowSeparator = true, fontWeight, fontColor),
+                cacheSetter = MeasurementCache::setMinWidthWithSeparator
+            )
+
+        val participantNameMinWidth = MeasurementCache.getMinWidthName(fontWeight).takeIf { it > 0 }
+            ?: measureMinParticipantRowWidth(
+                id = participantNameId,
+                constraints = constraints,
+                participant = ParticipantUiModel(minLengthParticipantName, shouldShowOfficialBadge = false),
+                displayData = ParticipantRowDisplayData(shouldShowSeparator = false, fontWeight, fontColor),
+                cacheSetter = MeasurementCache::setMinWidthName
+            )
 
         val measurables = participants.list.mapIndexed { index, participantUiModel ->
             subcompose("${participantUiModel.name}$index") {
@@ -146,13 +159,15 @@ private fun SubcomposeMeasureScope.measureMinParticipantRowWidth(
     id: String,
     constraints: Constraints,
     participant: ParticipantUiModel,
-    displayData: ParticipantRowDisplayData
+    displayData: ParticipantRowDisplayData,
+    cacheSetter: (FontWeight, Int) -> Unit
 ) = subcompose(id) {
     ParticipantRow(
         participant = participant,
         displayData = displayData
     )
 }.maxOf { it.measure(constraints).width }
+    .also { cacheSetter(displayData.fontWeight, it) }
 
 private fun shouldShowSeparator(index: Int) = index != 0
 
@@ -223,6 +238,41 @@ private fun ThreeDots(
         text = "...",
         style = ProtonTheme.typography.defaultNorm.copy(fontWeight = fontWeight, color = fontColor)
     )
+}
+
+// Participant subcompose() measurements are consuming too much CPU time since they are repeated for each row
+// Min values are produced from constant strings therefore we can cache their values
+object MeasurementCache {
+
+    private val minWidthMapWithBadgeAndSeparator = mutableMapOf<FontWeight, Int>()
+    private val minWidthMapWithBadge = mutableMapOf<FontWeight, Int>()
+    private val minWidthMapWithSeparator = mutableMapOf<FontWeight, Int>()
+    private val minWidthMapName = mutableMapOf<FontWeight, Int>()
+
+    fun setMinWidthWithBadgeAndSeparator(fontWeight: FontWeight, value: Int) {
+        minWidthMapWithBadgeAndSeparator[fontWeight] = value
+    }
+
+    fun setMinWidthWithBadge(fontWeight: FontWeight, value: Int) {
+        minWidthMapWithBadge[fontWeight] = value
+    }
+
+    fun setMinWidthWithSeparator(fontWeight: FontWeight, value: Int) {
+        minWidthMapWithSeparator[fontWeight] = value
+    }
+
+    fun setMinWidthName(fontWeight: FontWeight, value: Int) {
+        minWidthMapName[fontWeight] = value
+    }
+
+    fun getMinWidthWithBadgeAndSeparator(fontWeight: FontWeight): Int =
+        minWidthMapWithBadgeAndSeparator[fontWeight] ?: 0
+
+    fun getMinWidthWithBadge(fontWeight: FontWeight): Int = minWidthMapWithBadge[fontWeight] ?: 0
+
+    fun getMinWidthWithSeparator(fontWeight: FontWeight): Int = minWidthMapWithSeparator[fontWeight] ?: 0
+
+    fun getMinWidthName(fontWeight: FontWeight): Int = minWidthMapName[fontWeight] ?: 0
 }
 
 data class ParticipantRowDisplayData(
