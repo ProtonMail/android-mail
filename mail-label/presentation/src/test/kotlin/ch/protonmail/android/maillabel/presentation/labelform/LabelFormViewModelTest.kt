3cc7b85d2c71dc3e0dfda429e3c69056e7810f50
Benjamin GARCIA <benjamin.garcia@proton.ch>
2023-12-01 11:34:34 +0100

    Updated tests to add loader value.
    Removed spyk from label VM tests.

--- mail-label/presentation/src/test/kotlin/ch/protonmail/android/maillabel/presentation/labelform/LabelFormViewModelTest.kt
+++ mail-label/presentation/src/test/kotlin/ch/protonmail/android/maillabel/presentation/labelform/LabelFormViewModelTest.kt
@@ -228,6 +228,7 @@ class LabelFormViewModelTest {
                 loadedState.copy(
                     name = defaultTestUpdatedName,
                     isSaveEnabled = true,
+                    displayCreateLoader = true,
                     closeWithSave = Effect.of(Unit)
                 ),
                 awaitItem()
