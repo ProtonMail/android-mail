b0f721aeac72b59acf7a8bd0faeb7a2e245f87ab
Niccolò Forlini <niccolo.forlini@proton.ch>
2023-12-14 15:26:05 +0100

    Add unit tests for AutoLockRepositoryImpl
    Split in two classes as the suite was getting a bit too crowded.
    
    MAILANDR-1277

---
+++ mail-settings/data/src/test/kotlin/ch/protonmail/android/mailsettings/data/repository/AutoLockRepositoryFetchImplTest.kt
@@ -0,0 +1,243 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.data.repository
+
+import java.io.IOException
+import app.cash.turbine.test
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.PreferencesError
+import ch.protonmail.android.mailsettings.data.repository.local.AutoLockLocalDataSource
+import ch.protonmail.android.mailsettings.data.usecase.DecryptSerializableValue
+import ch.protonmail.android.mailsettings.data.usecase.EncryptSerializableValue
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockEnabledEncryptedValue
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockEncryptedInterval
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockEncryptedPin
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockInterval
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockLastEncryptedForegroundMillis
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockLastForegroundMillis
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockPin
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockPreference
+import ch.protonmail.android.mailsettings.domain.repository.AutoLockPreferenceError
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import me.proton.core.crypto.common.keystore.KeyStoreCrypto
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class AutoLockRepositoryFetchImplTest {
+
+    private val autoLockLocalDataSource = mockk<AutoLockLocalDataSource>()
+    private val keyStoreCrypto = mockk<KeyStoreCrypto>()
+
+    // Use cases here expose inline functions, cannot be mocked/spied.
+    private val encryptSerializableValue = EncryptSerializableValue(keyStoreCrypto)
+    private val decryptSerializableValue = DecryptSerializableValue(keyStoreCrypto)
+
+    private val autoLockRepository = AutoLockRepositoryImpl(
+        autoLockLocalDataSource,
+        encryptSerializableValue,
+        decryptSerializableValue
+    )
+
+    @After
+    fun teardown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `should propagate data correctly when auto lock enabled is observed`() = runTest {
+        // Given
+        val expectedValue = AutoLockPreference(isEnabled = false)
+        every { autoLockLocalDataSource.observeAutoLockEnabledEncryptedValue() } returns flowOf(
+            AutoLockEnabledEncryptedValue(BaseEncryptedDummyValue).right()
+        )
+
+        expectSuccessfulDecryption(expectedValue.isEnabled.toString())
+
+        // When + Then
+        autoLockRepository.observeAutoLockEnabledValue().assertValue(expectedValue)
+    }
+
+    @Test
+    fun `should propagate data store error when auto lock enabled cannot be retrieved`() = runTest {
+        // Given
+        val expectedValue = AutoLockPreferenceError.DataStoreError
+        every { autoLockLocalDataSource.observeAutoLockEnabledEncryptedValue() } returns flowOf(PreferencesError.left())
+
+        // When + Then
+        autoLockRepository.observeAutoLockEnabledValue().assertError(expectedValue)
+    }
+
+    @Test
+    fun `should propagate deserialization error when auto lock enabled cannot be decoded`() = runTest {
+        // Given
+        every { autoLockLocalDataSource.observeAutoLockEnabledEncryptedValue() } returns flowOf(
+            AutoLockEnabledEncryptedValue(BaseEncryptedDummyValue).right()
+        )
+
+        expectDecryptionError()
+
+        // When + Then
+        autoLockRepository.observeAutoLockEnabledValue().assertError(baseDecryptionError)
+    }
+
+    @Test
+    fun `should propagate data correctly when auto lock interval is observed`() = runTest {
+        // Given
+        val expectedValue = AutoLockInterval.NotEnabled
+        every { autoLockLocalDataSource.observeAutoLockEncryptedInterval() } returns flowOf(
+            AutoLockEncryptedInterval(BaseEncryptedDummyValue).right()
+        )
+
+        expectSuccessfulDecryption("\"${expectedValue}\"")
+
+        // When + Then
+        autoLockRepository.observeAutoLockInterval().assertValue(expectedValue)
+    }
+
+    @Test
+    fun `should propagate data store error when last auto lock interval cannot be retrieved`() = runTest {
+        // Given
+        val expectedValue = AutoLockPreferenceError.DataStoreError
+        every { autoLockLocalDataSource.observeAutoLockEncryptedInterval() } returns flowOf(PreferencesError.left())
+
+        // When + Then
+        autoLockRepository.observeAutoLockInterval().assertError(expectedValue)
+    }
+
+    @Test
+    fun `should propagate deserialization error when auto lock interval cannot be decoded`() = runTest {
+        // Given
+        every { autoLockLocalDataSource.observeAutoLockEncryptedInterval() } returns flowOf(
+            AutoLockEncryptedInterval(BaseEncryptedDummyValue).right()
+        )
+
+        expectDecryptionError()
+
+        // When + Then
+        autoLockRepository.observeAutoLockInterval().assertError(baseDecryptionError)
+    }
+
+    @Test
+    fun `should propagate data correctly when last foreground timestamp is observed`() = runTest {
+        // Given
+        val expectedValue = AutoLockLastForegroundMillis(123L)
+        every { autoLockLocalDataSource.observeLastEncryptedForegroundMillis() } returns flowOf(
+            AutoLockLastEncryptedForegroundMillis(BaseEncryptedDummyValue).right()
+        )
+
+        expectSuccessfulDecryption("\"${expectedValue.value}\"")
+
+        // When + Then
+        autoLockRepository.observeAutoLockLastForegroundMillis().assertValue(expectedValue)
+    }
+
+    @Test
+    fun `should propagate data store error when last foreground timestamp cannot be retrieved`() = runTest {
+        // Given
+        val expectedValue = AutoLockPreferenceError.DataStoreError
+        every { autoLockLocalDataSource.observeLastEncryptedForegroundMillis() } returns flowOf(PreferencesError.left())
+
+        // When + Then
+        autoLockRepository.observeAutoLockLastForegroundMillis().assertError(expectedValue)
+    }
+
+    @Test
+    fun `should propagate deserialization error when last foreground timestamp cannot be decoded`() = runTest {
+        // Given
+        every { autoLockLocalDataSource.observeLastEncryptedForegroundMillis() } returns flowOf(
+            AutoLockLastEncryptedForegroundMillis(BaseEncryptedDummyValue).right()
+        )
+
+        expectDecryptionError()
+
+        // When + Then
+        autoLockRepository.observeAutoLockLastForegroundMillis().assertError(baseDecryptionError)
+    }
+
+    @Test
+    fun `should propagate data correctly when pin data is observed`() = runTest {
+        // Given
+        val expectedValue = AutoLockPin("1234")
+        every { autoLockLocalDataSource.observeAutoLockEncryptedPin() } returns flowOf(
+            AutoLockEncryptedPin(BaseEncryptedDummyValue).right()
+        )
+
+        expectSuccessfulDecryption("\"${expectedValue.value}\"")
+
+        // When + Then
+        autoLockRepository.observeAutoLockPin().assertValue(expectedValue)
+    }
+
+    @Test
+    fun `should propagate data store error when encrypted pin data cannot be retrieved`() = runTest {
+        // Given
+        val expectedValue = AutoLockPreferenceError.DataStoreError
+        every { autoLockLocalDataSource.observeAutoLockEncryptedPin() } returns flowOf(PreferencesError.left())
+
+        // When + Then
+        autoLockRepository.observeAutoLockPin().assertError(expectedValue)
+    }
+
+    @Test
+    fun `should propagate deserialization error when encrypted pin cannot be decoded`() = runTest {
+        // Given
+        every { autoLockLocalDataSource.observeAutoLockEncryptedPin() } returns flowOf(
+            AutoLockEncryptedPin(BaseEncryptedDummyValue).right()
+        )
+
+        expectDecryptionError()
+
+        // When + Then
+        autoLockRepository.observeAutoLockPin().assertError(baseDecryptionError)
+    }
+
+    private fun expectSuccessfulDecryption(value: String) {
+        coEvery { keyStoreCrypto.decrypt(any<String>()) } returns value
+    }
+
+    private fun expectDecryptionError() {
+        coEvery { keyStoreCrypto.decrypt(any<String>()) } throws IOException(baseDecryptionError.message)
+    }
+
+    private suspend fun <T> Flow<T>.assertError(error: AutoLockPreferenceError) = test {
+        val item = awaitItem()
+        assertEquals(error.left(), item)
+        awaitComplete()
+    }
+
+    private suspend fun <T> Flow<T>.assertValue(value: T) = test {
+        val item = awaitItem()
+        assertEquals(value.right(), item)
+        awaitComplete()
+    }
+
+    private companion object {
+
+        val baseDecryptionError = AutoLockPreferenceError.DeserializationError("message")
+        const val BaseEncryptedDummyValue = "somevalue"
+    }
+}
---
+++ mail-settings/data/src/test/kotlin/ch/protonmail/android/mailsettings/data/repository/AutoLockRepositoryImplUpdateTest.kt
@@ -0,0 +1,275 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.data.repository
+
+import java.io.IOException
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.PreferencesError
+import ch.protonmail.android.mailsettings.data.repository.local.AutoLockLocalDataSource
+import ch.protonmail.android.mailsettings.data.usecase.DecryptSerializableValue
+import ch.protonmail.android.mailsettings.data.usecase.EncryptSerializableValue
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockEnabledEncryptedValue
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockEncryptedInterval
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockEncryptedPin
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockInterval
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockLastEncryptedForegroundMillis
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockLastForegroundMillis
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockPin
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockPreference
+import ch.protonmail.android.mailsettings.domain.repository.AutoLockPreferenceError
+import io.mockk.coEvery
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import kotlinx.coroutines.test.runTest
+import me.proton.core.crypto.common.keystore.KeyStoreCrypto
+import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class AutoLockRepositoryImplUpdateTest {
+
+    private val autoLockLocalDataSource = mockk<AutoLockLocalDataSource>()
+    private val keyStoreCrypto = mockk<KeyStoreCrypto>()
+
+    // Use cases here expose inline functions, cannot be mocked/spied.
+    private val encryptSerializableValue = EncryptSerializableValue(keyStoreCrypto)
+    private val decryptSerializableValue = DecryptSerializableValue(keyStoreCrypto)
+
+    private val autoLockRepository = AutoLockRepositoryImpl(
+        autoLockLocalDataSource,
+        encryptSerializableValue,
+        decryptSerializableValue
+    )
+
+    @After
+    fun teardown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `should return unit when auto lock enabled update is successful`() = runTest {
+        // Given
+        val autoLockPreference = AutoLockPreference(true)
+        val autoLockEncryptedPreference = AutoLockEnabledEncryptedValue("encrypted")
+        coEvery {
+            autoLockLocalDataSource.updateAutoLockEnabledEncryptedValue(autoLockEncryptedPreference)
+        } returns Unit.right()
+        expectSuccessfulEncryption()
+
+        // When
+        val result = autoLockRepository.updateAutoLockEnabledValue(autoLockPreference)
+
+        // Then
+        assertEquals(Unit.right(), result)
+    }
+
+    @Test
+    fun `should propagate data store error when auto lock value update is not successful`() = runTest {
+        // Given
+        val autoLockPreference = AutoLockPreference(true)
+        val autoLockEncryptedPreference = AutoLockEnabledEncryptedValue("encrypted")
+        coEvery {
+            autoLockLocalDataSource.updateAutoLockEnabledEncryptedValue(autoLockEncryptedPreference)
+        } returns PreferencesError.left()
+
+        expectSuccessfulEncryption()
+
+        // When
+        val result = autoLockRepository.updateAutoLockEnabledValue(autoLockPreference)
+
+        // Then
+        assertEquals(AutoLockPreferenceError.DataStoreError.left(), result)
+    }
+
+    @Test
+    fun `should propagate encryption error when auto lock value encryption is not successful`() = runTest {
+        // Given
+        val autoLockPreference = AutoLockPreference(true)
+        expectEncryptionError()
+
+        // When
+        val result = autoLockRepository.updateAutoLockEnabledValue(autoLockPreference)
+
+        // Then
+        assertEquals(baseEncryptionError.left(), result)
+    }
+
+    @Test
+    fun `should return unit when auto lock interval update is successful`() = runTest {
+        // Given
+        val autoLockIntervalPreference = AutoLockInterval.NotEnabled
+        val autoLockIntervalEncryptedPreference = AutoLockEncryptedInterval("encrypted")
+        coEvery {
+            autoLockLocalDataSource.updateAutoLockEncryptedInterval(autoLockIntervalEncryptedPreference)
+        } returns Unit.right()
+
+        expectSuccessfulEncryption()
+
+        // When
+        val result = autoLockRepository.updateAutoLockInterval(autoLockIntervalPreference)
+
+        // Then
+        assertEquals(Unit.right(), result)
+    }
+
+    @Test
+    fun `should propagate data store error when auto lock interval update is not successful`() = runTest {
+        // Given
+        val autoLockIntervalPreference = AutoLockInterval.NotEnabled
+        val autoLockIntervalEncryptedPreference = AutoLockEncryptedInterval("encrypted")
+        coEvery {
+            autoLockLocalDataSource.updateAutoLockEncryptedInterval(autoLockIntervalEncryptedPreference)
+        } returns PreferencesError.left()
+
+        expectSuccessfulEncryption()
+
+        // When
+        val result = autoLockRepository.updateAutoLockInterval(autoLockIntervalPreference)
+
+        // Then
+        assertEquals(AutoLockPreferenceError.DataStoreError.left(), result)
+    }
+
+    @Test
+    fun `should propagate encryption error when auto lock interval update encryption is not successful`() = runTest {
+        // Given
+        val autoLockIntervalPreference = AutoLockInterval.NotEnabled
+        expectEncryptionError()
+
+        // When
+        val result = autoLockRepository.updateAutoLockInterval(autoLockIntervalPreference)
+
+        // Then
+        assertEquals(baseEncryptionError.left(), result)
+    }
+
+    @Test
+    fun `should return unit when auto lock foreground timestamp update is successful`() = runTest {
+        // Given
+        val autoLockLastForegroundTimestamp = AutoLockLastForegroundMillis(123L)
+        val autoLockLastForegroundEncryptedTimestamp = AutoLockLastEncryptedForegroundMillis("encrypted")
+        coEvery {
+            autoLockLocalDataSource.updateLastEncryptedForegroundMillis(autoLockLastForegroundEncryptedTimestamp)
+        } returns Unit.right()
+
+        expectSuccessfulEncryption()
+
+        // When
+        val result = autoLockRepository.updateLastForegroundMillis(autoLockLastForegroundTimestamp)
+
+        // Then
+        assertEquals(Unit.right(), result)
+    }
+
+    @Test
+    fun `should propagate data store error when auto lock foreground timestamp update is not successful`() = runTest {
+        // Given
+        val autoLockLastForegroundTimestamp = AutoLockLastForegroundMillis(123L)
+        val autoLockLastForegroundEncryptedTimestamp = AutoLockLastEncryptedForegroundMillis("encrypted")
+        coEvery {
+            autoLockLocalDataSource.updateLastEncryptedForegroundMillis(autoLockLastForegroundEncryptedTimestamp)
+        } returns PreferencesError.left()
+
+        expectSuccessfulEncryption()
+
+        // When
+        val result = autoLockRepository.updateLastForegroundMillis(autoLockLastForegroundTimestamp)
+
+        // Then
+        assertEquals(AutoLockPreferenceError.DataStoreError.left(), result)
+    }
+
+    @Test
+    fun `should propagate encryption error when auto lock foreground timestamp update encryption is not successful`() =
+        runTest {
+            // Given
+            val autoLockLastForegroundTimestamp = AutoLockLastForegroundMillis(123L)
+            expectEncryptionError()
+
+            // When
+            val result = autoLockRepository.updateLastForegroundMillis(autoLockLastForegroundTimestamp)
+
+            // Then
+            assertEquals(baseEncryptionError.left(), result)
+        }
+
+    @Test
+    fun `should return unit when auto lock pin update is successful`() = runTest {
+        // Given
+        val autoLockPin = AutoLockPin("123")
+        val autoLockPinEncrypted = AutoLockEncryptedPin("encrypted")
+        coEvery {
+            autoLockLocalDataSource.updateAutoLockEncryptedPin(autoLockPinEncrypted)
+        } returns Unit.right()
+
+        expectSuccessfulEncryption()
+
+        // When
+        val result = autoLockRepository.updateAutoLockPin(autoLockPin)
+
+        // Then
+        assertEquals(Unit.right(), result)
+    }
+
+    @Test
+    fun `should propagate data store error when auto lock pin update is not successful`() = runTest {
+        // Given
+        val autoLockPin = AutoLockPin("123")
+        val autoLockPinEncrypted = AutoLockEncryptedPin("encrypted")
+        coEvery {
+            autoLockLocalDataSource.updateAutoLockEncryptedPin(autoLockPinEncrypted)
+        } returns PreferencesError.left()
+
+        expectSuccessfulEncryption()
+
+        // When
+        val result = autoLockRepository.updateAutoLockPin(autoLockPin)
+
+        // Then
+        assertEquals(AutoLockPreferenceError.DataStoreError.left(), result)
+    }
+
+    @Test
+    fun `should propagate encryption error when auto lock pin update encryption is not successful`() = runTest {
+        // Given
+        val autoLockPin = AutoLockPin("123")
+        expectEncryptionError()
+
+        // When
+        val result = autoLockRepository.updateAutoLockPin(autoLockPin)
+
+        // Then
+        assertEquals(baseEncryptionError.left(), result)
+    }
+
+
+    private fun expectSuccessfulEncryption() {
+        coEvery { keyStoreCrypto.encrypt(any<String>()) } returns "encrypted"
+    }
+
+    private fun expectEncryptionError() {
+        coEvery { keyStoreCrypto.encrypt(any<String>()) } throws IOException(baseEncryptionError.message)
+    }
+
+    private companion object {
+
+        val baseEncryptionError = AutoLockPreferenceError.SerializationError("message")
+    }
+}
