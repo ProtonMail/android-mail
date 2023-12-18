9937d7f05835c2cfa55801fa290c38509a1c4a1a
Niccolò Forlini <niccolo.forlini@proton.ch>
2023-12-18 18:07:41 +0100

    Add several use cases to read/store values via AutoLockRepository
    They're just proxies, nothing much going on.
    More will be added as the implementation moves forward.
    
    MAIALNDR-1296

---
+++ mail-settings/domain/src/test/kotlin/ch/protonmail/android/mailsettings/domain/usecase/autolock/ObserveSelectedAutoLockIntervalTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.domain.usecase.autolock
+
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockInterval
+import ch.protonmail.android.mailsettings.domain.repository.AutoLockPreferenceError
+import ch.protonmail.android.mailsettings.domain.repository.AutoLockRepository
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class ObserveSelectedAutoLockIntervalTest {
+
+    private val autoLockRepository = mockk<AutoLockRepository>()
+    private val observeSelectedAutoLockInterval = ObserveSelectedAutoLockInterval(autoLockRepository)
+
+    @Test
+    fun `should propagate the value when the repo returns a success`() = runTest {
+        // Given
+        val expectedValue = AutoLockInterval.OneHour.right()
+        every { autoLockRepository.observeAutoLockInterval() } returns flowOf(expectedValue)
+
+        // When
+        val actual = observeSelectedAutoLockInterval().firstOrNull()
+
+        // Then
+        assertEquals(expectedValue, actual)
+    }
+
+    @Test
+    fun `should propagate the error when the repo returns an error`() = runTest {
+        // Given
+        val expectedValue = AutoLockPreferenceError.DataStoreError.left()
+        every { autoLockRepository.observeAutoLockInterval() } returns flowOf(expectedValue)
+
+        // When
+        val actual = observeSelectedAutoLockInterval().firstOrNull()
+
+        // Then
+        assertEquals(expectedValue, actual)
+    }
+}
---
+++ mail-settings/domain/src/test/kotlin/ch/protonmail/android/mailsettings/domain/usecase/autolock/UpdateAutoLockIntervalTest.kt
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.domain.usecase.autolock
+
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockInterval
+import ch.protonmail.android.mailsettings.domain.repository.AutoLockPreferenceError
+import ch.protonmail.android.mailsettings.domain.repository.AutoLockRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+internal class UpdateAutoLockIntervalTest {
+
+    private val autoLockRepository = mockk<AutoLockRepository>()
+    private val updateAutoLockInterval = UpdateAutoLockInterval(autoLockRepository)
+
+    @Test
+    fun `should propagate the value when the repo returns a success`() = runTest {
+        // Given
+        val newPreference = AutoLockInterval.FifteenMinutes
+        val expectedValue = Unit.right()
+        coEvery { autoLockRepository.updateAutoLockInterval(newPreference) } returns expectedValue
+
+        // When
+        val actual = updateAutoLockInterval(newPreference)
+
+        // Then
+        assertEquals(expectedValue, actual)
+    }
+
+    @Test
+    fun `should propagate the error when the value cannot be updated`() = runTest {
+        // Given
+        val newPreference = AutoLockInterval.FifteenMinutes
+        val expectedValue = AutoLockPreferenceError.DataStoreError.left()
+        coEvery { autoLockRepository.updateAutoLockInterval(newPreference) } returns expectedValue
+
+        // When
+        val actual = updateAutoLockInterval(newPreference)
+
+        // Then
+        assertEquals(expectedValue, actual)
+    }
+}
