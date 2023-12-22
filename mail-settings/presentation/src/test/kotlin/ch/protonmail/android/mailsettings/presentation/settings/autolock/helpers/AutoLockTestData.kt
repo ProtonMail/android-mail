0343b00811bd1f2293648ffa20e13a0279841c5a
Niccolò Forlini <niccolo.forlini@proton.ch>
2023-12-22 20:37:38 +0100

    Add test data holder for Auto Lock pin related values
    MAILANDR-1115

---
+++ mail-settings/presentation/src/test/kotlin/ch/protonmail/android/mailsettings/presentation/settings/autolock/helpers/AutoLockTestData.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.autolock.helpers
+
+import ch.protonmail.android.mailcommon.presentation.Effect
+import ch.protonmail.android.mailcommon.presentation.model.TextUiModel
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockPin
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockPinContinuationAction
+import ch.protonmail.android.mailsettings.presentation.R
+import ch.protonmail.android.mailsettings.presentation.settings.autolock.model.pin.AutoLockPinState
+import ch.protonmail.android.mailsettings.presentation.settings.autolock.model.pin.ConfirmButtonUiModel
+import ch.protonmail.android.mailsettings.presentation.settings.autolock.model.pin.InsertedPin
+import ch.protonmail.android.mailsettings.presentation.settings.autolock.model.pin.PinInsertionStep
+import ch.protonmail.android.mailsettings.presentation.settings.autolock.model.pin.PinInsertionUiModel
+import ch.protonmail.android.mailsettings.presentation.settings.autolock.model.pin.PinVerificationRemainingAttempts
+import ch.protonmail.android.mailsettings.presentation.settings.autolock.model.pin.TopBarUiModel
+
+internal object AutoLockTestData {
+
+    val BaseTopBarUiModel = TopBarUiModel(true, R.string.mail_settings_pin_insertion_set_title)
+    val BaseTopBarState = AutoLockPinState.TopBarState(BaseTopBarUiModel)
+
+    val BasePinInsertionState = AutoLockPinState.PinInsertionState(
+        PinInsertionStep.PinInsertion, PinVerificationRemainingAttempts.Default,
+        PinInsertionUiModel(InsertedPin(emptyList()))
+    )
+
+    val BaseConfirmButtonUiModel =
+        ConfirmButtonUiModel(isEnabled = false, R.string.mail_settings_pin_insertion_button_confirm)
+    val BaseConfirmButtonState = AutoLockPinState.ConfirmButtonState(BaseConfirmButtonUiModel)
+
+    val BaseLoadedState = AutoLockPinState.DataLoaded(
+        topBarState = BaseTopBarState,
+        pinInsertionState = BasePinInsertionState,
+        confirmButtonState = BaseConfirmButtonState,
+        Effect.empty(),
+        Effect.empty(),
+        Effect.empty()
+    )
+
+    val OneRemainingAttempt = PinVerificationRemainingAttempts(1)
+    val NineRemainingAttempts = PinVerificationRemainingAttempts(9)
+
+    val BaseAutoLockPin = AutoLockPin("1234")
+    val BaseContinuationDestination = AutoLockPinContinuationAction.NavigateToDeepLink("continuationDestination")
+    val BaseInvalidPinInserted = InsertedPin(listOf(1, 2, 3))
+    val BaseValidPinInserted = InsertedPin(listOf(1, 2, 3, 4))
+
+    val PlaceholderTextUiModel = TextUiModel("placeholder")
+}
