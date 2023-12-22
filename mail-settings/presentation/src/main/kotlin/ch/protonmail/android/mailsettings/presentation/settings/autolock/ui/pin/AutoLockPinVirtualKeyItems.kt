b51cb414c5c989ee0b3295dd69def2a239c5c037
Niccolò Forlini <niccolo.forlini@proton.ch>
2023-12-22 21:07:43 +0100

    Add Auto Lock Virtual Keyboard UI components
    Digits + buttons, biometrics still not included.
    
    MAILANDR-1341

---
+++ mail-settings/presentation/src/main/kotlin/ch/protonmail/android/mailsettings/presentation/settings/autolock/ui/pin/AutoLockPinVirtualKeyItems.kt
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.autolock.ui.pin
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import ch.protonmail.android.mailcommon.presentation.compose.MailDimens.AutoLockPinScreen.InnerKeyboardButtonTextSize
+import ch.protonmail.android.mailcommon.presentation.compose.MailDimens.AutoLockPinScreen.KeyboardButtonItemSize
+import ch.protonmail.android.mailsettings.presentation.settings.autolock.model.pin.ConfirmButtonUiModel
+import me.proton.core.compose.component.ProtonButton
+import me.proton.core.compose.component.protonButtonColors
+import me.proton.core.compose.theme.ProtonDimens
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.core.compose.theme.ProtonTypography
+import me.proton.core.compose.theme.defaultNorm
+
+@Composable
+fun VirtualKeyboardDigitItem(
+    value: Int,
+    onElementClicked: (Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Box(
+        modifier = modifier
+            .size(KeyboardButtonItemSize)
+            .wrapContentSize()
+            .background(color = ProtonTheme.colors.interactionWeakNorm, shape = CircleShape),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            modifier = Modifier
+                .size(InnerKeyboardButtonTextSize)
+                .padding(ProtonDimens.DefaultSpacing)
+                .padding(ProtonDimens.ExtraSmallSpacing)
+                .clickable(interactionSource = interactionSource, indication = null) { onElementClicked(value) },
+            text = value.toString(),
+            style = ProtonTypography.Default.defaultNorm,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+@Composable
+fun VirtualKeyboardButtonItem(
+    @DrawableRes drawableRes: Int,
+    contentDescription: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    IconButton(
+        modifier = modifier
+            .size(KeyboardButtonItemSize)
+            .wrapContentSize()
+            .padding(ProtonDimens.SmallSpacing),
+        onClick = onClick
+    ) {
+        Icon(
+            painter = painterResource(drawableRes),
+            contentDescription = contentDescription
+        )
+    }
+}
+
+@Composable
+fun VirtualKeyboardConfirmButton(
+    confirmButtonUiModel: ConfirmButtonUiModel,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    ProtonButton(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(ProtonDimens.DefaultSpacing),
+        elevation = null,
+        border = null,
+        shape = ProtonTheme.shapes.medium,
+        colors = ButtonDefaults.protonButtonColors(),
+        enabled = confirmButtonUiModel.isEnabled,
+        onClick = onClick
+    ) {
+        Text(stringResource(id = confirmButtonUiModel.textRes))
+    }
+}
