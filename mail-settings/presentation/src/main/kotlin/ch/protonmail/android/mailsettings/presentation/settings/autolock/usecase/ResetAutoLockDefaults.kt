f35d7b9291292d793be539e346fc48b15617f154
Niccolò Forlini <niccolo.forlini@proton.ch>
2023-12-22 20:23:24 +0100

    Add use case to reset Auto Lock defaults
    MAILANDR-1296

---
+++ mail-settings/presentation/src/main/kotlin/ch/protonmail/android/mailsettings/presentation/settings/autolock/usecase/ResetAutoLockDefaults.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.autolock.usecase
+
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockPin
+import ch.protonmail.android.mailsettings.domain.usecase.autolock.SaveAutoLockPin
+import ch.protonmail.android.mailsettings.domain.usecase.autolock.ToggleAutoLockEnabled
+import ch.protonmail.android.mailsettings.domain.usecase.autolock.UpdateRemainingAutoLockAttempts
+import javax.inject.Inject
+
+class ResetAutoLockDefaults @Inject constructor(
+    private val toggleAutoLockEnabled: ToggleAutoLockEnabled,
+    private val updateRemainingAutoLockAttempts: UpdateRemainingAutoLockAttempts,
+    private val saveAutoLockPin: SaveAutoLockPin
+) {
+
+    suspend operator fun invoke() {
+        toggleAutoLockEnabled(Defaults.AutoLockPreference)
+        saveAutoLockPin(Defaults.AutoLockPin)
+        updateRemainingAutoLockAttempts(Defaults.AutoLockRemainingAttempts)
+    }
+
+    private object Defaults {
+
+        const val AutoLockPreference = false
+        val AutoLockPin = AutoLockPin("")
+        const val AutoLockRemainingAttempts = 10
+    }
+}
---
+++ mail-settings/presentation/src/test/kotlin/ch/protonmail/android/mailsettings/presentation/settings/autolock/usecase/ResetAutoLockDefaultsTest.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailsettings.presentation.settings.autolock.usecase
+
+import arrow.core.right
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockPin
+import ch.protonmail.android.mailsettings.domain.usecase.autolock.SaveAutoLockPin
+import ch.protonmail.android.mailsettings.domain.usecase.autolock.ToggleAutoLockEnabled
+import ch.protonmail.android.mailsettings.domain.usecase.autolock.UpdateRemainingAutoLockAttempts
+import io.mockk.coEvery
+import io.mockk.coVerifySequence
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+internal class ResetAutoLockDefaultsTest {
+
+    private val toggleAutoLockEnabled = mockk<ToggleAutoLockEnabled>()
+    private val updateRemainingAutoLockAttempts = mockk<UpdateRemainingAutoLockAttempts>()
+    private val saveAutoLockPin = mockk<SaveAutoLockPin>()
+
+    private val resetAutoLockDefaults = ResetAutoLockDefaults(
+        toggleAutoLockEnabled,
+        updateRemainingAutoLockAttempts,
+        saveAutoLockPin
+    )
+
+    @Test
+    fun `should toggle preference, restore the auto lock attempts and save an empty pin`() = runTest {
+        // Given
+        coEvery { toggleAutoLockEnabled(ExpectedDefaults.AutoLockPreference) } returns Unit.right()
+        coEvery { updateRemainingAutoLockAttempts(ExpectedDefaults.AutoLockRemainingAttempts) } returns Unit.right()
+        coEvery { saveAutoLockPin(ExpectedDefaults.AutoLockPin) } returns Unit.right()
+
+        // When
+        resetAutoLockDefaults()
+
+        // Then
+        coVerifySequence {
+            toggleAutoLockEnabled(ExpectedDefaults.AutoLockPreference)
+            saveAutoLockPin(ExpectedDefaults.AutoLockPin)
+            updateRemainingAutoLockAttempts(ExpectedDefaults.AutoLockRemainingAttempts)
+        }
+    }
+
+    private object ExpectedDefaults {
+
+        const val AutoLockPreference = false
+        val AutoLockPin = AutoLockPin("")
+        const val AutoLockRemainingAttempts = 10
+    }
+}
