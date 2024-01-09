0f118e16382c4428feeeb80c14dc1f3f7c428964
Niccolò Forlini <niccolo.forlini@proton.ch>
2024-01-09 10:58:41 +0100

    Add LockScreenActivity
    Create a secondary activity which only hosts the PIN insertion screen.
    
    MAILANDR-1386

---
+++ app/src/main/kotlin/ch/protonmail/android/LockScreenActivity.kt
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import ch.protonmail.android.mailsettings.domain.model.autolock.AutoLockInsertionMode
+import ch.protonmail.android.navigation.model.Destination
+import ch.protonmail.android.navigation.route.addAutoLockPinScreen
+import dagger.hilt.android.AndroidEntryPoint
+import io.sentry.compose.withSentryObservableEffect
+import me.proton.core.compose.theme.ProtonTheme
+
+@AndroidEntryPoint
+internal class LockScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ProtonTheme {
+                val navController = rememberNavController().withSentryObservableEffect()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = Destination.Screen.AutoLockPinScreen.route
+                ) {
+                    addAutoLockPinScreen(
+                        onShowSuccessSnackbar = {},
+                        onBack = { this@LockScreenActivity.finish() }
+                    )
+                }
+
+                navController.navigate(
+                    Destination.Screen.AutoLockPinScreen(AutoLockInsertionMode.VerifyPin)
+                ) {
+                    popUpTo(navController.graph.id) { inclusive = true }
+                }
+            }
+        }
+    }
+}
