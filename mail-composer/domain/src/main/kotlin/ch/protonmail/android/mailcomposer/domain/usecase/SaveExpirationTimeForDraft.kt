5f5c922ea4696375353a33cac861f807375a76f7
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2024-02-08 17:10:13 +0100

    Create a use case for saving expiration time for draft
    MAILANDR-1084

---
+++ mail-composer/domain/src/main/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/SaveExpirationTimeForDraft.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.Either
+import arrow.core.raise.either
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcomposer.domain.Transactor
+import ch.protonmail.android.mailcomposer.domain.model.MessageExpirationTime
+import ch.protonmail.android.mailcomposer.domain.model.SenderEmail
+import ch.protonmail.android.mailcomposer.domain.repository.MessageExpirationTimeRepository
+import ch.protonmail.android.mailmessage.domain.model.MessageId
+import ch.protonmail.android.mailmessage.domain.repository.MessageRepository
+import me.proton.core.domain.entity.UserId
+import timber.log.Timber
+import javax.inject.Inject
+import kotlin.time.Duration
+
+class SaveExpirationTimeForDraft @Inject constructor(
+    private val getLocalDraft: GetLocalDraft,
+    private val messageExpirationTimeRepository: MessageExpirationTimeRepository,
+    private val messageRepository: MessageRepository,
+    private val saveDraft: SaveDraft,
+    private val transactor: Transactor
+) {
+
+    suspend operator fun invoke(
+        userId: UserId,
+        messageId: MessageId,
+        senderEmail: SenderEmail,
+        expiresIn: Duration
+    ): Either<DataError.Local, Unit> = transactor.performTransaction {
+        either {
+            val draft = getLocalDraft(userId, messageId, senderEmail)
+                .mapLeft { DataError.Local.NoDataCached }
+                .bind()
+            // Verify that draft exists in db, if not create it
+            val messageWithBody = messageRepository.getLocalMessageWithBody(userId, draft.message.messageId)
+            if (messageWithBody == null) {
+                val success = saveDraft(draft, userId)
+                if (!success) {
+                    Timber.d("Failed to save draft")
+                    raise(DataError.Local.Unknown)
+                }
+            }
+
+            val messageExpirationTime = MessageExpirationTime(userId, draft.message.messageId, expiresIn)
+            messageExpirationTimeRepository.saveMessageExpirationTime(messageExpirationTime).bind()
+        }
+    }
+}
---
+++ mail-composer/domain/src/test/kotlin/ch/protonmail/android/mailcomposer/domain/usecase/SaveExpirationTimeForDraftTest.kt
@@ -0,0 +1,142 @@
+package ch.protonmail.android.mailcomposer.domain.usecase
+
+import arrow.core.left
+import arrow.core.right
+import ch.protonmail.android.mailcommon.domain.model.DataError
+import ch.protonmail.android.mailcomposer.domain.model.MessageExpirationTime
+import ch.protonmail.android.mailcomposer.domain.model.SenderEmail
+import ch.protonmail.android.mailcomposer.domain.repository.MessageExpirationTimeRepository
+import ch.protonmail.android.mailmessage.domain.repository.MessageRepository
+import ch.protonmail.android.mailmessage.domain.sample.MessageIdSample
+import ch.protonmail.android.mailmessage.domain.sample.MessageWithBodySample
+import ch.protonmail.android.test.utils.FakeTransactor
+import ch.protonmail.android.testdata.user.UserIdTestData
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.time.Duration.Companion.days
+
+class SaveExpirationTimeForDraftTest {
+
+    private val userId = UserIdTestData.userId
+    private val messageId = MessageIdSample.NewDraftWithSubjectAndBody
+    private val senderEmail = SenderEmail("sender@pm.me")
+    private val expiresIn = 1.days
+
+    private val getLocalDraft = mockk<GetLocalDraft>()
+    private val messageExpirationTimeRepository = mockk<MessageExpirationTimeRepository>()
+    private val messageRepository = mockk<MessageRepository>()
+    private val saveDraft = mockk<SaveDraft>()
+    private val transactor = FakeTransactor()
+
+    private val saveExpirationTimeForDraft = SaveExpirationTimeForDraft(
+        getLocalDraft, messageExpirationTimeRepository, messageRepository, saveDraft, transactor
+    )
+
+    @Test
+    fun `should return unit when draft exists and message expiration time stored successfully`() = runTest {
+        // Given
+        expectDraftAlreadyExists()
+        coEvery {
+            messageExpirationTimeRepository.saveMessageExpirationTime(
+                MessageExpirationTime(
+                    userId,
+                    MessageWithBodySample.EmptyDraft.message.messageId,
+                    expiresIn
+                )
+            )
+        } returns Unit.right()
+
+        // When
+        val actual = saveExpirationTimeForDraft(userId, messageId, senderEmail, expiresIn)
+
+        // Then
+        assertEquals(Unit.right(), actual)
+    }
+
+    @Test
+    fun `should return unit when draft is saved and message expiration time is stored successfully`() = runTest {
+        // Given
+        expectDraftDoesNotExist()
+        coEvery { saveDraft(MessageWithBodySample.EmptyDraft, userId) } returns true
+        coEvery {
+            messageExpirationTimeRepository.saveMessageExpirationTime(
+                MessageExpirationTime(
+                    userId,
+                    MessageWithBodySample.EmptyDraft.message.messageId,
+                    expiresIn
+                )
+            )
+        } returns Unit.right()
+
+        // When
+        val actual = saveExpirationTimeForDraft(userId, messageId, senderEmail, expiresIn)
+
+        // Then
+        assertEquals(Unit.right(), actual)
+    }
+
+    @Test
+    fun `should return error when getting the local draft has failed `() = runTest {
+        // Given
+        coEvery {
+            getLocalDraft(userId, messageId, senderEmail)
+        } returns GetLocalDraft.Error.ResolveUserAddressError.left()
+
+        // When
+        val actual = saveExpirationTimeForDraft(userId, messageId, senderEmail, expiresIn)
+
+        // Then
+        assertEquals(DataError.Local.NoDataCached.left(), actual)
+    }
+
+    @Test
+    fun `should return error when saving draft has failed`() = runTest {
+        // Given
+        expectDraftDoesNotExist()
+        coEvery { saveDraft(MessageWithBodySample.EmptyDraft, userId) } returns false
+
+        // When
+        val actual = saveExpirationTimeForDraft(userId, messageId, senderEmail, expiresIn)
+
+        // Then
+        assertEquals(DataError.Local.Unknown.left(), actual)
+    }
+
+    @Test
+    fun `should return error when saving of message expiration time fails`() = runTest {
+        // Given
+        expectDraftAlreadyExists()
+        coEvery {
+            messageExpirationTimeRepository.saveMessageExpirationTime(
+                MessageExpirationTime(
+                    userId,
+                    MessageWithBodySample.EmptyDraft.message.messageId,
+                    expiresIn
+                )
+            )
+        } returns DataError.Local.Unknown.left()
+
+        // When
+        val actual = saveExpirationTimeForDraft(userId, messageId, senderEmail, expiresIn)
+
+        // Then
+        assertEquals(DataError.Local.Unknown.left(), actual)
+    }
+
+    private fun expectDraftAlreadyExists() {
+        coEvery { getLocalDraft(userId, messageId, senderEmail) } returns MessageWithBodySample.EmptyDraft.right()
+        coEvery {
+            messageRepository.getLocalMessageWithBody(userId, MessageWithBodySample.EmptyDraft.message.messageId)
+        } returns MessageWithBodySample.EmptyDraft
+    }
+
+    private fun expectDraftDoesNotExist() {
+        coEvery { getLocalDraft(userId, messageId, senderEmail) } returns MessageWithBodySample.EmptyDraft.right()
+        coEvery {
+            messageRepository.getLocalMessageWithBody(userId, MessageWithBodySample.EmptyDraft.message.messageId)
+        } returns null
+    }
+}
