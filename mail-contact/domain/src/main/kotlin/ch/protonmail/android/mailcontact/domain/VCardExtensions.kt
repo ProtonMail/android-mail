51b132b48ebb0136e6dd8cab09decd5184662f9e
Adam Jodlowski <adam.jodlowski@proton.ch>
2024-02-08 16:12:33 +0100

    Create a helper function to sanitize contact VCards
    MAILANDR-1067

---
+++ mail-contact/domain/src/main/kotlin/ch/protonmail/android/mailcontact/domain/VCardExtensions.kt
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcontact.domain
+
+import ezvcard.VCard
+import ezvcard.property.Address
+import ezvcard.property.Anniversary
+import ezvcard.property.Birthday
+import ezvcard.property.Categories
+import ezvcard.property.Email
+import ezvcard.property.FormattedName
+import ezvcard.property.Gender
+import ezvcard.property.Language
+import ezvcard.property.Logo
+import ezvcard.property.Member
+import ezvcard.property.Nickname
+import ezvcard.property.Note
+import ezvcard.property.Organization
+import ezvcard.property.Photo
+import ezvcard.property.ProductId
+import ezvcard.property.Related
+import ezvcard.property.Role
+import ezvcard.property.StructuredName
+import ezvcard.property.Telephone
+import ezvcard.property.Timezone
+import ezvcard.property.Title
+import ezvcard.property.Uid
+import ezvcard.property.Url
+import me.proton.core.util.kotlin.takeIfNotBlank
+
+/**
+ * Creates an empty VCard and copies into it only the properties supported by Proton Contacts.
+ */
+fun VCard.sanitizeAndBuildVCard(): VCard {
+
+    val vCard = VCard().apply {
+        productId = ProductId(VCARD_PROD_ID)
+    }
+
+    this.properties.forEach { iCalProperty ->
+        // we ignore (Version, Prod ID) because we set our own values of them
+        // but we copy all the supported properties if they were present in the original VCard
+        when (iCalProperty) {
+            is Uid,
+            is FormattedName,
+            is Email,
+            is Categories,
+            is StructuredName, // in VCARD it's a property called "N"
+            is Telephone,
+            is Address,
+            is Birthday,
+            is Note,
+            is Photo,
+            is Organization,
+            is Title,
+            is Role,
+            is Timezone,
+            is Logo,
+            is Member,
+            is Language,
+            is Url,
+            is Gender,
+            is Anniversary,
+            is Nickname,
+            is Related -> vCard.addProperty(iCalProperty)
+        }
+
+    }
+
+    this.extendedProperties.forEach {
+        if ((it.propertyName?.takeIfNotBlank() ?: "").uppercase() in protonSupportedVCardExtendedProperties) {
+            vCard.extendedProperties.add(it)
+        }
+    }
+
+    // if UID is still blank after copying, set a random one
+    if (vCard.uid?.value?.takeIfNotBlank() == null) {
+        vCard.uid = Uid.random()
+    }
+
+    return vCard
+}
+
+const val VCARD_PROD_ID = "-//ProtonMail//ProtonMail for Android vCard 1.0.0//EN"
+
+val protonSupportedVCardExtendedProperties = listOf(
+    "KEY",
+    "X-PM-MIMETYPE",
+    "X-PM-ENCRYPT",
+    "X-PM-SIGN",
+    "X-PM-SCHEME",
+    "X-PM-TLS",
+    "X-PM-ENCRYPT-UNTRUSTED"
+)
---
+++ mail-contact/domain/src/test/kotlin/ch/protonmail/android/mailcontact/domain/usecase/VCardExtensionsTest.kt
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcontact.domain.usecase
+
+import ch.protonmail.android.mailcontact.domain.VCARD_PROD_ID
+import ch.protonmail.android.mailcontact.domain.protonSupportedVCardExtendedProperties
+import ch.protonmail.android.mailcontact.domain.sanitizeAndBuildVCard
+import ch.protonmail.android.testdata.contact.ContactVCardSample
+import ezvcard.Ezvcard
+import ezvcard.VCard
+import ezvcard.property.Expertise
+import ezvcard.property.ProductId
+import ezvcard.property.RawProperty
+import ezvcard.property.Uid
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+@Suppress("MaxLineLength")
+class VCardExtensionsTest {
+
+    @Test
+    fun `sanitizeAndBuildVCard sets UID if UID in original VCARD was empty`() = runTest {
+        // Given
+        val given = VCard()
+
+        // When
+        val actual = given.sanitizeAndBuildVCard()
+
+        // Then
+        assertNotNull(actual.uid)
+    }
+
+    @Test
+    fun `sanitizeAndBuildVCard copies UID if UID in original VCARD was present`() = runTest {
+        // Given
+        val givenUid = Uid("original UID")
+        val given = VCard().apply {
+            uid = givenUid
+        }
+
+        // When
+        val actual = given.sanitizeAndBuildVCard()
+
+        // Then
+        assertEquals(givenUid, actual.uid)
+    }
+
+    @Test
+    fun `sanitizeAndBuildVCard sets PRODID to Proton Product ID`() = runTest {
+        // Given
+        val given = VCard().apply {
+            productId = ProductId("original PRODID")
+        }
+
+        // When
+        val actual = given.sanitizeAndBuildVCard()
+
+        // Then
+        assertEquals(VCARD_PROD_ID, actual.productId.value)
+    }
+
+    @Test
+    fun `sanitizeAndBuildVCard copies supported crypto-related extended properties from original VCARD`() = runTest {
+        // Given
+        val givenNotSupportedRawProperty = RawProperty("X-not-supported-property", "value")
+        val given = VCard().apply {
+            protonSupportedVCardExtendedProperties.forEach {
+                extendedProperties.add(RawProperty(it, it))
+            }
+            extendedProperties.add(givenNotSupportedRawProperty)
+        }
+
+        // When
+        val actual = given.sanitizeAndBuildVCard()
+
+        // Then
+        protonSupportedVCardExtendedProperties.forEach {
+            assertEquals(it, actual.getExtendedProperty(it).value)
+        }
+        assertNull(actual.getExtendedProperty(givenNotSupportedRawProperty.propertyName))
+    }
+
+    @Test
+    fun `sanitizeAndBuildVCard copies only supported properties from original VCARD`() = runTest {
+        // Given
+        val givenNotSupportedRawProperty = RawProperty("X-not-supported-property", "value")
+        val givenNotSupportedProperty = Expertise("skill we do not possess")
+
+        val given = Ezvcard.parse(ContactVCardSample.marioVCardType3).first().apply {
+            addExpertise(givenNotSupportedProperty)
+            extendedProperties.add(givenNotSupportedRawProperty)
+        }
+
+        // When
+        val actual = given.sanitizeAndBuildVCard()
+
+        // Then
+        // properties copied
+        assertNotNull(actual.structuredName.given)
+        assertTrue(actual.telephoneNumbers.size == 9)
+        assertTrue(actual.addresses.size == 4)
+        assertTrue(actual.notes.size == 2)
+        assertTrue(actual.organizations.size == 2)
+        assertTrue(actual.titles.size == 1)
+        assertTrue(actual.logos.size == 1)
+        assertTrue(actual.photos.size == 1)
+        assertTrue(actual.roles.size == 1)
+        assertTrue(actual.timezones.size == 1)
+        assertTrue(actual.members.size == 1)
+        assertTrue(actual.languages.size == 1)
+        assertTrue(actual.urls.size == 1)
+        assertTrue(actual.anniversaries.size == 1)
+        assertNotNull(actual.birthday.date)
+        assertNotNull(actual.gender)
+
+        // properties not copied
+        assertTrue(actual.expertise.isEmpty())
+        assertNull(actual.getExtendedProperty(givenNotSupportedRawProperty.propertyName))
+    }
+
+}
