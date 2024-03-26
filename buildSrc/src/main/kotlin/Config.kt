320d179f0f546a84c39c1bcd2757f1150dfbcaf8
Niccolò Forlini <niccolo.forlini@proton.ch>
2024-03-26 18:15:18 +0100

    Bump to 4.0.7
    NOJIRA

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -23,5 +23,5 @@ object Config {
     const val targetSdk = 33
     const val testInstrumentationRunner = "ch.protonmail.android.uitest.HiltTestRunner"
     const val versionCode = 1
-    const val versionName = "4.0.6"
+    const val versionName = "4.0.7"
 }
