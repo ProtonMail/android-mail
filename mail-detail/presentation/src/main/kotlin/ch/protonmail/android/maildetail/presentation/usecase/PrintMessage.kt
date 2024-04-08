06698c86b42e3d96e199b87047f1655181c1b5d0
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2024-04-08 18:13:26 +0200

    Create a use case to handle printing a message
    MAILANDR-1657

---
+++ mail-detail/presentation/src/main/kotlin/ch/protonmail/android/maildetail/presentation/usecase/PrintMessage.kt
@@ -0,0 +1,43 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.maildetail.presentation.usecase
+
+import android.content.Context
+import android.print.PrintAttributes
+import android.print.PrintManager
+import android.webkit.WebView
+import javax.inject.Inject
+
+class PrintMessage @Inject constructor() {
+
+    operator fun invoke(context: Context, webView: WebView) {
+        val printManager = context.getSystemService(Context.PRINT_SERVICE) as PrintManager
+        val printAdapter = webView.createPrintDocumentAdapter(JobName)
+
+        printManager.print(
+            JobName,
+            printAdapter,
+            PrintAttributes.Builder().build()
+        )
+    }
+
+    companion object {
+        const val JobName = "Proton Mail Document"
+    }
+}
---
+++ mail-detail/presentation/src/test/kotlin/ch/protonmail/android/maildetail/presentation/usecase/PrintMessageTest.kt
@@ -0,0 +1,42 @@
+package ch.protonmail.android.maildetail.presentation.usecase
+
+import android.content.Context
+import android.print.PrintAttributes
+import android.print.PrintDocumentAdapter
+import android.print.PrintManager
+import android.webkit.WebView
+import ch.protonmail.android.maildetail.presentation.usecase.PrintMessage.Companion.JobName
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkConstructor
+import io.mockk.verify
+import kotlin.test.Test
+
+class PrintMessageTest {
+
+    private val printManager = mockk<PrintManager>()
+    private val context = mockk<Context> {
+        every { getSystemService(Context.PRINT_SERVICE) } returns printManager
+    }
+    private val printAdapter = mockk<PrintDocumentAdapter>()
+    private val webView = mockk<WebView> {
+        every { createPrintDocumentAdapter(JobName) } returns printAdapter
+    }
+
+    private val printMessage = PrintMessage()
+
+    @Test
+    fun `should call print method from the print manager`() {
+        // Given
+        mockkConstructor(PrintAttributes.Builder::class)
+        val printAttributes = mockk<PrintAttributes>()
+        every { anyConstructed<PrintAttributes.Builder>().build() } returns printAttributes
+        every { printManager.print(JobName, printAdapter, printAttributes) } returns mockk()
+
+        // When
+        printMessage(context, webView)
+
+        // Then
+        verify { printManager.print(JobName, printAdapter, printAttributes) }
+    }
+}
