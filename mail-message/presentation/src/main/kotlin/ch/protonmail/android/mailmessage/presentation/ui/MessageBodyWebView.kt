ProtonMail/android-mail

commit c1bba2b8a65f446ce748b76b576e56cf6d5bb43f

Add WebView padding instead of forcing CSS property
Since the HTML content is often unpredictable and we don't exactly know what kind of CSS styling is applied to newsletters, add a default padding without relying on CSS.

MAILANDR-1108

mail-message/presentation/src/main/kotlin/ch/protonmail/android/mailmessage/presentation/ui/MessageBodyWebView.kt
@@ -43,6 +43,7 @@ import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
@@ -142,7 +143,7 @@ fun MessageBodyWebView(
     val isSystemInDarkTheme = isSystemInDarkTheme()
 
     // This is will on be used if the WebView height is higher than the max constraint.
-    var webViewHeightPx by remember { mutableStateOf(0) }
+    var webViewHeightPx by remember { mutableIntStateOf(0) }
 
     Column(modifier = modifier) {
         key(client) {
@@ -163,6 +164,7 @@ fun MessageBodyWebView(
                 modifier = with(
                     Modifier
                         .testTag(MessageBodyWebViewTestTags.WebView)
+                        .padding(ProtonDimens.DefaultSpacing)
                         .fillMaxWidth()
                         // There are no guarantees onSizeChanged will not be re-invoked with the same size.
                         // We need to take our own measures to avoid callback with the same size.

mail-message/presentation/src/main/res/raw/css_reset_with_media_scheme_plus_custom_props.css
@@ -126,7 +126,6 @@ body{
     font-size:100%;
     width:100%;
     font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;
-    padding:1rem;
     line-height:1.5;
 	  background: var(--background-norm);
 	  color: var(--text-norm);
@@ -134,7 +133,6 @@ body{
 /* Only override <pre> when it's the first direct child of the body (case when replying to HTML). */
 body > pre:first-child {
     margin:0;
-    padding:1rem;
     font-size:100%;
     width:100%;
     font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif;

commit fa396cdfabf54ce485843cffa8ddb27d10389ab3

Fix incorrect padding when switching to light mode
Fix a side effect introduced by c1bba2b8 when viewing the message in light mode.

MAILANDR-1670

mail-message/presentation/src/main/kotlin/ch/protonmail/android/mailmessage/presentation/ui/MessageBodyWebView.kt
@@ -207,7 +207,6 @@ fun MessageBodyWebView(
                 modifier = with(
                     Modifier
                         .testTag(MessageBodyWebViewTestTags.WebView)
-                        .padding(ProtonDimens.DefaultSpacing)
                         .fillMaxWidth()
                         // There are no guarantees onSizeChanged will not be re-invoked with the same size.
                         // We need to take our own measures to avoid callback with the same size.

mail-message/presentation/src/main/res/raw/css_reset_with_custom_props.css
@@ -188,5 +188,6 @@ blockquote blockquote blockquote blockquote blockquote blockquote{
 :root {
 	--background-norm: white;
 	--text-norm: #262a33;
+	padding: 1rem;
 }
  