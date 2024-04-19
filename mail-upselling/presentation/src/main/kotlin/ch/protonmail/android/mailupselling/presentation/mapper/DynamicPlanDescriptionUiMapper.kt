7616d3f15440a14de7a3689d9de0babd2d5b5323
Niccolò Forlini <niccolo.forlini@proton.ch>
2024-04-19 14:49:08 +0200

    Add DynamicPlanDescriptionUiModel + Mapper
    MAILANDR-1588

---
+++ mail-upselling/presentation/src/main/kotlin/ch/protonmail/android/mailupselling/presentation/mapper/DynamicPlanDescriptionUiMapper.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailupselling.presentation.mapper
+
+import ch.protonmail.android.mailcommon.presentation.model.TextUiModel
+import ch.protonmail.android.mailupselling.domain.annotations.ForceOneClickUpsellingDetailsOverride
+import ch.protonmail.android.mailupselling.domain.model.DynamicPlansOneClickIds
+import ch.protonmail.android.mailupselling.presentation.R
+import ch.protonmail.android.mailupselling.presentation.model.DynamicPlanDescriptionUiModel
+import me.proton.core.plan.domain.entity.DynamicPlan
+import javax.inject.Inject
+
+internal class DynamicPlanDescriptionUiMapper @Inject constructor(
+    @ForceOneClickUpsellingDetailsOverride private val shouldOverrideEntitlementsList: Boolean
+) {
+
+    fun toUiModel(dynamicPlan: DynamicPlan): DynamicPlanDescriptionUiModel {
+        if (!shouldOverrideEntitlementsList) return DynamicPlanDescriptionUiModel(getDefaultDescription(dynamicPlan))
+
+        val description = when (dynamicPlan.name) {
+            DynamicPlansOneClickIds.UnlimitedPlanId -> getUnlimitedDescription()
+            DynamicPlansOneClickIds.PlusPlanId -> getPlusDescription()
+            else -> getDefaultDescription(dynamicPlan)
+        }
+
+        return DynamicPlanDescriptionUiModel(description)
+    }
+
+    private fun getDefaultDescription(dynamicPlan: DynamicPlan) = TextUiModel.Text(dynamicPlan.description ?: "")
+    private fun getUnlimitedDescription() = TextUiModel.TextRes(R.string.upselling_unlimited_description_override)
+    private fun getPlusDescription() = TextUiModel.TextRes(R.string.upselling_plus_description_override)
+}
---
+++ mail-upselling/presentation/src/test/kotlin/ch/protonmail/upselling/presentation/mapper/DynamicPlanDescriptionUiMapperTest.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.upselling.presentation.mapper
+
+import ch.protonmail.android.mailcommon.presentation.model.TextUiModel
+import ch.protonmail.android.mailupselling.presentation.R
+import ch.protonmail.android.mailupselling.presentation.mapper.DynamicPlanDescriptionUiMapper
+import ch.protonmail.android.mailupselling.presentation.model.DynamicPlanDescriptionUiModel
+import ch.protonmail.android.testdata.upselling.UpsellingTestData
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import javax.inject.Provider
+import kotlin.test.AfterTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class DynamicPlanDescriptionUiMapperTest {
+
+    private val forceOverride = mockk<Provider<Boolean>>()
+    private val mapper: DynamicPlanDescriptionUiMapper
+        get() = DynamicPlanDescriptionUiMapper(forceOverride.get())
+
+    @AfterTest
+    fun teardown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `should return the default description when override is unset`() {
+        // Given
+        every { forceOverride.get() } returns false
+        val expected = DynamicPlanDescriptionUiModel(
+            text = TextUiModel.Text(UpsellingTestData.PlusPlan.description!!)
+        )
+
+        // When
+        val actual = mapper.toUiModel(UpsellingTestData.PlusPlan)
+
+        // Then
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `should return the local description when override is set`() {
+        // Given
+        every { forceOverride.get() } returns true
+        val expected = DynamicPlanDescriptionUiModel(
+            text = TextUiModel.TextRes(R.string.upselling_plus_description_override)
+        )
+
+        // When
+        val actual = mapper.toUiModel(UpsellingTestData.PlusPlan)
+
+        // Then
+        assertEquals(expected, actual)
+    }
+}
