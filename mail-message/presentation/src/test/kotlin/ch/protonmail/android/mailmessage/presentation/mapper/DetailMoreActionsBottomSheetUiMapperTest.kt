7838582790d2a28b292ba89cddd1bcd3ea4bea0f
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2024-06-12 10:46:14 +0200

    Hide "Move to" quick action
    MAILANDR-1702

--- mail-message/presentation/src/test/kotlin/ch/protonmail/android/mailmessage/presentation/mapper/DetailMoreActionsBottomSheetUiMapperTest.kt
+++ mail-message/presentation/src/test/kotlin/ch/protonmail/android/mailmessage/presentation/mapper/DetailMoreActionsBottomSheetUiMapperTest.kt
@@ -63,7 +63,7 @@ internal class DetailMoreActionsBottomSheetUiMapperTest {
             ActionUiModel(Action.Trash),
             ActionUiModel(Action.Archive),
             ActionUiModel(Action.Spam),
-            ActionUiModel(Action.Move),
+//            ActionUiModel(Action.Move),
             ActionUiModel(Action.Print),
             ActionUiModel(Action.ReportPhishing)
         ).toImmutableList()
@@ -99,7 +99,7 @@ internal class DetailMoreActionsBottomSheetUiMapperTest {
             ActionUiModel(Action.Trash),
             ActionUiModel(Action.Archive),
             ActionUiModel(Action.Spam),
-            ActionUiModel(Action.Move),
+//            ActionUiModel(Action.Move),
             ActionUiModel(Action.Print),
             ActionUiModel(Action.ReportPhishing)
         ).toImmutableList()
