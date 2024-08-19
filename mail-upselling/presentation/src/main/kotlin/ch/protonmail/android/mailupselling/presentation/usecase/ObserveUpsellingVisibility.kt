b0cb661e248310a81cf46bec12a2f30b1c062955
Adam Jodlowski <adam.jodlowski@proton.ch>
2024-08-19 19:38:34 +0200

    Add usecase for observing visibility of upselling features
    MAILANDR-2044

---
+++ mail-upselling/presentation/src/main/kotlin/ch/protonmail/android/mailupselling/presentation/usecase/ObserveUpsellingVisibility.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailupselling.presentation.usecase
+
+import ch.protonmail.android.mailcommon.domain.usecase.ObservePrimaryUser
+import ch.protonmail.android.mailupselling.domain.usecase.UserHasAvailablePlans
+import ch.protonmail.android.mailupselling.domain.usecase.UserHasPendingPurchases
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
+import kotlinx.coroutines.flow.distinctUntilChanged
+import me.proton.core.payment.domain.PurchaseManager
+import me.proton.core.plan.domain.usecase.CanUpgradeFromMobile
+import javax.inject.Inject
+
+class ObserveUpsellingVisibility @Inject constructor(
+    private val observePrimaryUser: ObservePrimaryUser,
+    private val purchaseManager: PurchaseManager,
+    private val canUpgradeFromMobile: CanUpgradeFromMobile,
+    private val userHasAvailablePlans: UserHasAvailablePlans,
+    private val userHasPendingPurchases: UserHasPendingPurchases
+) {
+
+    operator fun invoke(isFeatureFlagEnabled: Flow<Boolean>): Flow<Boolean> = combine(
+        observePrimaryUser().distinctUntilChanged(),
+        purchaseManager.observePurchases(),
+        isFeatureFlagEnabled
+    ) { user, purchases, featureFlag ->
+        if (user == null) return@combine false
+        if (!canUpgradeFromMobile()) return@combine false
+        if (userHasPendingPurchases(purchases, user.userId)) return@combine false
+        if (featureFlag.not()) return@combine false
+
+        userHasAvailablePlans(user.userId)
+    }
+}
---
+++ mail-upselling/presentation/src/test/kotlin/ch/protonmail/upselling/presentation/usecase/ObserveUpsellingVisibilityTest.kt
@@ -0,0 +1,191 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.upselling.presentation.usecase
+
+import app.cash.turbine.test
+import ch.protonmail.android.mailcommon.domain.sample.UserSample
+import ch.protonmail.android.mailcommon.domain.usecase.ObservePrimaryUser
+import ch.protonmail.android.mailupselling.domain.usecase.UserHasAvailablePlans
+import ch.protonmail.android.mailupselling.domain.usecase.UserHasPendingPurchases
+import ch.protonmail.android.mailupselling.presentation.usecase.ObserveUpsellingVisibility
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.unmockkAll
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import me.proton.core.domain.entity.UserId
+import me.proton.core.payment.domain.PurchaseManager
+import me.proton.core.payment.domain.entity.Purchase
+import me.proton.core.plan.domain.usecase.CanUpgradeFromMobile
+import me.proton.core.user.domain.entity.User
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+internal class ObserveUpsellingVisibilityTest {
+
+    private val observePrimaryUser = mockk<ObservePrimaryUser>()
+    private val userHasAvailablePlans = mockk<UserHasAvailablePlans>()
+    private val purchaseManager = mockk<PurchaseManager>()
+    private val userHasPendingPurchases = mockk<UserHasPendingPurchases>()
+    private val canUpgradeFromMobile = mockk<CanUpgradeFromMobile>()
+    private val sut: ObserveUpsellingVisibility
+        get() = ObserveUpsellingVisibility(
+            observePrimaryUser,
+            purchaseManager,
+            canUpgradeFromMobile,
+            userHasAvailablePlans,
+            userHasPendingPurchases
+        )
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+    }
+
+    @AfterTest
+    fun teardown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `should return false if observed user is null`() = runTest {
+        // Given
+        expectedUser(null)
+        expectPurchases(emptyList())
+        expectCanUpgradeFromMobile(true)
+        val expectedFeatureFlagEnabled = flowOf(true)
+
+        // When + Then
+        sut(expectedFeatureFlagEnabled).test {
+            assertEquals(false, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `should return false if user can not upgrade from mobile`() = runTest {
+        // Given
+        expectedUser(UserSample.Primary)
+        expectPurchases(emptyList())
+        expectCanUpgradeFromMobile(false)
+        val expectedFeatureFlagEnabled = flowOf(true)
+
+        // When + Then
+        sut(expectedFeatureFlagEnabled).test {
+            assertEquals(false, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `should return false if user has pending purchases`() = runTest {
+        // Given
+        expectedUser(UserSample.Primary)
+        expectPurchases(listOf(mockk<Purchase>()))
+        expectCanUpgradeFromMobile(true)
+        expectPendingPurchasesValue(UserSample.Primary.userId, true)
+        val expectedFeatureFlagEnabled = flowOf(true)
+
+        // When + Then
+        sut(expectedFeatureFlagEnabled).test {
+            assertEquals(false, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `should return false if user has no available plans`() = runTest {
+        // Given
+        expectedUser(UserSample.Primary)
+        expectPurchases(listOf(mockk<Purchase>()))
+        expectCanUpgradeFromMobile(true)
+        expectPendingPurchasesValue(UserSample.Primary.userId, false)
+        expectUserHasAvailablePlans(UserSample.Primary.userId, false)
+        val expectedFeatureFlagEnabled = flowOf(true)
+
+        // When + Then
+        sut(expectedFeatureFlagEnabled).test {
+            assertEquals(false, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Test
+    fun `should return false if passed Feature Flag is disabled`() = runTest {
+        // Given
+        expectedUser(UserSample.Primary)
+        expectPurchases(listOf(mockk<Purchase>()))
+        expectCanUpgradeFromMobile(true)
+        expectPendingPurchasesValue(UserSample.Primary.userId, false)
+        expectUserHasAvailablePlans(UserSample.Primary.userId, true)
+        val expectedFeatureFlagEnabled = flowOf(false)
+
+        // When + Then
+        sut(expectedFeatureFlagEnabled).test {
+            assertEquals(false, awaitItem())
+            awaitComplete()
+        }
+    }
+
+    @Suppress("MaxLineLength")
+    @Test
+    fun `should return true if user has no pending purchases, can upgrade from mobile, has available plans and FF is enabled`() =
+        runTest {
+            // Given
+            expectedUser(UserSample.Primary)
+            expectPurchases(listOf(mockk<Purchase>()))
+            expectCanUpgradeFromMobile(true)
+            expectPendingPurchasesValue(UserSample.Primary.userId, false)
+            expectUserHasAvailablePlans(UserSample.Primary.userId, true)
+            val expectedFeatureFlagEnabled = flowOf(true)
+
+            // When + Then
+            sut(expectedFeatureFlagEnabled).test {
+                assertEquals(true, awaitItem())
+                awaitComplete()
+            }
+        }
+
+    private fun expectedUser(user: User?) {
+        every { observePrimaryUser() } returns flowOf(user)
+    }
+
+    private fun expectPurchases(list: List<Purchase>) {
+        every { purchaseManager.observePurchases() } returns flowOf(list)
+    }
+
+    private fun expectUserHasAvailablePlans(userId: UserId, value: Boolean) {
+        coEvery { userHasAvailablePlans(userId) } returns value
+    }
+
+    private fun expectPendingPurchasesValue(userId: UserId, value: Boolean) {
+        coEvery { userHasPendingPurchases(any(), userId) } returns value
+    }
+
+    private fun expectCanUpgradeFromMobile(value: Boolean) {
+        coEvery { canUpgradeFromMobile() } returns value
+    }
+
+}
