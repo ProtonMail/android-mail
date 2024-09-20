3afd3685988657a212d49d4d92320869235d0e17
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2024-09-20 15:29:11 +0200

    Create UI models and preview data for the OnboardingUpsellScreen
    MAILANDR-2107

---
+++ mail-upselling/presentation/src/main/kotlin/ch/protonmail/android/mailupselling/presentation/model/OnboardingUpsellPlanUiModel.kt
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailupselling.presentation.model
+
+internal data class OnboardingUpsellPlanUiModels(
+    val monthlyPlans: List<OnboardingUpsellPlanUiModel>,
+    val annualPlans: List<OnboardingUpsellPlanUiModel>
+)
+
+internal data class OnboardingUpsellPlanUiModel(
+    val title: String,
+    val currency: String?,
+    val monthlyPrice: Float?,
+    val monthlyPriceWithDiscount: Float?,
+    val entitlements: List<DynamicEntitlementUiModel>
+)
---
+++ mail-upselling/presentation/src/main/kotlin/ch/protonmail/android/mailupselling/presentation/ui/onboarding/OnboardingUpsellPreviewData.kt
@@ -0,0 +1,190 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailupselling.presentation.ui.onboarding
+
+import ch.protonmail.android.mailcommon.presentation.model.TextUiModel
+import ch.protonmail.android.mailupselling.presentation.R
+import ch.protonmail.android.mailupselling.presentation.model.DynamicEntitlementUiModel
+import ch.protonmail.android.mailupselling.presentation.model.OnboardingUpsellButtonsUiModel
+import ch.protonmail.android.mailupselling.presentation.model.OnboardingUpsellPlanSwitcherUiModel
+import ch.protonmail.android.mailupselling.presentation.model.OnboardingUpsellPlanUiModel
+import ch.protonmail.android.mailupselling.presentation.model.OnboardingUpsellPlanUiModels
+
+internal object OnboardingUpsellPreviewData {
+
+    val PlanSwitcherUiModel = OnboardingUpsellPlanSwitcherUiModel(
+        discount = TextUiModel.Text("Save 20%")
+    )
+
+    val PlanUiModels = OnboardingUpsellPlanUiModels(
+        monthlyPlans = listOf(
+            OnboardingUpsellPlanUiModel(
+                title = "Unlimited",
+                currency = "CHF",
+                monthlyPrice = null,
+                monthlyPriceWithDiscount = 12.99f,
+                entitlements = listOf(
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 1"),
+                        localResource = R.drawable.ic_upselling_pass
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 2"),
+                        localResource = R.drawable.ic_upselling_mail
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 3"),
+                        localResource = R.drawable.ic_upselling_gift
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 4"),
+                        localResource = R.drawable.ic_upselling_tag
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 5"),
+                        localResource = R.drawable.ic_upselling_storage
+                    )
+                )
+            ),
+            OnboardingUpsellPlanUiModel(
+                title = "Mail Plus",
+                currency = "CHF",
+                monthlyPrice = null,
+                monthlyPriceWithDiscount = 4.99f,
+                entitlements = listOf(
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 1"),
+                        localResource = R.drawable.ic_upselling_pass
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 2"),
+                        localResource = R.drawable.ic_upselling_mail
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 3"),
+                        localResource = R.drawable.ic_upselling_gift
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 4"),
+                        localResource = R.drawable.ic_upselling_tag
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 5"),
+                        localResource = R.drawable.ic_upselling_storage
+                    )
+                )
+            ),
+            OnboardingUpsellPlanUiModel(
+                title = "Proton Free",
+                currency = null,
+                monthlyPrice = null,
+                monthlyPriceWithDiscount = null,
+                entitlements = listOf(
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 1"),
+                        localResource = R.drawable.ic_upselling_pass
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 2"),
+                        localResource = R.drawable.ic_upselling_mail
+                    )
+                )
+            )
+        ),
+        annualPlans = listOf(
+            OnboardingUpsellPlanUiModel(
+                title = "Unlimited",
+                currency = "CHF",
+                monthlyPrice = 12.99f,
+                monthlyPriceWithDiscount = 9.99f,
+                entitlements = listOf(
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 1"),
+                        localResource = R.drawable.ic_upselling_pass
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 2"),
+                        localResource = R.drawable.ic_upselling_mail
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 3"),
+                        localResource = R.drawable.ic_upselling_gift
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 4"),
+                        localResource = R.drawable.ic_upselling_tag
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 5"),
+                        localResource = R.drawable.ic_upselling_storage
+                    )
+                )
+            ),
+            OnboardingUpsellPlanUiModel(
+                title = "Mail Plus",
+                currency = "CHF",
+                monthlyPrice = 4.99f,
+                monthlyPriceWithDiscount = 3.99f,
+                entitlements = listOf(
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 1"),
+                        localResource = R.drawable.ic_upselling_pass
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 2"),
+                        localResource = R.drawable.ic_upselling_mail
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 3"),
+                        localResource = R.drawable.ic_upselling_gift
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 4"),
+                        localResource = R.drawable.ic_upselling_tag
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 5"),
+                        localResource = R.drawable.ic_upselling_storage
+                    )
+                )
+            ),
+            OnboardingUpsellPlanUiModel(
+                title = "Proton Free",
+                currency = null,
+                monthlyPrice = null,
+                monthlyPriceWithDiscount = null,
+                entitlements = listOf(
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 1"),
+                        localResource = R.drawable.ic_upselling_pass
+                    ),
+                    DynamicEntitlementUiModel.Overridden(
+                        text = TextUiModel.Text("Entitlement 2"),
+                        localResource = R.drawable.ic_upselling_mail
+                    )
+                )
+            )
+        )
+    )
+
+    val ButtonsUiModel = OnboardingUpsellButtonsUiModel(
+        monthlyBillingMessage = TextUiModel.Text("Billed at CHF 9.99 every month"),
+        annualBillingMessage = TextUiModel.Text("Billed at CHF 119.88 every 12 months")
+    )
+}
