b884a3288b1c652f94b50a171bdf85bb5899a641
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2024-09-24 18:09:31 +0200

    Create OnboardingUpsellViewModel
    MAILANDR-2185

---
+++ mail-upselling/presentation/src/main/kotlin/ch/protonmail/android/mailupselling/presentation/viewmodel/OnboardingUpsellViewModel.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailupselling.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import ch.protonmail.android.mailcommon.domain.usecase.ObservePrimaryUser
+import ch.protonmail.android.mailupselling.presentation.model.OnboardingUpsellState
+import ch.protonmail.android.mailupselling.presentation.model.OnboardingUpsellState.Loading
+import ch.protonmail.android.mailupselling.presentation.model.OnboardingUpsellState.OnboardingUpsellOperation.OnboardingUpsellEvent
+import ch.protonmail.android.mailupselling.presentation.reducer.OnboardingUpsellReducer
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.mapLatest
+import kotlinx.coroutines.flow.update
+import me.proton.core.plan.domain.usecase.GetDynamicPlansAdjustedPrices
+import javax.inject.Inject
+
+@HiltViewModel
+internal class OnboardingUpsellViewModel @Inject constructor(
+    observePrimaryUser: ObservePrimaryUser,
+    private val getDynamicPlansAdjustedPrices: GetDynamicPlansAdjustedPrices,
+    private val onboardingUpsellReducer: OnboardingUpsellReducer
+) : ViewModel() {
+
+    private val mutableState = MutableStateFlow<OnboardingUpsellState>(Loading)
+    val state = mutableState.asStateFlow()
+
+    init {
+        observePrimaryUser().mapLatest { user ->
+            val userId = user?.userId
+                ?: return@mapLatest emitNewStateFrom(OnboardingUpsellEvent.LoadingError.NoUserId)
+
+            val dynamicPlans = runCatching { getDynamicPlansAdjustedPrices(userId) }.getOrElse {
+                return@mapLatest emitNewStateFrom(OnboardingUpsellEvent.LoadingError.NoSubscriptions)
+            }
+
+            emitNewStateFrom(OnboardingUpsellEvent.DataLoaded(dynamicPlans))
+        }.launchIn(viewModelScope)
+    }
+
+    private fun emitNewStateFrom(operation: OnboardingUpsellState.OnboardingUpsellOperation) {
+        mutableState.update { onboardingUpsellReducer.newStateFrom(operation) }
+    }
+}
---
+++ mail-upselling/presentation/src/test/kotlin/ch/protonmail/android/mailupselling/presentation/viewmodel/OnboardingUpsellViewModelTest.kt
@@ -0,0 +1,135 @@
+package ch.protonmail.android.mailupselling.presentation.viewmodel
+
+import java.time.Instant
+import app.cash.turbine.test
+import ch.protonmail.android.mailcommon.domain.sample.UserSample
+import ch.protonmail.android.mailcommon.domain.usecase.ObservePrimaryUser
+import ch.protonmail.android.mailupselling.domain.model.DynamicPlansOneClickIds
+import ch.protonmail.android.mailupselling.presentation.mapper.OnboardingUpsellButtonsUiModelMapper
+import ch.protonmail.android.mailupselling.presentation.mapper.OnboardingUpsellPlanSwitcherUiModelMapper
+import ch.protonmail.android.mailupselling.presentation.mapper.OnboardingUpsellPlanUiModelsMapper
+import ch.protonmail.android.mailupselling.presentation.model.OnboardingUpsellState
+import ch.protonmail.android.mailupselling.presentation.reducer.OnboardingUpsellReducer
+import ch.protonmail.android.mailupselling.presentation.ui.onboarding.OnboardingUpsellPreviewData
+import ch.protonmail.android.testdata.upselling.UpsellingTestData
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.mockkStatic
+import io.mockk.unmockkAll
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import me.proton.core.domain.entity.UserId
+import me.proton.core.plan.domain.entity.DynamicPlans
+import me.proton.core.plan.domain.usecase.GetDynamicPlansAdjustedPrices
+import me.proton.core.user.domain.entity.User
+import kotlin.test.AfterTest
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertIs
+
+class OnboardingUpsellViewModelTest {
+
+    private val user = UserSample.Primary
+    private val dynamicPlans = UpsellingTestData.DynamicPlans
+
+    private val observePrimaryUser = mockk<ObservePrimaryUser>()
+    private val getDynamicPlansAdjustedPrices = mockk<GetDynamicPlansAdjustedPrices>()
+    private val onboardingUpsellPlanSwitchUiModelMapper = mockk<OnboardingUpsellPlanSwitcherUiModelMapper> {
+        every { toUiModel(UpsellingTestData.DynamicPlans) } returns OnboardingUpsellPreviewData.PlanSwitcherUiModel
+    }
+    private val onboardingUpsellPlanUiModelsMapper = mockk<OnboardingUpsellPlanUiModelsMapper> {
+        every { toUiModel(UpsellingTestData.DynamicPlans) } returns OnboardingUpsellPreviewData.PlanUiModels
+    }
+    private val onboardingUpsellButtonsUiModelMapper = mockk<OnboardingUpsellButtonsUiModelMapper> {
+        every {
+            toUiModel(UpsellingTestData.DynamicPlans, DynamicPlansOneClickIds.UnlimitedPlanId)
+        } returns OnboardingUpsellPreviewData.ButtonsUiModel
+    }
+
+    private val onboardingUpsellReducer = OnboardingUpsellReducer(
+        onboardingUpsellPlanSwitchUiModelMapper,
+        onboardingUpsellPlanUiModelsMapper,
+        onboardingUpsellButtonsUiModelMapper
+    )
+
+    private val viewModel: OnboardingUpsellViewModel by lazy {
+        OnboardingUpsellViewModel(
+            observePrimaryUser,
+            getDynamicPlansAdjustedPrices,
+            onboardingUpsellReducer
+        )
+    }
+
+    @BeforeTest
+    fun setUp() {
+        Dispatchers.setMain(UnconfinedTestDispatcher())
+        mockkStatic(Instant::class)
+    }
+
+    @AfterTest
+    fun teardown() {
+        unmockkAll()
+    }
+
+    @Test
+    fun `should emit loading state at start`() = runTest {
+        every { observePrimaryUser() } returns flowOf()
+
+        // When + Then
+        viewModel.state.test {
+            assertEquals(OnboardingUpsellState.Loading, awaitItem())
+        }
+    }
+
+    @Test
+    fun `should emit error when user is null`() = runTest {
+        // Given
+        expectPrimaryUser(null)
+
+        // When + Then
+        viewModel.state.test {
+            assertIs<OnboardingUpsellState.Error>(awaitItem())
+        }
+    }
+
+    @Test
+    fun `should emit error when dynamic plans fail to be fetched`() = runTest {
+        // Given
+        expectPrimaryUser(user)
+        expectDynamicPlansError(user.userId)
+
+        // When + Then
+        viewModel.state.test {
+            assertIs<OnboardingUpsellState.Error>(awaitItem())
+        }
+    }
+
+    @Test
+    fun `should emit data when dynamic plans are fetched`() = runTest {
+        // Given
+        expectPrimaryUser(user)
+        expectDynamicPlans(user.userId, dynamicPlans)
+
+        // When + Then
+        viewModel.state.test {
+            assertIs<OnboardingUpsellState.Data>(awaitItem())
+        }
+    }
+
+    private fun expectPrimaryUser(user: User?) {
+        every { observePrimaryUser() } returns flowOf(user)
+    }
+
+    private fun expectDynamicPlans(userId: UserId, dynamicPlans: DynamicPlans) {
+        coEvery { getDynamicPlansAdjustedPrices(userId) } returns dynamicPlans
+    }
+
+    private fun expectDynamicPlansError(userId: UserId) {
+        coEvery { getDynamicPlansAdjustedPrices(userId) } throws Exception()
+    }
+}
