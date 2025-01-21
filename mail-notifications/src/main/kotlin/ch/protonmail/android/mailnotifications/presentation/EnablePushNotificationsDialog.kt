db55c8099051a80a15390ee28f373ca89d08b0a7
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2025-01-21 14:17:18 +0100

    Make the content of the EnablePushNotificationsDialog scrollable
    MAILANDR-2450

--- mail-notifications/src/main/kotlin/ch/protonmail/android/mailnotifications/presentation/EnablePushNotificationsDialog.kt
+++ mail-notifications/src/main/kotlin/ch/protonmail/android/mailnotifications/presentation/EnablePushNotificationsDialog.kt
@@ -29,7 +29,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -95,7 +97,9 @@ fun EnablePushNotificationsDialog(
             }
 
             Column(
-                modifier = Modifier.fillMaxWidth(),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(rememberScrollState()),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Box(
