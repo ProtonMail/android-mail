2f128f2b0df8eed540470165918edbe59383f596
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2025-01-28 15:39:35 +0100

    Create notification permission telemetry models
    MAILANDR-2405

---
+++ mail-notifications/src/main/kotlin/ch/protonmail/android/mailnotifications/domain/model/telemetry/NotificationPermissionTelemetryEvent.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailnotifications.domain.model.telemetry
+
+import me.proton.core.telemetry.domain.entity.TelemetryEvent
+
+sealed class NotificationPermissionTelemetryEvent(
+    val name: String,
+    open val dimensions: NotificationPermissionTelemetryEventDimensions
+) {
+
+    data class NotificationPermissionDialogDisplayed(
+        override val dimensions: NotificationPermissionTelemetryEventDimensions
+    ) : NotificationPermissionTelemetryEvent(name = "notification_permission_dialog_shown", dimensions)
+
+    data class NotificationPermissionDialogEnable(
+        override val dimensions: NotificationPermissionTelemetryEventDimensions
+    ) : NotificationPermissionTelemetryEvent(name = "notification_permission_dialog_enable", dimensions)
+
+    data class NotificationPermissionDialogDismiss(
+        override val dimensions: NotificationPermissionTelemetryEventDimensions
+    ) : NotificationPermissionTelemetryEvent(name = "notification_permission_dialog_dismiss", dimensions)
+
+    fun toTelemetryEvent() = TelemetryEvent(
+        group = EventGroup,
+        name = name,
+        dimensions = dimensions.asMap()
+    )
+
+    private companion object {
+
+        const val EventGroup = "mail.any.notification_permission"
+    }
+}
---
+++ mail-notifications/src/main/kotlin/ch/protonmail/android/mailnotifications/domain/model/telemetry/NotificationPermissionTelemetryEventType.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailnotifications.domain.model.telemetry
+
+import ch.protonmail.android.mailnotifications.presentation.model.NotificationPermissionDialogType
+
+sealed interface NotificationPermissionTelemetryEventType {
+
+    data class NotificationPermissionDialogDisplayed(
+        val notificationPermissionDialogType: NotificationPermissionDialogType
+    ) : NotificationPermissionTelemetryEventType
+
+    data class NotificationPermissionDialogEnable(
+        val notificationPermissionDialogType: NotificationPermissionDialogType
+    ) : NotificationPermissionTelemetryEventType
+
+    data class NotificationPermissionDialogDismiss(
+        val notificationPermissionDialogType: NotificationPermissionDialogType
+    ) : NotificationPermissionTelemetryEventType
+}
