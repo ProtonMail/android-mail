82249dabad60b5d0dbab2a5bbc20830d08cce781
Rok Oblak <rok.oblak@external.proton.ch>
2025-02-10 23:20:16 +0700

    Bump to 4.7.2
    MAILANDR-2489: trash action condition

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -23,5 +23,5 @@ object Config {
     const val targetSdk = 34
     const val testInstrumentationRunner = "ch.protonmail.android.uitest.HiltTestRunner"
     const val versionCode = 1
-    const val versionName = "4.7.1"
+    const val versionName = "4.7.2"
 }
