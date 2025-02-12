195f82641b3220bb41d2d59b2252a69b86e870c9
Niccolò Forlini <niccolo.forlini@proton.ch>
2025-02-12 16:53:11 +0100

    Add new Subject and Body textfield
    Both have the TFS hoisted compared to the "v1".
    
    MAILANDR-2468

---
+++ mail-composer/presentation/src/main/kotlin/ch/protonmail/android/mailcomposer/presentation/ui/BodyTextField2.kt
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.presentation.ui
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import ch.protonmail.android.mailcommon.presentation.ConsumableLaunchedEffect
+import ch.protonmail.android.mailcommon.presentation.Effect
+import ch.protonmail.android.mailcomposer.presentation.R
+import me.proton.core.compose.theme.ProtonDimens
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.core.compose.theme.defaultNorm
+
+@Composable
+internal fun BodyTextField2(
+    textFieldState: TextFieldState,
+    shouldRequestFocus: Effect<Unit>,
+    modifier: Modifier = Modifier
+) {
+    val focusRequester = remember { FocusRequester() }
+    val orientation = LocalConfiguration.current.orientation
+
+    var shouldFocus by remember { mutableStateOf(false) }
+    var isFocused by remember { mutableStateOf(false) }
+
+    val bodyMinLines = if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+        Composer.MessageBodyPortraitMinLines
+    } else {
+        1
+    }
+
+    BasicTextField(
+        state = textFieldState,
+        modifier = modifier
+            .fillMaxSize()
+            .padding(ProtonDimens.DefaultSpacing)
+            .focusRequester(focusRequester)
+            .onFocusChanged { isFocused = it.isFocused }
+            .onGloballyPositioned {
+                if (shouldFocus) {
+                    focusRequester.requestFocus()
+                    shouldFocus = false
+                }
+            },
+        keyboardOptions = KeyboardOptions.Default.copy(capitalization = KeyboardCapitalization.Sentences),
+        textStyle = ProtonTheme.typography.defaultNorm,
+        cursorBrush = SolidColor(TextFieldDefaults.colors().cursorColor),
+        lineLimits = TextFieldLineLimits.MultiLine(minHeightInLines = bodyMinLines),
+        decorator = @Composable { innerTextField ->
+            if (textFieldState.text.isEmpty()) {
+                PlaceholderText()
+            }
+
+            innerTextField()
+        }
+    )
+
+    ConsumableLaunchedEffect(shouldRequestFocus) {
+        shouldFocus = true
+    }
+}
+
+@Composable
+private fun PlaceholderText() {
+    Text(
+        modifier = Modifier.testTag(ComposerTestTags.MessageBodyPlaceholder),
+        text = stringResource(R.string.compose_message_placeholder),
+        color = ProtonTheme.colors.textHint,
+        style = ProtonTheme.typography.defaultNorm
+    )
+}
---
+++ mail-composer/presentation/src/main/kotlin/ch/protonmail/android/mailcomposer/presentation/ui/SubjectTextField2.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2022 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.mailcomposer.presentation.ui
+
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.text.input.TextFieldLineLimits
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import ch.protonmail.android.mailcomposer.presentation.R
+import me.proton.core.compose.theme.ProtonTheme
+import me.proton.core.compose.theme.defaultNorm
+
+@Composable
+internal fun SubjectTextField2(textFieldState: TextFieldState, modifier: Modifier = Modifier) {
+
+    val keyboardOptions = remember {
+        KeyboardOptions.Default.copy(capitalization = KeyboardCapitalization.Sentences, imeAction = ImeAction.Next)
+    }
+
+    BasicTextField(
+        modifier = modifier,
+        state = textFieldState,
+        textStyle = ProtonTheme.typography.defaultNorm,
+        cursorBrush = SolidColor(TextFieldDefaults.colors().cursorColor),
+        lineLimits = TextFieldLineLimits.SingleLine,
+        keyboardOptions = keyboardOptions,
+        decorator = @Composable { innerTextField ->
+            if (textFieldState.text.isEmpty()) {
+                PlaceholderText()
+            }
+            innerTextField()
+        }
+    )
+}
+
+@Composable
+private fun PlaceholderText() {
+    Text(
+        modifier = Modifier.testTag(ComposerTestTags.SubjectPlaceholder),
+        text = stringResource(R.string.subject_placeholder),
+        color = ProtonTheme.colors.textHint,
+        style = ProtonTheme.typography.defaultNorm
+    )
+}
