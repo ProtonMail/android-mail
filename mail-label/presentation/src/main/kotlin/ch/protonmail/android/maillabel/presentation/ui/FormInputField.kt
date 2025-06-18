40ac9023420901b1360e5d0945e366d21aaf1017
Michaël Sobol <michael.sobol@proton.ch>
2025-06-18 08:46:23 +0000

    Edit strings.xml

--- mail-label/presentation/src/main/kotlin/ch/protonmail/android/maillabel/presentation/ui/FormInputField.kt
+++ mail-label/presentation/src/main/kotlin/ch/protonmail/android/maillabel/presentation/ui/FormInputField.kt
@@ -36,6 +36,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.pluralStringResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardCapitalization
@@ -115,10 +116,10 @@ fun FormInputField(
         )
 
         Text(
-            text = stringResource(
-                id = R.string.input_field_max_characters_count,
-                textFieldValue.text.length,
-                maxCharacters
+            text = pluralStringResource(
+                id = R.plurals.input_field_max_characters_count,
+                count = textFieldValue.text.length,
+                textFieldValue.text.length, maxCharacters
             ),
             modifier = Modifier.padding(
                 top = ProtonDimens.ExtraSmallSpacing
