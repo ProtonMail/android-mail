13e4cfc6db7faf8bb794e980a9f6bef9cee27f37
Stefanija Boshkovska <stefanija.boshkovska@proton.ch>
2026-02-09 15:10:38 +0100

    Add discreet app icon name in settings
    ET-5833

--- mail-settings/presentation/src/main/kotlin/ch/protonmail/android/mailsettings/presentation/settings/appicon/ui/SelectableAppIcon.kt
+++ mail-settings/presentation/src/main/kotlin/ch/protonmail/android/mailsettings/presentation/settings/appicon/ui/SelectableAppIcon.kt
@@ -22,9 +22,13 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
@@ -32,6 +36,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import ch.protonmail.android.design.compose.theme.ProtonDimens
 import ch.protonmail.android.design.compose.theme.ProtonTheme
+import ch.protonmail.android.design.compose.theme.bodyLargeNorm
 import ch.protonmail.android.mailcommon.presentation.NO_CONTENT_DESCRIPTION
 import ch.protonmail.android.mailsettings.presentation.settings.appicon.model.AppIconUiModel
 import coil.compose.AsyncImage
@@ -43,33 +48,44 @@ internal fun SelectableAppIcon(
     onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
-    Box(
-        modifier = modifier
-            .then(
-                if (isSelected) {
-                    Modifier.border(
-                        width = ProtonDimens.BorderSize.Semilarge,
-                        color = ProtonTheme.colors.brandNorm,
-                        shape = RoundedCornerShape(ProtonDimens.CornerRadius.Jumbo)
-                    )
-                } else {
-                    Modifier
-                }
-            )
-            .padding(ProtonDimens.Spacing.Compact),
-        contentAlignment = Alignment.Center
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        AsyncImage(
-            model = preset.iconPreviewResId,
-            contentDescription = NO_CONTENT_DESCRIPTION,
-            modifier = Modifier
-                .size(ProtonDimens.IconSize.ExtraExtraLarge)
-                .clickable(
-                    interactionSource = remember { MutableInteractionSource() },
-                    indication = null,
-                    onClick = onClick
-                ),
-            contentScale = ContentScale.Fit
+        Box(
+            modifier = modifier
+                .then(
+                    if (isSelected) {
+                        Modifier.border(
+                            width = ProtonDimens.BorderSize.Semilarge,
+                            color = ProtonTheme.colors.brandNorm,
+                            shape = RoundedCornerShape(ProtonDimens.CornerRadius.Jumbo)
+                        )
+                    } else {
+                        Modifier
+                    }
+                )
+                .padding(ProtonDimens.Spacing.Compact),
+            contentAlignment = Alignment.Center
+        ) {
+            AsyncImage(
+                model = preset.iconPreviewResId,
+                contentDescription = NO_CONTENT_DESCRIPTION,
+                modifier = Modifier
+                    .size(ProtonDimens.IconSize.ExtraExtraLarge)
+                    .clickable(
+                        interactionSource = remember { MutableInteractionSource() },
+                        indication = null,
+                        onClick = onClick
+                    ),
+                contentScale = ContentScale.Fit
+            )
+        }
+        Spacer(
+            modifier = Modifier.height(ProtonDimens.Spacing.Standard)
+        )
+        Text(
+            text = preset.data.id.name,
+            style = ProtonTheme.typography.bodyLargeNorm
         )
     }
 }
