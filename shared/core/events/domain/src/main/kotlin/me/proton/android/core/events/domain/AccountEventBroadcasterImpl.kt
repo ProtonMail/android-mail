d04f7511c2d0a6822f56a14f2f64c0d2c3c47a39
Niccolò Forlini <niccolo.forlini@proton.ch>
2026-02-16 14:25:56 +0100

    Add shared events infrastructure
    Introduces AccountEvent and AccountEventBroadcaster for cross-feature event communication
    
    ET-5812

---
+++ shared/core/events/domain/src/main/kotlin/me/proton/android/core/events/domain/AccountEventBroadcasterImpl.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2026 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package me.proton.android.core.events.domain
+
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.receiveAsFlow
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class AccountEventBroadcasterImpl @Inject constructor() : AccountEventBroadcaster {
+
+    private val channel = Channel<AccountEvent>(capacity = Channel.BUFFERED)
+    override val events: Flow<AccountEvent> = channel.receiveAsFlow()
+
+    override suspend fun emit(event: AccountEvent) {
+        channel.send(event)
+    }
+}
