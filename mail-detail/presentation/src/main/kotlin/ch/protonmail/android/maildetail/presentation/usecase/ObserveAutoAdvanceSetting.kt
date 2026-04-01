e549e723696d46dd2e134646ac26470bfa13bfd3
Serdar Ozturk <serdar.ozturk@external.proton.ch>
2026-04-01 14:14:11 +0100

    Add pager settings observer use cases
     - Introduce dedicated use cases for observing swipe-next and auto-advance
    preferences.
    
    ET-6061
    
    (cherry picked from commit baebfe5f1aad4a5b3844951415a5b93c5a42e832)

---
+++ mail-detail/presentation/src/main/kotlin/ch/protonmail/android/maildetail/presentation/usecase/ObserveAutoAdvanceSetting.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2026 Proton Technologies AG
+ * This file is part of Proton Technologies AG and Proton Mail.
+ *
+ * Proton Mail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Proton Mail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Proton Mail. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.protonmail.android.maildetail.presentation.usecase
+
+import ch.protonmail.android.mailsettings.domain.repository.AutoAdvanceRepository
+import kotlinx.coroutines.flow.Flow
+import me.proton.core.domain.entity.UserId
+import javax.inject.Inject
+
+class ObserveAutoAdvanceSetting @Inject constructor(
+    private val autoAdvanceRepository: AutoAdvanceRepository
+) {
+
+    operator fun invoke(userId: UserId): Flow<Boolean> = autoAdvanceRepository.observeAutoAdvance(userId)
+}
