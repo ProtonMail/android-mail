c1bff9e3c3c73cc20e44ae3e83343a2b768017f7
Serdar Ozturk <serdar.ozturk@proton.ch>
2026-05-19 12:24:28 +0100

    Category View:  Prevent text clipping with large font sizes
    ET-6227

--- mail-category-view/presentation/src/main/kotlin/ch/protonmail/android/mailcategory/presentation/CategoryPill.kt
+++ mail-category-view/presentation/src/main/kotlin/ch/protonmail/android/mailcategory/presentation/CategoryPill.kt
@@ -26,7 +26,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -82,7 +82,7 @@ fun CategoryPill(
             .clip(shape)
             .background(backgroundColor)
             .clickable(onClick = onClick)
-            .height(MailDimens.CategoryView.ItemHeight)
+            .defaultMinSize(minHeight = MailDimens.CategoryView.ItemHeight)
             .padding(
                 start = ProtonDimens.Spacing.ModeratelyLarge,
                 top = ProtonDimens.Spacing.MediumLight,
